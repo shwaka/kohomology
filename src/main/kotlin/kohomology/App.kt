784de59558a2f241e8fea6fb1b2fe2892d84b303
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-12-27 19:46:26 +0900

    add Fp

--- src/main/kotlin/kohomology/App.kt
+++ src/main/kotlin/kohomology/App.kt
@@ -1,5 +1,23 @@
 package kohomology
 
+interface Scalar<S> {
+    operator fun plus(other: S): S
+    operator fun plus(other: Scalar<S>): Scalar<S> {
+        return this.field.wrap(this + other.unwrap())
+    }
+    fun unwrap(): S
+    val field: Field<S>
+}
+
+interface Field<S> {
+    fun wrap(a: S): Scalar<S>
+    fun fromInteger(n: Int): Scalar<S>
+}
+
+fun <S> add(a: Scalar<S>, b: Scalar<S>): Scalar<S> {
+    return a.field.wrap(a + b.unwrap())
+}
+
 data class Rational(val numerator: Int, val denominator: Int) : Scalar<Rational> {
     override val field = RationalField
     override operator fun plus(other: Rational): Rational {
@@ -27,26 +45,41 @@ object RationalField : Field<Rational> {
     override fun wrap(a: Rational): Scalar<Rational> {
         return a
     }
-    override fun fromInteger(n: Int): Scalar<Rational> {
+    override fun fromInteger(n: Int): Rational {
         return Rational(n, 1)
     }
 }
 
-interface Scalar<S> {
-    operator fun plus(other: S): S
-    fun unwrap(): S
-    val field: Field<S>
+data class IntModp(val value: Int, val p: Int) : Scalar<IntModp> {
+    // 色々とチェックした方が良さそう
+    // p > 0
+    override val field: Field<IntModp>
+    init {
+        // 毎回オブジェクト生成するのは無駄
+        this.field = Fp(this.p)
+    }
+    override operator fun plus(other: IntModp): IntModp {
+        if (this.p != other.p) {
+            throw Error("different characteristic!")
+        }
+        return IntModp(this.value + other.value, this.p)
+    }
+    override fun unwrap(): IntModp {
+        return this
+    }
+    override fun toString(): String {
+        return "${this.value % this.p} mod ${this.p}"
+    }
 }
 
-interface Field<S> {
-    fun wrap(a: S): Scalar<S>
-    fun fromInteger(n: Int): Scalar<S>
+data class Fp(val p: Int) : Field<IntModp> {
+    override fun wrap(a: IntModp): Scalar<IntModp> {
+        return a
+    }
+    override fun fromInteger(n: Int): IntModp {
+        return IntModp(n, p)
+    }
 }
-
-fun <S> add(a: Scalar<S>, b: Scalar<S>): Scalar<S> {
-    return a.field.wrap(a + b.unwrap())
-}
-
 
 fun main(args: Array<String>) {
     println("Hello world!")
@@ -56,4 +89,10 @@ fun main(args: Array<String>) {
     val a = Rational(1, 2)
     val b = RationalField.fromInteger(2)
     println(add(a, b))
+
+    val p = 5
+    val c = IntModp(3, p)
+    val d = IntModp(1, p)
+    println(add(c, d))
+    println(add(c, d + Fp(p).fromInteger(1)))
 }

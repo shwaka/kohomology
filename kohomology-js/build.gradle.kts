4b508543aa305824a1cb304d67fccef9eb575323
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-02 15:01:09 +0900

    add kohomology-js

---
+++ kohomology-js/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("js") version "1.4.10"
+}
+
+group = "me.shun"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    flatDir {
+        dirs("../kohomology/core/build/libs")
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test-js"))
+    implementation("")
+}
+
+kotlin {
+    js(LEGACY) {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+    }
+}
--- kohomology/core/build.gradle.kts
+++ kohomology/core/build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     // id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    kotlin("jvm") version "1.4.10"
+    kotlin("multiplatform") version "1.4.10"
     id("io.kotest") version "0.2.6"
-
-    // Apply the application plugin to add support for building a CLI application.
-    application
 }
 
 repositories {
@@ -14,36 +11,54 @@ repositories {
     jcenter()
 }
 
+kotlin {
+    js {
+        browser()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                // Align versions of all Kotlin components
+                implementation("org.jetbrains.kotlin:kotlin-bom")
+                // Use the Kotlin JDK 8 standard library.
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+                implementation("com.ionspin.kotlin:bignum:0.2.3")
+            }
+        }
+    }
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
 val ktlint by configurations.creating
 
-dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    implementation("com.ionspin.kotlin:bignum:0.2.3")
-
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    // kotest
-    val version = "4.3.2"
-    testImplementation("io.kotest:kotest-runner-junit5:$version")
-    testImplementation("io.kotest:kotest-assertions-core:$version")
-    testImplementation("io.kotest:kotest-property:$version")
-    testImplementation("io.kotest:kotest-assertions-compiler:$version")
-
-    ktlint("com.pinterest:ktlint:0.40.0")
-}
+//dependencies {
+//    // Align versions of all Kotlin components
+//    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+//
+//    // Use the Kotlin JDK 8 standard library.
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+//
+//    implementation("com.ionspin.kotlin:bignum:0.2.3")
+//
+//    // Use the Kotlin test library.
+//    testImplementation("org.jetbrains.kotlin:kotlin-test")
+//
+//    // Use the Kotlin JUnit integration.
+//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+//
+//    // kotest
+//    val version = "4.3.2"
+//    testImplementation("io.kotest:kotest-runner-junit5:$version")
+//    testImplementation("io.kotest:kotest-assertions-core:$version")
+//    testImplementation("io.kotest:kotest-property:$version")
+//    testImplementation("io.kotest:kotest-assertions-compiler:$version")
+//
+//    ktlint("com.pinterest:ktlint:0.40.0")
+//}
 
 buildscript {
     repositories {

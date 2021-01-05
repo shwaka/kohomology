4f2f749c52152304fa34d6c21fa46d17bcaa3e49
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-05 17:12:33 +0900

    improve app/build.gradle.kts around ktlint

--- kohomology/app/build.gradle.kts
+++ kohomology/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     // id("org.jetbrains.kotlin.jvm") version "1.3.72"
     kotlin("jvm")
     id("io.kotest") version "0.2.6"
+    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 
     // Apply the application plugin to add support for building a CLI application.
     application
@@ -18,8 +19,6 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-val ktlint by configurations.creating
-
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
@@ -29,20 +28,8 @@ dependencies {
 
     implementation("com.ionspin.kotlin:bignum:0.2.3")
 
-    ktlint("com.pinterest:ktlint:0.40.0")
-
     implementation(project(":core"))
 }
-
-buildscript {
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-    }
-    dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
-    }
-}
-apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
 application {
     // Define the main class for the application.

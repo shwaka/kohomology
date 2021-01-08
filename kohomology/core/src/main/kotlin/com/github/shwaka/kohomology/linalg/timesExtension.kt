03799c8e1b5b1d2b4710adca58c91c981815c3e3
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-08 10:50:23 +0900

    add test for times

--- kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/linalg/timesExtension.kt
+++ kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/linalg/timesExtension.kt
@@ -1,8 +1,14 @@
 package com.github.shwaka.kohomology.linalg
 
+import com.github.shwaka.kohomology.field.BigRational
 import com.github.shwaka.kohomology.field.IntRational
 
 // Cannot use generics since IntRational.times without generics already exists
+
 operator fun IntRational.times(other: DenseNumVector<IntRational>): DenseNumVector<IntRational> {
+    return other * this
+}
+
+operator fun BigRational.times(other: DenseNumVector<BigRational>): DenseNumVector<BigRational> {
     return other * this
 }
--- kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/DenseNumVectorTest.kt
+++ kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/DenseNumVectorTest.kt
@@ -1,16 +1,17 @@
 package com.github.shwaka.kohomology
 
-import com.github.shwaka.kohomology.field.IntRationalField
+import com.github.shwaka.kohomology.field.*
 import com.github.shwaka.kohomology.linalg.DenseNumVector
 import com.github.shwaka.kohomology.linalg.times
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.stringSpec
 import io.kotest.matchers.compilation.shouldCompile
 import io.kotest.matchers.shouldBe
 
-class IntRationalNumVectorTest : StringSpec({
-    val zero = IntRationalField.ZERO
-    val one = IntRationalField.ONE
-    val two = IntRationalField.fromInt(2)
+fun <S> denseNumVectorTest(field: Field<S>) = stringSpec {
+    val zero = field.ZERO
+    val one = field.ONE
+    val two = field.fromInt(2)
     "(0, 1) + (0, 1) should be (0, 2)" {
         val v = DenseNumVector(listOf(zero, one))
         val w = DenseNumVector(listOf(zero, two))
@@ -21,23 +22,34 @@ class IntRationalNumVectorTest : StringSpec({
         val w = DenseNumVector(listOf(two, zero))
         (v * two) shouldBe w
     }
-    "2 * (1, 0) should be (2, 0)" {
-        // This works only for IntRational
-        val v = DenseNumVector(listOf(one, zero))
-        val w = DenseNumVector(listOf(two, zero))
-        (two * v) shouldBe w
-    }
     "multiplying scalar from left should compile" {
         val fieldName = IntRationalField::class.java.simpleName
         val code =
             """
                 import com.github.shwaka.kohomology.field.$fieldName
-                import com.github.shwaka.kohomology.linalg.NumericalDenseVector
+                import com.github.shwaka.kohomology.linalg.DenseNumVector
                 import com.github.shwaka.kohomology.linalg.times
                 val one = $fieldName.ONE
-                val v = NumericalDenseVector(listOf(one, one))
+                val v = DenseNumVector(listOf(one, one))
                 val w = one * v
             """
         code.shouldCompile()
     }
+}
+
+class IntRationalDenseNumVectorTest : StringSpec({
+    include(denseNumVectorTest(IntRationalField))
+    "2 * (1, 0) should be (2, 0)" {
+        val field = IntRationalField
+        val zero = field.ZERO
+        val one = field.ONE
+        val two = field.fromInt(2)
+        val v = DenseNumVector(listOf(one, zero))
+        val w = DenseNumVector(listOf(two, zero))
+        (two * v) shouldBe w
+    }
+})
+
+class BigRationalDenseNumVectorTest : StringSpec({
+    include(denseNumVectorTest(BigRationalField))
 })

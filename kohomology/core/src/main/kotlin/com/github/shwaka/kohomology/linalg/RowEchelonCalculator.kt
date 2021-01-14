600540299c3e0cb5f2f1701923bf335861aa07ef
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-14 22:54:40 +0900

    use extension function in RowEchelonCalculator.kt

--- kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/linalg/RowEchelonCalculator.kt
+++ kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/linalg/RowEchelonCalculator.kt
@@ -2,35 +2,33 @@ package com.github.shwaka.kohomology.linalg
 
 import com.github.shwaka.kohomology.field.Scalar
 
-object RowEchelonCalculator {
-    fun <S : Scalar<S>> exchange(values: List<List<S>>, i1: Int, i2: Int): List<List<S>> {
-        if (i1 == i2) throw IllegalArgumentException("Row numbers must be distinct")
-        return values.indices.map { i ->
-            when (i) {
-                i1 -> values[i2]
-                i2 -> values[i1]
-                else -> values[i]
-            }
+fun <S : Scalar<S>> List<List<S>>.exchangeRows(i1: Int, i2: Int): List<List<S>> {
+    if (i1 == i2) throw IllegalArgumentException("Row numbers must be distinct")
+    return this.indices.map { i ->
+        when (i) {
+            i1 -> this[i2]
+            i2 -> this[i1]
+            else -> this[i]
         }
     }
+}
 
-    fun <S : Scalar<S>> add(values: List<List<S>>, from: Int, to: Int, scalar: S): List<List<S>> {
-        if (from == to) throw IllegalArgumentException("Row numbers must be distinct")
-        return values.indices.map { i ->
-            when (i) {
-                to -> values[to].zip(values[from]).map { (a, b) -> a + b * scalar }
-                else -> values[i]
-            }
+fun <S : Scalar<S>> List<List<S>>.addToAnotherRow(from: Int, to: Int, scalar: S): List<List<S>> {
+    if (from == to) throw IllegalArgumentException("Row numbers must be distinct")
+    return this.indices.map { i ->
+        when (i) {
+            to -> this[to].zip(this[from]).map { (a, b) -> a + b * scalar }
+            else -> this[i]
         }
     }
+}
 
-    fun <S : Scalar<S>> multiply(values: List<List<S>>, to: Int, scalar: S): List<List<S>> {
-        if (scalar == scalar.field.zero) throw IllegalArgumentException("scalar must be non-zero")
-        return values.indices.map { i ->
-            when (i) {
-                to -> values[to].map { a -> a * scalar }
-                else -> values[i]
-            }
+fun <S : Scalar<S>> List<List<S>>.multiplyScalarToRow(to: Int, scalar: S): List<List<S>> {
+    if (scalar == scalar.field.zero) throw IllegalArgumentException("scalar must be non-zero")
+    return this.indices.map { i ->
+        when (i) {
+            to -> this[to].map { a -> a * scalar }
+            else -> this[i]
         }
     }
 }
--- kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/linalg/RowEchelonCalculatorTest.kt
+++ kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/linalg/RowEchelonCalculatorTest.kt
@@ -23,14 +23,14 @@ fun <S : Scalar<S>> rowEchelonCalculatorTest(field: Field<S>) = stringSpec {
             listOf(one, one),
             listOf(one, two)
         )
-        RowEchelonCalculator.exchange(mat1, 0, 1) shouldBe mat2
+        mat1.exchangeRows(0, 1) shouldBe mat2
     }
     "add to another row" {
         val mat3 = listOf(
             listOf(one, two),
             listOf(zero, -one)
         )
-        RowEchelonCalculator.add(mat1, 0, 1, -one) shouldBe mat3
+        mat1.addToAnotherRow(0, 1, -one) shouldBe mat3
     }
 }
 

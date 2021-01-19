22eefba112811191309f395cc9ccd6e25a7750dd
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-19 17:45:24 +0900

    improve OverflowDetector

--- kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/field/OverflowDetector.kt
+++ kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/field/OverflowDetector.kt
@@ -4,6 +4,9 @@ import com.ionspin.kotlin.bignum.integer.BigInteger
 
 interface Integer<I : Integer<I>> {
     operator fun plus(other: Integer<*>): I
+    operator fun minus(other: Integer<*>): I
+    operator fun unaryMinus(): I
+    operator fun times(other: Integer<*>): I
     fun toWrappedBigInteger(): WrappedBigInteger
     fun toBigInteger(): BigInteger {
         return this.toWrappedBigInteger().value
@@ -14,6 +17,20 @@ class WrappedInt(private val value: Int) : Integer<WrappedInt> {
     override operator fun plus(other: Integer<*>): WrappedInt {
         other as WrappedInt
         return WrappedInt(this.value + other.value)
+    }
+
+    override fun minus(other: Integer<*>): WrappedInt {
+        other as WrappedInt
+        return WrappedInt(this.value - other.value)
+    }
+
+    override fun unaryMinus(): WrappedInt {
+        return WrappedInt(-this.value)
+    }
+
+    override fun times(other: Integer<*>): WrappedInt {
+        other as WrappedInt
+        return WrappedInt(this.value * other.value)
     }
 
     override fun toWrappedBigInteger(): WrappedBigInteger {
@@ -27,6 +44,20 @@ class WrappedBigInteger(val value: BigInteger) : Integer<WrappedBigInteger> {
         return WrappedBigInteger(this.value + other.value)
     }
 
+    override fun minus(other: Integer<*>): WrappedBigInteger {
+        other as WrappedBigInteger
+        return WrappedBigInteger(this.value - other.value)
+    }
+
+    override fun unaryMinus(): WrappedBigInteger {
+        return WrappedBigInteger(-this.value)
+    }
+
+    override fun times(other: Integer<*>): WrappedBigInteger {
+        other as WrappedBigInteger
+        return WrappedBigInteger(this.value * other.value)
+    }
+
     override fun toWrappedBigInteger(): WrappedBigInteger {
         return this
     }
@@ -37,6 +68,30 @@ class OverflowDetector {
         fun <I : Integer<I>> assertNoOverflow(n1: I, n2: I, operation: (m1: Integer<*>, m2: Integer<*>) -> Integer<*>) {
             val originalResult: Integer<*> = operation(n1, n2) // as I
             val bigIntegerResult: Integer<*> = operation(n1.toWrappedBigInteger(), n2.toWrappedBigInteger()) // as WrappedBigInteger
+            if (originalResult.toBigInteger() != bigIntegerResult.toBigInteger()) {
+                throw ArithmeticException("Overflow!")
+            }
+        }
+
+        fun <I : Integer<I>> assertNoOverflow(
+            n1: I, n2: I, n3: I,
+            operation: (m1: Integer<*>, m2: Integer<*>, m3: Integer<*>) -> Integer<*>) {
+            val originalResult: Integer<*> = operation(n1, n2, n3) // as I
+            val bigIntegerResult: Integer<*> = operation(
+                n1.toWrappedBigInteger(), n2.toWrappedBigInteger(), n3.toWrappedBigInteger()
+            ) // as WrappedBigInteger
+            if (originalResult.toBigInteger() != bigIntegerResult.toBigInteger()) {
+                throw ArithmeticException("Overflow!")
+            }
+        }
+
+        fun <I : Integer<I>> assertNoOverflow(
+            n1: I, n2: I, n3: I, n4: I,
+            operation: (m1: Integer<*>, m2: Integer<*>, m3: Integer<*>, m4: Integer<*>) -> Integer<*>) {
+            val originalResult: Integer<*> = operation(n1, n2, n3, n4) // as I
+            val bigIntegerResult: Integer<*> = operation(
+                n1.toWrappedBigInteger(), n2.toWrappedBigInteger(), n3.toWrappedBigInteger(), n4.toWrappedBigInteger()
+            ) // as WrappedBigInteger
             if (originalResult.toBigInteger() != bigIntegerResult.toBigInteger()) {
                 throw ArithmeticException("Overflow!")
             }

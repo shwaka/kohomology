e7dba53a53ef1f683f84b2bb0f570d0a54683f39
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-19 22:12:24 +0900

    add tests for OverflowDetector

--- kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/field/OverflowDetectorTest.kt
+++ kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/field/OverflowDetectorTest.kt
@@ -10,16 +10,35 @@ val overflowTag = NamedTag("Overflow")
 class OverflowDetectorTest : StringSpec({
     tags(overflowTag)
 
-    "Int.MAX_VALUE + 1 should cause overflow" {
+    "Int.MAX_VALUE + 1 should cause overflow for Int" {
         shouldThrow<ArithmeticException> {
             OverflowDetector.assertNoOverflow(Int.MAX_VALUE, 1) { x, y -> x + y }
         }
     }
 
-    "Int.MAX_VALUE + 0 and Int.MAX_VALUE - 1 should not cause overflow" {
+    "Int.MAX_VALUE + 0 and Int.MAX_VALUE - 1 should not cause overflow for Int" {
         shouldNotThrowAny {
             OverflowDetector.assertNoOverflow(Int.MAX_VALUE, 0) { x, y -> x + y }
             OverflowDetector.assertNoOverflow(Int.MAX_VALUE, -1) { x, y -> x + y }
+        }
+    }
+
+    "Long.MAX_VALUE + 1 should cause overflow for Long" {
+        shouldThrow<ArithmeticException> {
+            OverflowDetector.assertNoOverflow(Long.MAX_VALUE, 1L) { x, y -> x + y }
+        }
+    }
+
+    "Long.MAX_VALUE + 0 and Long.MAX_VALUE - 1 should not cause overflow for Long" {
+        shouldNotThrowAny {
+            OverflowDetector.assertNoOverflow(Long.MAX_VALUE, 0L) { x, y -> x + y }
+            OverflowDetector.assertNoOverflow(Long.MAX_VALUE, -1L) { x, y -> x + y }
+        }
+    }
+
+    "Int.MAX_VALUE + Int.MAX_VALUE should not cause overflow for Long" {
+        shouldNotThrowAny {
+            OverflowDetector.assertNoOverflow(Int.MAX_VALUE.toLong(), Int.MAX_VALUE.toLong()) { x, y -> x + y }
         }
     }
 })

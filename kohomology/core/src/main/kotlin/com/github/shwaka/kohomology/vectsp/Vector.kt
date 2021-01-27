91415932efd7fb6d9ebd2759774a3475b57c0893
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-27 10:50:08 +0900

    add Vector.kt

---
+++ kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/vectsp/Vector.kt
@@ -0,0 +1,62 @@
+package com.github.shwaka.kohomology.vectsp
+
+import com.github.shwaka.kohomology.field.Scalar
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.NumVectorSpace
+
+class Vector<B, S : Scalar<S>, V : NumVector<S, V>>(private val numVector: V, val vectorSpace: VectorSpace<B, S, V>) {
+    operator fun plus(other: Vector<B, S, V>): Vector<B, S, V> {
+        return Vector(this.numVector + other.numVector, this.vectorSpace)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        if (this::class != other::class) return false
+
+        other as Vector<*, *, *>
+
+        if (numVector != other.numVector) return false
+        if (vectorSpace != other.vectorSpace) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = numVector.hashCode()
+        result = 31 * result + vectorSpace.hashCode()
+        return result
+    }
+}
+
+class VectorSpace<B, S : Scalar<S>, V : NumVector<S, V>>(
+    val numVectorSpace: NumVectorSpace<S, V>,
+    val dim: Int,
+    val basis: List<B>
+) {
+    fun fromNumVector(numVector: V): Vector<B, S, V> {
+        return Vector(numVector, this)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        if (this::class != other::class) return false
+
+        other as VectorSpace<*, *, *>
+
+        if (numVectorSpace != other.numVectorSpace) return false
+        if (dim != other.dim) return false
+        if (basis != other.basis) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = numVectorSpace.hashCode()
+        result = 31 * result + dim
+        result = 31 * result + basis.hashCode()
+        return result
+    }
+
+}
---
+++ kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/vectsp/VectorTest.kt
@@ -0,0 +1,35 @@
+package com.github.shwaka.kohomology.vectsp
+
+import com.github.shwaka.kohomology.bigRationalTag
+import com.github.shwaka.kohomology.field.BigRationalField
+import com.github.shwaka.kohomology.field.Scalar
+import com.github.shwaka.kohomology.linalg.DenseNumVectorSpace
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.NumVectorSpace
+import io.kotest.core.NamedTag
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.stringSpec
+import io.kotest.matchers.shouldBe
+
+val vectorTag = NamedTag("Vector")
+
+fun <S : Scalar<S>, V : NumVector<S, V>> vectorTest(numVectorSpace: NumVectorSpace<S, V>) = stringSpec {
+    val field = numVectorSpace.field
+    val zero = field.zero
+    val one = field.one
+    val two = field.fromInt(2)
+    "addition of Vector" {
+        val vectorSpace = VectorSpace(numVectorSpace, 3, listOf("a", "b", "c"))
+        val numVector = numVectorSpace.fromValues(one, zero, one)
+        val v = vectorSpace.fromNumVector(numVector)
+        val expected = vectorSpace.fromNumVector(numVectorSpace.fromValues(two, zero, two))
+        (v + v) shouldBe expected
+    }
+}
+
+class BigRationalVectorTest : StringSpec({
+    tags(vectorTag, bigRationalTag)
+
+    val numVectorSpace = DenseNumVectorSpace.from(BigRationalField)
+    include(vectorTest(numVectorSpace))
+})

0673f269478670a7ce8fa4c011d8f9d572e4a839
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-01-29 12:32:19 +0900

    ktlintFormat

--- kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/vectsp/GVectorTest.kt
+++ kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/vectsp/GVectorTest.kt
@@ -3,9 +3,9 @@ package com.github.shwaka.kohomology.vectsp
 import com.github.shwaka.kohomology.bigRationalTag
 import com.github.shwaka.kohomology.field.BigRationalField
 import com.github.shwaka.kohomology.field.Scalar
+import com.github.shwaka.kohomology.linalg.DenseNumVectorSpace
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.NumVectorSpace
-import com.github.shwaka.kohomology.linalg.DenseNumVectorSpace
 import io.kotest.core.NamedTag
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.core.spec.style.stringSpec

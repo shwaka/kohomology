94c2f06f14bda2c91550c31eb91ba679a90e714c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-02 00:48:25 +0900

    improve summary of faliures

--- kohomology/buildSrc/src/main/kotlin/com/github/shwaka/kohomology/MyTestListener.kt
+++ kohomology/buildSrc/src/main/kotlin/com/github/shwaka/kohomology/MyTestListener.kt
@@ -15,23 +15,42 @@ import org.gradle.api.tasks.testing.TestResult
 //     MyTestListener.printSummary(logger)
 // }
 
+data class FailedTest(
+    val className: String?,
+    val name: String
+) {
+    fun format(): String {
+        return "[${this.shortClassName}] ${this.name}"
+    }
+
+    private val shortClassName: String
+        get() {
+            val splitClassName: List<String> = this.className?.split(".") ?: listOf("null")
+            return if (splitClassName.isNotEmpty()) {
+                splitClassName.last()
+            } else {
+                "null"
+            }
+        }
+}
+
 object MyTestListener : TestListener {
-    private val failedTests: MutableList<String> = mutableListOf()
+    private val failedTests: MutableList<FailedTest> = mutableListOf()
 
     override fun beforeSuite(suite: TestDescriptor?) = Unit
     override fun afterSuite(suite: TestDescriptor?, result: TestResult?) = Unit
     override fun beforeTest(testDescriptor: TestDescriptor?) = Unit
     override fun afterTest(testDescriptor: TestDescriptor?, result: TestResult?) {
         if (result?.resultType == TestResult.ResultType.FAILURE) {
-            val message = testDescriptor!!.className + "\n  " + testDescriptor.name
-            this.failedTests.add(message)
+            val failedTest = FailedTest(testDescriptor!!.className, testDescriptor.name)
+            this.failedTests.add(failedTest)
         }
     }
 
     fun printSummary(logger: Logger) {
         if (failedTests.isNotEmpty()) {
             logger.error("==== FAILED TESTS ====")
-            failedTests.forEach { logger.error(it) }
+            failedTests.forEach { logger.error(it.format()) }
         }
     }
 }

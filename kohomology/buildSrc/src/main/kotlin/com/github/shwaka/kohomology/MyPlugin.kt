d905d79b91315da7e2669642bbaf70af5df76610
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-02 13:47:06 +0900

    improve summary of failures

--- kohomology/buildSrc/src/main/kotlin/com/github/shwaka/kohomology/MyPlugin.kt
+++ kohomology/buildSrc/src/main/kotlin/com/github/shwaka/kohomology/MyPlugin.kt
@@ -29,8 +29,8 @@ class MyPlugin : Plugin<Project> {
 }
 
 data class FailedTest(
-    val className: String?,
-    val name: String?
+    val className: String,
+    val name: String
 ) {
     fun format(): String {
         return "[${this.shortClassName}] ${this.name}"
@@ -38,7 +38,7 @@ data class FailedTest(
 
     private val shortClassName: String
         get() {
-            val splitClassName: List<String> = this.className?.split(".") ?: listOf("null")
+            val splitClassName: List<String> = this.className.split(".")
             return if (splitClassName.isNotEmpty()) {
                 splitClassName.last()
             } else {
@@ -51,22 +51,31 @@ class MyTestListener : TestListener {
     // class ではなく object にすると、gradle の実行毎に object が再生成されず、
     // そのまま再利用されてしまうっぽい。
     // class にして自分で constructor を呼ぶなら大丈夫。
-    private val failedTests: MutableList<FailedTest> = mutableListOf()
+    private val failedTests: MutableMap<String, MutableList<FailedTest>> = mutableMapOf()
 
     override fun beforeSuite(suite: TestDescriptor?) = Unit
     override fun afterSuite(suite: TestDescriptor?, result: TestResult?) = Unit
     override fun beforeTest(testDescriptor: TestDescriptor?) = Unit
     override fun afterTest(testDescriptor: TestDescriptor?, result: TestResult?) {
         if (result?.resultType == TestResult.ResultType.FAILURE) {
-            val failedTest = FailedTest(testDescriptor?.className, testDescriptor?.name)
-            this.failedTests.add(failedTest)
+            val className: String = testDescriptor?.className ?: "null"
+            val name: String = testDescriptor?.name ?: "no name"
+            val failedTest = FailedTest(className, name)
+            this.failedTests
+                .getOrPut(className, { mutableListOf() })
+                .add(failedTest)
         }
     }
 
     fun printSummary(logger: Logger) {
-        if (failedTests.isNotEmpty()) {
+        if (this.failedTests.isNotEmpty()) {
             logger.error("==== FAILED TESTS ====")
-            failedTests.forEach { logger.error(it.format()) }
+            for ( (className, failedTestList) in this.failedTests) {
+                println(className)
+                for (failedTest in failedTestList) {
+                    println("  ${failedTest.name}")
+                }
+            }
         }
     }
 }

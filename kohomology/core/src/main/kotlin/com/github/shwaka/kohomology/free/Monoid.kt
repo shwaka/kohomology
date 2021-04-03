e4bb0a5952a8a1ab0f571154165d9b56f1ae9dbc
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-03 12:23:24 +0900

    add MonoidFromList and test for the cohomology of complex projective spaces

--- kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/free/Monoid.kt
+++ kohomology/core/src/main/kotlin/com/github/shwaka/kohomology/free/Monoid.kt
@@ -27,3 +27,42 @@ interface Monoid<E : MonoidElement> {
     fun multiply(monoidElement1: E, monoidElement2: E): MaybeZero<Pair<E, Sign>>
     fun listAll(degree: Degree): List<E>
 }
+
+data class SimpleMonoidElement<T>(val name: T, override val degree: Degree) : MonoidElement {
+    override fun toString(): String {
+        return this.name.toString()
+    }
+}
+
+class MonoidFromList<T>(
+    val elements: List<SimpleMonoidElement<T>>,
+    val multiplicationTable: List<List<MaybeZero<Pair<SimpleMonoidElement<T>, Sign>>>>
+) : Monoid<SimpleMonoidElement<T>> {
+    init {
+        if (this.elements.isEmpty())
+            throw IllegalArgumentException("'elements' must be non-empty list")
+        if (this.elements[0].degree != 0)
+            throw IllegalArgumentException("The first element of the list 'elements' should be the unit (degree 0)")
+    }
+
+    override val unit = this.elements[0]
+
+    override fun multiply(
+        monoidElement1: SimpleMonoidElement<T>,
+        monoidElement2: SimpleMonoidElement<T>
+    ): MaybeZero<Pair<SimpleMonoidElement<T>, Sign>> {
+        val index1: Int = this.elements.indexOf(monoidElement1).also {
+            if (it == -1)
+                throw IllegalArgumentException("$monoidElement1 is not found in the list 'elements'")
+        }
+        val index2: Int = this.elements.indexOf(monoidElement2).also {
+            if (it == -1)
+                throw IllegalArgumentException("$monoidElement2 is not found in the list 'elements'")
+        }
+        return this.multiplicationTable[index1][index2]
+    }
+
+    override fun listAll(degree: Degree): List<SimpleMonoidElement<T>> {
+        return this.elements.filter { it.degree == degree }
+    }
+}
---
+++ kohomology/core/src/test/kotlin/com/github/shwaka/kohomology/free/MonoidGAlgebraTest.kt
@@ -0,0 +1,64 @@
+package com.github.shwaka.kohomology.free
+
+import com.github.shwaka.kohomology.bigRationalTag
+import com.github.shwaka.kohomology.dg.GVector
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.MatrixSpace
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.specific.DenseMatrixSpaceOverBigRational
+import com.github.shwaka.kohomology.util.Sign
+import io.kotest.core.NamedTag
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.stringSpec
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.shouldBe
+
+val monoidGAlgebraTag = NamedTag("MonoidGAlgebra")
+
+fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> complexProjectiveSpaceTest(
+    matrixSpace: MatrixSpace<S, V, M>,
+    n: Int
+) = stringSpec {
+    if (n < 0)
+        throw IllegalArgumentException("Invalid test parameter: n must be non-negative")
+    "complex projective space of complex dimension $n" {
+        val elements = (0..n).map { i -> SimpleMonoidElement("c$i", 2 * i) }
+        val multiplicationTable: List<List<MaybeZero<Pair<SimpleMonoidElement<String>, Sign>>>> =
+            (0..n).map { i ->
+                (0..n).map { j ->
+                    if (i + j <= n) {
+                        NonZero(Pair(elements[i + j], 1))
+                    } else {
+                        Zero()
+                    }
+                }
+            }
+        val monoid = MonoidFromList(elements, multiplicationTable)
+        val gAlgebra = MonoidGAlgebra(matrixSpace, monoid)
+        for (degree in 0..(3 * n)) {
+            val expectedDim = if ((degree <= 2 * n) && (degree % 2 == 0)) 1 else 0
+            gAlgebra[degree].dim shouldBe expectedDim
+        }
+        val basis: List<GVector<SimpleMonoidElement<String>, S, V>> =
+            (0..n).map { i -> gAlgebra.getBasis(2 * i)[0] }
+        gAlgebra.withGAlgebraContext {
+            for (i in 0..n) {
+                for (j in 0..n) {
+                    if (i + j <= n) {
+                        (basis[i] * basis[j]) shouldBe basis[i + j]
+                    } else {
+                        (basis[i] * basis[j]).isZero().shouldBeTrue()
+                    }
+                }
+            }
+        }
+    }
+}
+
+class MonoidGAlgebraTest : StringSpec({
+    tags(monoidGAlgebraTag, bigRationalTag)
+
+    include(complexProjectiveSpaceTest(DenseMatrixSpaceOverBigRational, 5))
+})

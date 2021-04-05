d11f157b4d42dc9f01a40677799998e142396deb
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-06 02:21:34 +0900

    add kohomology-app

---
+++ kohomology-app/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "kohomology-app"
+
---
+++ kohomology-app/src/main/kotlin/main.kt
@@ -0,0 +1,46 @@
+import com.github.shwaka.kohomology.linalg.Field
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.specific.Fp
+import com.github.shwaka.kohomology.specific.IntRational
+import com.github.shwaka.kohomology.specific.IntRationalField
+
+fun <S : Scalar> add(field: Field<S>, a: S, b: S): S {
+    // return a.field.wrap(a + b.unwrap())
+    return field.withContext { a + b }
+}
+
+fun <S : Scalar> test(field: Field<S>, a: S, b: S) {
+    field.withContext {
+        println("------------------------------")
+        println("test: a=$a, b=$b")
+        println("a + b = ${a + b}")
+        println("a - b = ${a - b}")
+        println("a * b = ${a * b}")
+        println("------------------------------")
+    }
+}
+
+fun main() {
+    println("Hello world!")
+    IntRationalField.withContext {
+        println(IntRational(1, 2) + IntRational(1, 3))
+        println(IntRational(1, 3) + IntRational(-2, 6))
+    }
+
+    val a = IntRational(1, 2)
+    val b = IntRationalField.fromInt(2)
+    println(add(IntRationalField, a, b))
+    test(IntRationalField, a, b)
+
+    val p = 5
+    val field = Fp.get(p)
+    field.withContext {
+        val c = three
+        val d = one
+        println(add(field, c, d))
+        field.withContext {
+            println(add(field, c, d + one))
+        }
+        test(field, c, d)
+    }
+}

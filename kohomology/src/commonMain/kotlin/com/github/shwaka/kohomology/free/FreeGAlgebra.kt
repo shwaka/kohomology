74de49fa97cd33d36bac4a69c68f80f651c3074a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-07 19:23:14 +0900

    remove FreeGAlgebraFactory (which is unused, since MonoidGAlgebraFactory is sufficient)

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/FreeGAlgebra.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/FreeGAlgebra.kt
@@ -9,47 +9,6 @@ import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.util.Degree
-import com.github.shwaka.kohomology.util.Sign
-import com.github.shwaka.kohomology.vectsp.BilinearMap
-import com.github.shwaka.kohomology.vectsp.Vector
-import com.github.shwaka.kohomology.vectsp.VectorSpace
-
-private class FreeGAlgebraFactory<I, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
-    private val matrixSpace: MatrixSpace<S, V, M>,
-    val indeterminateList: List<Indeterminate<I>>,
-) {
-    val monoid = FreeMonoid(indeterminateList)
-
-    private fun getBasisNames(degree: Degree): List<Monomial<I>> {
-        return this.monoid.listAll(degree)
-    }
-
-    fun getVectorSpace(degree: Degree): VectorSpace<Monomial<I>, S, V> {
-        return VectorSpace(this.matrixSpace.numVectorSpace, this.getBasisNames(degree))
-    }
-
-    fun getMultiplication(p: Degree, q: Degree): BilinearMap<Monomial<I>, Monomial<I>, Monomial<I>, S, V, M> {
-        val source1 = this.getVectorSpace(p)
-        val source2 = this.getVectorSpace(q)
-        val target = this.getVectorSpace(p + q)
-        val values = source1.basisNames.map { monomial1 ->
-            source2.basisNames.map { monomial2 ->
-                this.monoid.multiply(monomial1, monomial2).let { monomialOrZero ->
-                    when (monomialOrZero) {
-                        is Zero -> target.zeroVector
-                        is NonZero -> {
-                            val (monomial: Monomial<I>, sign: Sign) = monomialOrZero.value
-                            target.fromBasisName(monomial, sign)
-                        }
-                    }
-                }
-            }
-        }
-        return BilinearMap.fromVectors(source1, source2, target, this.matrixSpace, values)
-    }
-
-    val unitVector: Vector<Monomial<I>, S, V> = this.getVectorSpace(0).getBasis()[0]
-}
 
 class FreeGAlgebra<I, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
     matrixSpace: MatrixSpace<S, V, M>,
@@ -135,7 +94,7 @@ class FreeGAlgebra<I, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
         }
         return GLinearMap.fromGVectors(this, target, 0, this.matrixSpace) { k ->
             val sourceVectorSpace = this[k]
-            val targetVectorSpace = target[k]
+            // val targetVectorSpace = target[k]
             sourceVectorSpace.basisNames.map { monomial: Monomial<I> ->
                 val gVectorValue = this.getAlgebraMapValue(target, gVectorValueList, monomial)
                 gVectorValue

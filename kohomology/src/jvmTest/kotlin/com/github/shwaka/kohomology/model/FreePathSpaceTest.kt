cae32fb2acc0c69b683bd7247a305a1d3145cfd4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-07 23:04:35 +0900

    add model of CP^n to FreePathSpaceTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/model/FreePathSpaceTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/model/FreePathSpaceTest.kt
@@ -35,16 +35,50 @@ fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> freePathSpaceOfEvenSphereTe
     val freePathSpace = FreePathSpace(sphere)
     val (x1, y1, x2, y2, sx, sy) = freePathSpace.gAlgebra.generatorList
 
-    "[dim=$sphereDim] check differential" {
+    "[S^$sphereDim] check differential" {
         freePathSpace.context.run {
             d(sx) shouldBe (x2 - x1)
             d(sy) shouldBe (y2 - y1 - (x2 + x1) * sx)
         }
     }
-    "[dim=$sphereDim] check that inclusions are isomorphism on cohomology" {
+    "[S^$sphereDim] check that inclusions are isomorphism on cohomology" {
         val cohomologyInclusion1 = freePathSpace.inclusion1.inducedMapOnCohomology()
         val cohomologyInclusion2 = freePathSpace.inclusion2.inducedMapOnCohomology()
         for (degree in 0 until sphereDim * 5) {
+            cohomologyInclusion1.getLinearMap(degree).isIsomorphism().shouldBeTrue()
+            cohomologyInclusion2.getLinearMap(degree).isIsomorphism().shouldBeTrue()
+        }
+    }
+}
+
+fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> freePathSpaceOfCPnTest(
+    matrixSpace: MatrixSpace<S, V, M>,
+    n: Int
+) = stringSpec {
+    if (n <= 0)
+        throw IllegalArgumentException("The complex dimension n of CP^n must be positive")
+    val indeterminateList = listOf(
+        Indeterminate("c", 2),
+        Indeterminate("x", 2 * n + 1)
+    )
+    val sphere = FreeDGAlgebra(matrixSpace, indeterminateList) { (c, _) ->
+        listOf(zeroGVector, c.pow(n + 1))
+    }
+    val freePathSpace = FreePathSpace(sphere)
+    val (c1, x1, c2, x2, sc, sx) = freePathSpace.gAlgebra.generatorList
+
+    "[CP^$n] check differential" {
+        freePathSpace.context.run {
+            d(sc) shouldBe (c2 - c1)
+            val dsxExpected = x2 - x1 -
+                (0..n).map { i -> c1.pow(i) * c2.pow(n - i) * sc }.reduce { a, b -> a + b }
+            d(sx) shouldBe dsxExpected
+        }
+    }
+    "[CP^$n] check that inclusions are isomorphism on cohomology" {
+        val cohomologyInclusion1 = freePathSpace.inclusion1.inducedMapOnCohomology()
+        val cohomologyInclusion2 = freePathSpace.inclusion2.inducedMapOnCohomology()
+        for (degree in 0 until 2 * n + 4) {
             cohomologyInclusion1.getLinearMap(degree).isIsomorphism().shouldBeTrue()
             cohomologyInclusion2.getLinearMap(degree).isIsomorphism().shouldBeTrue()
         }
@@ -55,4 +89,5 @@ class FreePathSpaceTest : StringSpec({
     tags(freePathSpaceTag, bigRationalTag)
 
     include(freePathSpaceOfEvenSphereTest(DenseMatrixSpaceOverBigRational, 2))
+    include(freePathSpaceOfCPnTest(DenseMatrixSpaceOverBigRational, 4))
 })

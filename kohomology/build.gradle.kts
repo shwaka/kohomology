f60fc9318b661d2875a1e607167f7d506c8ac981
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-10 00:20:50 +0900

    add simple-counter

--- kohomology/build.gradle.kts
+++ kohomology/build.gradle.kts
@@ -66,6 +66,7 @@ kotlin {
                     implementation(kococoDebug)
                 }
 
+                implementation("com.github.shwaka.counter:simple-counter:0.1")
                 implementation("io.github.microutils:kotlin-logging:2.0.6")
             }
         }

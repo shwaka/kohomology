b0396da26e8ec9e82884b085431d037555676646
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-19 18:53:36 +0900

    ktlintFormat

--- kohomology/src/jvmMain/kotlin/com/github/shwaka/kohomology/util/Parallel.kt
+++ kohomology/src/jvmMain/kotlin/com/github/shwaka/kohomology/util/Parallel.kt
@@ -1,6 +1,8 @@
 package com.github.shwaka.kohomology.util
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 
 actual fun <T, R> Iterable<T>.pmap(transform: (T) -> R): List<R> = runBlocking {
     this@pmap.map { async(Dispatchers.Default) { transform(it) } }.map { it.await() }

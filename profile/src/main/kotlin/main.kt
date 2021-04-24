1bacb1ee9257bd1588a177d2b2cbe02993bd497d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-24 20:18:04 +0900

    refactoring in profile

--- profile/src/main/kotlin/main.kt
+++ profile/src/main/kotlin/main.kt
@@ -4,26 +4,43 @@ import com.github.shwaka.kohomology.model.FreeLoopSpace
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverBigRational
 
 fun main() {
+    val scriptList: List<ProfiledScript> = listOf(CohomologyOfFreeLoopSpace)
+    println("Select script to profile: (default = 0)")
+    scriptList.mapIndexed { index, script ->
+        println("$index: ${script.description}")
+    }
+    val index = readLine()?.toIntOrNull() ?: 0
+    val script = scriptList[index]
+    script.setup()
     print("Press ENTER to continue!!!")
     readLine()
-    cohomologyOfFreeLoopSpace()
+    script.main()
 }
 
-fun cohomologyOfFreeLoopSpace(): String {
-    val sphereDim = 2
-    val indeterminateList = listOf(
-        Indeterminate("x", sphereDim),
-        Indeterminate("y", sphereDim * 2 - 1)
-    )
-    val matrixSpace = SparseMatrixSpaceOverBigRational
-    val sphere = FreeDGAlgebra(matrixSpace, indeterminateList) { (x, _) ->
-        listOf(zeroGVector, x.pow(2))
-    }
-    val freeLoopSpace = FreeLoopSpace(sphere)
+interface ProfiledScript {
+    val description: String
+    fun setup(){}
+    fun main(): String
+}
 
-    var result = ""
-    for (degree in 0 until 150) {
-        result += freeLoopSpace.cohomology[degree].toString() + "\n"
+object CohomologyOfFreeLoopSpace : ProfiledScript {
+    override val description = "cohomology of free loop space of 2-sphere"
+    override fun main(): String {
+        val sphereDim = 2
+        val indeterminateList = listOf(
+            Indeterminate("x", sphereDim),
+            Indeterminate("y", sphereDim * 2 - 1)
+        )
+        val matrixSpace = SparseMatrixSpaceOverBigRational
+        val sphere = FreeDGAlgebra(matrixSpace, indeterminateList) { (x, _) ->
+            listOf(zeroGVector, x.pow(2))
+        }
+        val freeLoopSpace = FreeLoopSpace(sphere)
+
+        var result = ""
+        for (degree in 0 until 150) {
+            result += freeLoopSpace.cohomology[degree].toString() + "\n"
+        }
+        return result
     }
-    return result
 }

6dcf1b5fc855db4717d0c30ee9376193e8a3b8cb
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-05-01 13:09:56 +0900

    move isomorphismToCohomologyOfFreePathSpace to Executable.kt

--- profile/src/main/kotlin/com/github/shwaka/kohomology/profile/Executable.kt
+++ profile/src/main/kotlin/com/github/shwaka/kohomology/profile/Executable.kt
@@ -11,6 +11,7 @@ import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.model.FreeLoopSpace
+import com.github.shwaka.kohomology.model.FreePathSpace
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverBigRational
 
 abstract class Executable {
@@ -81,6 +82,27 @@ class CohomologyOfFreeLoopSpaceWithLinearDegree(
                 val degree = 2 * n + i * (2 * n - 1)
                 result += freeLoopSpace.cohomology[degree].toString() + "\n"
             }
+        }
+        return result
+    }
+}
+
+class IsomorphismToCohomologyOfFreePathSpace(val n: Int, val degreeLimit: Int) : Executable() {
+    override val description: String = "cohomology of the free path space of CP^n"
+    override fun mainFun(): String {
+        val indeterminateList = listOf(
+            GeneralizedIndeterminate("c", 2),
+            GeneralizedIndeterminate("x", 2 * this.n + 1)
+        )
+        val sphere = FreeDGAlgebra(SparseMatrixSpaceOverBigRational, indeterminateList) { (c, _) ->
+            listOf(zeroGVector, c.pow(this@IsomorphismToCohomologyOfFreePathSpace.n + 1))
+        }
+        val freePathSpace = FreePathSpace(sphere)
+
+        val cohomologyInclusion1 = freePathSpace.inclusion1.inducedMapOnCohomology()
+        var result = ""
+        for (degree in 0 until this.degreeLimit) {
+            result += cohomologyInclusion1[degree].isIsomorphism().toString()
         }
         return result
     }
--- profile/src/main/kotlin/com/github/shwaka/kohomology/profile/KohomologyBenchmark.kt
+++ profile/src/main/kotlin/com/github/shwaka/kohomology/profile/KohomologyBenchmark.kt
@@ -1,9 +1,5 @@
 package com.github.shwaka.kohomology.profile
 
-import com.github.shwaka.kohomology.free.FreeDGAlgebra
-import com.github.shwaka.kohomology.free.GeneralizedIndeterminate
-import com.github.shwaka.kohomology.model.FreePathSpace
-import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverBigRational
 import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
@@ -22,21 +18,6 @@ class KohomologyBenchmark {
 
     @Benchmark
     fun isomorphismToCohomologyOfFreePathSpace(): String {
-        val n = 5
-        val indeterminateList = listOf(
-            GeneralizedIndeterminate("c", 2),
-            GeneralizedIndeterminate("x", 2 * n + 1)
-        )
-        val sphere = FreeDGAlgebra(SparseMatrixSpaceOverBigRational, indeterminateList) { (c, _) ->
-            listOf(zeroGVector, c.pow(n + 1))
-        }
-        val freePathSpace = FreePathSpace(sphere)
-
-        val cohomologyInclusion1 = freePathSpace.inclusion1.inducedMapOnCohomology()
-        var result = ""
-        for (degree in 0 until 35) {
-            result += cohomologyInclusion1[degree].isIsomorphism().toString()
-        }
-        return result
+        return IsomorphismToCohomologyOfFreePathSpace(n = 5, degreeLimit = 35).main()
     }
 }

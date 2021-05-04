b36e619e7fdbca414433b339008381dc3ad88d1a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-05-05 00:08:36 +0900

    fix IntDegreeTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/degree/IntDegreeTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/degree/IntDegreeTest.kt
@@ -2,11 +2,11 @@ package com.github.shwaka.kohomology.dg.degree
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.property.Arb
-import io.kotest.property.arbitrary.bind
 import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.map
 
 fun IntDegreeGroup.arb(intArb: Arb<Int> = Arb.int(Int.MIN_VALUE..Int.MAX_VALUE)): Arb<IntDegree> {
-    return Arb.bind(intArb) { n -> IntDegree(n) }
+    return intArb.map { n -> IntDegree(n) }
 }
 
 class IntDegreeTest : FreeSpec({

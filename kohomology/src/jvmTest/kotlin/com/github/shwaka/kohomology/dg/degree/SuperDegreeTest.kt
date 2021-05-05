0534b1784ab20add62bfd888df3d01f3b7f3e8ce
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-05-05 17:55:58 +0900

    improve SuperDegreeTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/degree/SuperDegreeTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/degree/SuperDegreeTest.kt
@@ -1,6 +1,7 @@
 package com.github.shwaka.kohomology.dg.degree
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.bool
 import io.kotest.property.arbitrary.map
@@ -16,5 +17,14 @@ class SuperDegreeTest : FreeSpec({
 
     "SuperDegreeTest" - {
         degreeTestTemplate(SuperDegreeGroup, SuperDegreeGroup.arb())
+
+        SuperDegreeGroup.context.run {
+            "1 + 1 should be 0" {
+                (OddSuperDegree + OddSuperDegree) shouldBe EvenSuperDegree
+            }
+            "1 * 2 should be 0" {
+                (OddSuperDegree * 2) shouldBe EvenSuperDegree
+            }
+        }
     }
 })

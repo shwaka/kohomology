e0c392013a93f3a5f4abb055b9b7694b82e0c314
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-05-05 11:04:42 +0900

    improve DegreeTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/degree/DegreeTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/degree/DegreeTest.kt
@@ -1,22 +1,49 @@
 package com.github.shwaka.kohomology.dg.degree
 
 import io.kotest.core.spec.style.scopes.FreeScope
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
 import io.kotest.property.checkAll
 
-suspend inline fun <D : Degree> FreeScope.degreeTestTemplate(degreeGroup: DegreeGroup<D>, degreeArb: Arb<D>) {
+suspend inline fun <D : Degree> FreeScope.degreeTestTemplate(
+    degreeGroup: DegreeGroup<D>,
+    degreeArb: Arb<D>,
+    intArb: Arb<Int> = Arb.int(Int.MIN_VALUE..Int.MAX_VALUE)
+) {
     "Requirements for $degreeGroup" - {
         degreeGroup.context.run {
-            "zero should be the unit of addition" {
-                checkAll(degreeArb) { a ->
-                    (a + zero) shouldBe a
-                    (zero + a) shouldBe a
+            "group axioms" - {
+                "zero should be the unit of addition" {
+                    checkAll(degreeArb) { a ->
+                        (a + zero) shouldBe a
+                        (zero + a) shouldBe a
+                    }
+                }
+                "addition should be associative" {
+                    checkAll(degreeArb, degreeArb, degreeArb) { a, b, c ->
+                        ((a + b) + c) shouldBe (a + (b + c))
+                    }
+                }
+                "subtraction (a - a) should be zero" {
+                    checkAll(degreeArb) { a ->
+                        (a - a) shouldBe zero
+                    }
                 }
             }
-            "addition should be associative" {
-                checkAll(degreeArb, degreeArb, degreeArb) { a, b, c ->
-                    ((a + b) + c) shouldBe (a + (b + c))
+            "fromInt() should be a group homomorphism" {
+                checkAll(intArb, intArb) { n, m ->
+                    fromInt(n + m) shouldBe (fromInt(n) + fromInt(m))
+                }
+            }
+            "zero.isZero() should be true" {
+                zero.isZero().shouldBeTrue()
+            }
+            "(degree.isZero() == true) implies (degree == zero)" {
+                checkAll(degreeArb) { a ->
+                    if (a.isZero())
+                        a shouldBe zero
                 }
             }
         }

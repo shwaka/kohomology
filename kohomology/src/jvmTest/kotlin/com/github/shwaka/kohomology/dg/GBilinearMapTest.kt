1643c5b9c9b8f2b31a1d7f04f9f862656acc5600
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-05-06 18:33:50 +0900

    use nested test in GBilinearMapTest.kt

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/GBilinearMapTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/GBilinearMapTest.kt
@@ -18,41 +18,46 @@ import io.kotest.matchers.shouldBe
 val gBilinearMapTag = NamedTag("GBilinearMap")
 
 fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> gBilinearMapTest(matrixSpace: MatrixSpace<S, V, M>) = freeSpec {
-    // 1つの元で生成される外積代数
-    val numVectorSpace = matrixSpace.numVectorSpace
-    val gVectorSpace = GVectorSpace.fromStringBasisNamesWithIntDegree(numVectorSpace, "span<u, v>") { degree ->
-        when (degree) {
-            0 -> listOf("u") // unit
-            1 -> listOf("v")
-            else -> emptyList()
+    "graded bilinear map test" - {
+        // 1つの元で生成される外積代数
+        val numVectorSpace = matrixSpace.numVectorSpace
+        val gVectorSpace = GVectorSpace.fromStringBasisNamesWithIntDegree(numVectorSpace, "span<u, v>") { degree ->
+            when (degree) {
+                0 -> listOf("u") // unit
+                1 -> listOf("v")
+                else -> emptyList()
+            }
         }
-    }
-    val gBilinearMap = GBilinearMap.withIntDegree(gVectorSpace, gVectorSpace, gVectorSpace, 0, "f") { p, q ->
-        val u: Vector<StringBasisName, S, V> = gVectorSpace[0].getBasis()[0]
-        val v: Vector<StringBasisName, S, V> = gVectorSpace[1].getBasis()[0]
-        val z2: Vector<StringBasisName, S, V> = gVectorSpace[2].zeroVector
-        when (Pair(p, q)) {
-            Pair(0, 0) -> BilinearMap(gVectorSpace[0], gVectorSpace[0], gVectorSpace[0], matrixSpace, listOf(listOf(u)))
-            Pair(1, 0) -> BilinearMap(gVectorSpace[1], gVectorSpace[0], gVectorSpace[1], matrixSpace, listOf(listOf(v)))
-            Pair(0, 1) -> BilinearMap(gVectorSpace[0], gVectorSpace[1], gVectorSpace[1], matrixSpace, listOf(listOf(v)))
-            Pair(1, 1) -> BilinearMap(gVectorSpace[1], gVectorSpace[1], gVectorSpace[2], matrixSpace, listOf(listOf(z2)))
-            else -> BilinearMap(gVectorSpace[p], gVectorSpace[q], gVectorSpace[p + q], matrixSpace, emptyList())
+        val gBilinearMap = GBilinearMap.withIntDegree(gVectorSpace, gVectorSpace, gVectorSpace, 0, "f") { p, q ->
+            val u: Vector<StringBasisName, S, V> = gVectorSpace[0].getBasis()[0]
+            val v: Vector<StringBasisName, S, V> = gVectorSpace[1].getBasis()[0]
+            val z2: Vector<StringBasisName, S, V> = gVectorSpace[2].zeroVector
+            when (Pair(p, q)) {
+                Pair(0, 0) -> BilinearMap(gVectorSpace[0], gVectorSpace[0], gVectorSpace[0], matrixSpace, listOf(listOf(u)))
+                Pair(1, 0) -> BilinearMap(gVectorSpace[1], gVectorSpace[0], gVectorSpace[1], matrixSpace, listOf(listOf(v)))
+                Pair(0, 1) -> BilinearMap(gVectorSpace[0], gVectorSpace[1], gVectorSpace[1], matrixSpace, listOf(listOf(v)))
+                Pair(1, 1) -> BilinearMap(gVectorSpace[1], gVectorSpace[1], gVectorSpace[2], matrixSpace, listOf(listOf(z2)))
+                else -> BilinearMap(gVectorSpace[p], gVectorSpace[q], gVectorSpace[p + q], matrixSpace, emptyList())
+            }
         }
-    }
-    gVectorSpace.context.run {
-        "graded bilinear map test" {
-            val u: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getBasis(0)[0]
-            val v: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getBasis(1)[0]
-            val z2: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getZero(2)
-            val z3: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getZero(3)
-            gBilinearMap(u, u) shouldBe u
-            gBilinearMap(u, v) shouldBe v
-            gBilinearMap(v, u) shouldBe v
-            gBilinearMap(v, v) shouldBe gVectorSpace.getZero(2)
-            gBilinearMap(z2, v) shouldBe z3
-            gBilinearMap(v, z2) shouldBe z3
-            gBilinearMap(z3, u) shouldBe z3
-            gBilinearMap(u, z3) shouldBe z3
+        "gBilinearMap.degree should be zero" {
+            gBilinearMap.degree.value shouldBe 0
+        }
+        gVectorSpace.context.run {
+            "check values of the bilinear map" {
+                val u: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getBasis(0)[0]
+                val v: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getBasis(1)[0]
+                val z2: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getZero(2)
+                val z3: GVector<IntDegree, StringBasisName, S, V> = gVectorSpace.getZero(3)
+                gBilinearMap(u, u) shouldBe u
+                gBilinearMap(u, v) shouldBe v
+                gBilinearMap(v, u) shouldBe v
+                gBilinearMap(v, v) shouldBe gVectorSpace.getZero(2)
+                gBilinearMap(z2, v) shouldBe z3
+                gBilinearMap(v, z2) shouldBe z3
+                gBilinearMap(z3, u) shouldBe z3
+                gBilinearMap(u, z3) shouldBe z3
+            }
         }
     }
 }

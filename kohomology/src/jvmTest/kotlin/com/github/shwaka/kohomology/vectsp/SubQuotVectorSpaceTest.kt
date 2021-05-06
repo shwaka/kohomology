0c5ea842992de9fa2556119ba337693b5b879fee
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-05-06 18:50:59 +0900

    use nested test in SubQuotVectorSpaceTest.kt

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/vectsp/SubQuotVectorSpaceTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/vectsp/SubQuotVectorSpaceTest.kt
@@ -17,35 +17,38 @@ val subQuotVectorSpaceTag = NamedTag("SubQuotVectorSpace")
 
 fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>>
 subQuotVectorSpaceTest(matrixSpace: MatrixSpace<S, V, M>) = freeSpec {
-    val numVectorSpace = matrixSpace.numVectorSpace
-    val vectorSpace = VectorSpace(numVectorSpace, listOf("u", "v", "w"))
-    val (u, v, w) = vectorSpace.getBasis()
-    vectorSpace.context.run {
-        val subspaceGenerator = listOf(u + v, v + w)
-        val quotientGenerator = listOf(u + 2 * v + w)
-        val subQuotVectorSpace = SubQuotVectorSpace(
-            matrixSpace,
-            vectorSpace,
-            subspaceGenerator = subspaceGenerator,
-            quotientGenerator = quotientGenerator
-        )
-        "sub quotient space test" {
-            subQuotVectorSpace.dim shouldBe 1
-            val x = subQuotVectorSpace.getBasis()[0]
-            val sect = subQuotVectorSpace.section
-            val proj = subQuotVectorSpace.projection
-            // section(x) shouldBe (u + v) // depends on the choice
-            proj(sect(x)) shouldBe x
-            proj(u + 2 * v + w).isZero().shouldBeTrue()
-            (proj(u - w) == proj(2 * (u + v))).shouldBeTrue()
+    "sub-quotient space test" - {
+        val numVectorSpace = matrixSpace.numVectorSpace
+        val vectorSpace = VectorSpace(numVectorSpace, listOf("u", "v", "w"))
+        val (u, v, w) = vectorSpace.getBasis()
+        vectorSpace.context.run {
+            val subspaceGenerator = listOf(u + v, v + w)
+            val quotientGenerator = listOf(u + 2 * v + w)
+            val subQuotVectorSpace = SubQuotVectorSpace(
+                matrixSpace,
+                vectorSpace,
+                subspaceGenerator = subspaceGenerator,
+                quotientGenerator = quotientGenerator
+            )
+            "check projection and section" {
+                subQuotVectorSpace.dim shouldBe 1
+                val x = subQuotVectorSpace.getBasis()[0]
+                val sect = subQuotVectorSpace.section
+                val proj = subQuotVectorSpace.projection
+                // section(x) shouldBe (u + v) // depends on the choice
+                proj(sect(x)) shouldBe x
+                proj(u + 2 * v + w).isZero().shouldBeTrue()
+                (proj(u - w) == proj(2 * (u + v))).shouldBeTrue()
+            }
+            "subspaceContains should work" {
+                subQuotVectorSpace.subspaceContains(u + v).shouldBeTrue()
+                subQuotVectorSpace.subspaceContains(v + w).shouldBeTrue()
+                subQuotVectorSpace.subspaceContains(-u - 2 * v - w).shouldBeTrue()
+                subQuotVectorSpace.subspaceContains(u).shouldBeFalse()
+                subQuotVectorSpace.subspaceContains(zeroVector).shouldBeTrue()
+            }
         }
-        "subspaceContains should work" {
-            subQuotVectorSpace.subspaceContains(u + v).shouldBeTrue()
-            subQuotVectorSpace.subspaceContains(v + w).shouldBeTrue()
-            subQuotVectorSpace.subspaceContains(-u - 2 * v - w).shouldBeTrue()
-            subQuotVectorSpace.subspaceContains(u).shouldBeFalse()
-            subQuotVectorSpace.subspaceContains(zeroVector).shouldBeTrue()
-        }
+
     }
 }
 

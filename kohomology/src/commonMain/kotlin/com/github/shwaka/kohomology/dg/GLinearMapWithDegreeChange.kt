ad3136ee61e23509de1fa1a78e8bd5f89743c9fc
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-07-22 15:19:47 +0900

    add GLinearMapWithDegreeChange.kt

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/GLinearMapWithDegreeChange.kt
@@ -0,0 +1,53 @@
+package com.github.shwaka.kohomology.dg
+
+import com.github.shwaka.kohomology.dg.degree.Degree
+import com.github.shwaka.kohomology.dg.degree.DegreeMorphism
+import com.github.shwaka.kohomology.exception.IllegalContextException
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.MatrixSpace
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.vectsp.BasisName
+
+open class GLinearMapWithDegreeChange<DS : Degree, BS : BasisName, DT : Degree, BT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
+    val source: GVectorSpace<DS, BS, S, V>,
+    val target: GVectorSpace<DT, BT, S, V>,
+    val degreeMorphism: DegreeMorphism<DS, DT>,
+    val matrixSpace: MatrixSpace<S, V, M>,
+    val name: String,
+    private val getValueOnBasis: (BS, DS) -> GVector<DT, BT, S, V>,
+) {
+    companion object {
+        operator fun <DS : Degree, BS : BasisName, DT : Degree, BT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> invoke(
+            source: GVectorSpace<DS, BS, S, V>,
+            target: GVectorSpace<DT, BT, S, V>,
+            degreeMorphism: DegreeMorphism<DS, DT>,
+            matrixSpace: MatrixSpace<S, V, M>,
+            name: String,
+            basisMap: (BS) -> BT,
+        ): GLinearMapWithDegreeChange<DS, BS, DT, BT, S, V, M> {
+            return GLinearMapWithDegreeChange(source, target, degreeMorphism, matrixSpace, name) { basisName, degree ->
+                val targetBasisName = basisMap(basisName)
+                val targetDegree = degreeMorphism(degree)
+                target.fromBasisName(targetBasisName, targetDegree)
+            }
+        }
+    }
+
+    operator fun invoke(gVector: GVector<DS, BS, S, V>): GVector<DT, BT, S, V> {
+        if (gVector !in this.source)
+            throw IllegalContextException("Invalid graded vector is given as an argument for a graded linear map with degree change")
+        val targetDegree = this.degreeMorphism(gVector.degree)
+        return this.target.context.run {
+            gVector.vector.toBasisMap().toList().map { (basisName, coeff) ->
+                this@GLinearMapWithDegreeChange.getValueOnBasis(basisName, gVector.degree) * coeff
+            }.fold(this@GLinearMapWithDegreeChange.target.getZero(targetDegree)) { acc, gVector ->
+                acc + gVector
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return this.name
+    }
+}

3b633ffc44d7fe5ff1632c71761c1a7e23452619
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-09-09 02:13:58 +0900

    refactoring in TexScriptInterface

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/tex/TexScript.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/tex/TexScript.kt
@@ -1,37 +1,35 @@
 package com.github.shwaka.kohomology.tex
 
 public interface TexScriptInterface<T : ScriptInterface<T>> : ScriptInterface<T> {
-    private companion object {
-        fun newcommandBase(
-            newcommandType: String,
-            name: String,
-            value: String,
-            numArgs: Int = 0,
-            defaultArg: String? = null,
-        ): String {
-            val nameWithBackslash = if (name.startsWith("\\")) name else "\\$name"
-            val numArgsString: String = when {
-                (numArgs == 0) -> ""
-                (numArgs > 0) -> "[$numArgs]"
-                else -> throw Exception("numArgs must be non-negative, but $numArgs is given")
-            }
-            val defaultArgString = if (defaultArg == null) "" else "[$defaultArg]"
-            return "\\$newcommandType{$nameWithBackslash}$numArgsString$defaultArgString{$value}"
+    private fun newcommandBase(
+        newcommandType: String,
+        name: String,
+        value: String,
+        numArgs: Int = 0,
+        defaultArg: String? = null,
+    ) {
+        val nameWithBackslash = if (name.startsWith("\\")) name else "\\$name"
+        val numArgsString: String = when {
+            (numArgs == 0) -> ""
+            (numArgs > 0) -> "[$numArgs]"
+            else -> throw Exception("numArgs must be non-negative, but $numArgs is given")
         }
+        val defaultArgString = if (defaultArg == null) "" else "[$defaultArg]"
+        this.addLines("\\$newcommandType{$nameWithBackslash}$numArgsString$defaultArgString{$value}")
     }
 
     public fun newcommand(name: String, value: String, numArgs: Int = 0, defaultArg: String? = null) {
-        this.addLines(TexScriptInterface.newcommandBase("newcommand", name, value, numArgs, defaultArg))
+        this.newcommandBase("newcommand", name, value, numArgs, defaultArg)
     }
     public fun newcommandStar(name: String, value: String, numArgs: Int = 0, defaultArg: String? = null) {
-        this.addLines(TexScriptInterface.newcommandBase("newcommand*", name, value, numArgs, defaultArg))
+        this.newcommandBase("newcommand*", name, value, numArgs, defaultArg)
     }
 
     public fun renewcommand(name: String, value: String, numArgs: Int = 0, defaultArg: String? = null) {
-        this.addLines(TexScriptInterface.newcommandBase("renewcommand", name, value, numArgs, defaultArg))
+        this.newcommandBase("renewcommand", name, value, numArgs, defaultArg)
     }
     public fun renewcommandStar(name: String, value: String, numArgs: Int = 0, defaultArg: String? = null) {
-        this.addLines(TexScriptInterface.newcommandBase("renewcommand*", name, value, numArgs, defaultArg))
+        this.newcommandBase("renewcommand*", name, value, numArgs, defaultArg)
     }
 
     public fun comment(commentText: String) {

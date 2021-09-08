5c1bc9de3c210a76dcbf0d2322b64890f8a2ab55
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-09-08 16:26:00 +0900

    refactoring and add TexDocument

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/tex/ScriptBuilder.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/tex/ScriptBuilder.kt
@@ -1,44 +1,61 @@
 package com.github.shwaka.kohomology.tex
 
-public open class ScriptBuilder(
+public interface Builder<T : Builder<T>> {
+    public fun addLines(lines: List<String>)
+    public fun toStringList(): List<String>
+    public fun newBuilder(linePrefix: String): T
+
+    public fun addLines(lines: String) {
+        this.addLines(lines.split("\n"))
+    }
+
+    public fun addLines(builder: Builder<*>) {
+        this.addLines(builder.toStringList())
+    }
+
+    public fun withLinePrefix(linePrefix: String, block: T.() -> Unit) {
+        val builder = this.newBuilder(linePrefix).apply(block)
+        this.addLines(builder)
+    }
+
+    public fun withIndent(indent: Int, block: T.() -> Unit) {
+        val linePrefix = " ".repeat(indent)
+        this.withLinePrefix(linePrefix, block)
+    }
+}
+
+public abstract class ScriptBuilderBase<T : ScriptBuilderBase<T>>(
     public val linePrefix: String = "",
     lines: MutableList<String> = mutableListOf(),
-) {
+) : Builder<T> {
     private val lines: MutableList<String> = lines.toMutableList() // copy
 
+    abstract override fun newBuilder(linePrefix: String): T
+
+    override fun addLines(lines: List<String>) {
+        this.lines.addAll(lines)
+    }
+
+    override fun toStringList(): List<String> {
+        return this.lines.map { this.linePrefix + it }.toList()
+    }
+
+    override fun toString(): String {
+        return this.toStringList().joinToString("\n")
+    }
+}
+
+public class ScriptBuilder(
+    linePrefix: String = "",
+    lines: MutableList<String> = mutableListOf()
+) : ScriptBuilderBase<ScriptBuilder>(linePrefix, lines) {
     public companion object {
         public operator fun invoke(linePrefix: String = "", block: ScriptBuilder.() -> Unit): ScriptBuilder {
             return ScriptBuilder(linePrefix).apply(block)
         }
     }
 
-    public fun addLines(lines: String) {
-        this.lines.addAll(lines.split("\n"))
-    }
-
-    public fun addLines(lines: List<String>) {
-        this.lines.addAll(lines)
-    }
-
-    public fun addLines(scriptBuilder: ScriptBuilder) {
-        this.addLines(scriptBuilder.toStringList())
-    }
-
-    public fun withLinePrefix(linePrefix: String, block: ScriptBuilder.() -> Unit) {
-        val scriptBuilder = ScriptBuilder(linePrefix).apply(block)
-        this.addLines(scriptBuilder)
-    }
-
-    public fun withIndent(indent: Int, block: ScriptBuilder.() -> Unit) {
-        val linePrefix = " ".repeat(indent)
-        this.withLinePrefix(linePrefix, block)
-    }
-
-    override fun toString(): String {
-        return this.toStringList().joinToString("\n")
-    }
-
-    protected open fun toStringList(): List<String> {
-        return this.lines.map { this.linePrefix + it }.toList()
+    override fun newBuilder(linePrefix: String): ScriptBuilder {
+        return ScriptBuilder(linePrefix)
     }
 }
--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/tex/TexScript.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/tex/TexScript.kt
@@ -1,12 +1,6 @@
 package com.github.shwaka.kohomology.tex
 
-public class TexScript(linePrefix: String = "") : ScriptBuilder(linePrefix) {
-    public companion object {
-        public operator fun invoke(linePrefix: String = "", block: TexScript.() -> Unit): TexScript {
-            return TexScript(linePrefix).apply(block)
-        }
-    }
-
+public interface TexScriptInterface<T : Builder<T>> : Builder<T> {
     public fun newcommand(name: String, value: String) {
         val nameWithBackslash = if (name.startsWith("\\")) name else "\\$name"
         this.addLines("\\newcommand{$nameWithBackslash}{$value}")
@@ -16,5 +10,43 @@ public class TexScript(linePrefix: String = "") : ScriptBuilder(linePrefix) {
         this.withLinePrefix("% ") {
             addLines(commentText)
         }
+    }
+}
+
+public class TexScript(linePrefix: String = "") :
+    ScriptBuilderBase<TexScript>(linePrefix),
+    TexScriptInterface<TexScript> {
+    public companion object {
+        public operator fun invoke(linePrefix: String = "", block: TexScript.() -> Unit): TexScript {
+            return TexScript(linePrefix).apply(block)
+        }
+    }
+
+    override fun newBuilder(linePrefix: String): TexScript {
+        return TexScript(linePrefix)
+    }
+}
+
+public interface TexDocumentInterface<T : TexScriptInterface<T>> : TexScriptInterface<T> {
+    public fun begin(name: String, block: T.() -> Unit) {
+        this.addLines("\\begin{$name}")
+        this.withIndent(2) {
+            block()
+        }
+        this.addLines("\\end{$name}")
+    }
+}
+
+public class TexDocument(linePrefix: String = "") :
+    ScriptBuilderBase<TexDocument>(linePrefix),
+    TexDocumentInterface<TexDocument> {
+    public companion object {
+        public operator fun invoke(linePrefix: String = "", block: TexDocument.() -> Unit): TexDocument {
+            return TexDocument(linePrefix).apply(block)
+        }
+    }
+
+    override fun newBuilder(linePrefix: String): TexDocument {
+        return TexDocument(linePrefix)
     }
 }

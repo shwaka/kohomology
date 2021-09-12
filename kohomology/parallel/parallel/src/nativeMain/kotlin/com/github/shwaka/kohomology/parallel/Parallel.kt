7e4f1b51e0aa891c6ba24d79aacdf09a0b249202
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-09-12 16:56:08 +0900

    symlink from nativeMain/**/Parallel.kt to jsMain/**/Parallel.kt

--- kohomology/parallel/parallel/src/nativeMain/kotlin/com/github/shwaka/kohomology/parallel/Parallel.kt
+++ kohomology/parallel/parallel/src/nativeMain/kotlin/com/github/shwaka/kohomology/parallel/Parallel.kt
@@ -1,3 +1,1 @@
-package com.github.shwaka.kohomology.parallel
-
-actual fun <T, R> Iterable<T>.pmap(transform: (T) -> R): List<R> = this.map(transform)
+../../../../../../../jsMain/kotlin/com/github/shwaka/kohomology/parallel/Parallel.kt

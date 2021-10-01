8c3e34799c808d44e1f5b69b50c6f8e2b3b8e019
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-10-01 19:44:37 +0900

    improve DirectSumTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/vectsp/DirectSumTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/vectsp/DirectSumTest.kt
@@ -6,6 +6,7 @@ import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverBigRational
+import com.github.shwaka.kohomology.util.list.* // ktlint-disable no-wildcard-imports
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.NamedTag
 import io.kotest.core.spec.style.FreeSpec
@@ -105,6 +106,49 @@ fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> directSumTest(matrixSpace: 
             // zero vector space からの LinearMap は常に zero なことが保証されているけど、
             // エラーを出さずに LinearMap のインスタンスをちゃんと生成できることの確認も込めてテストする
             directSum.inclusion(1).isZero().shouldBeTrue()
+        }
+    }
+
+    "direct sum of many vector spaces" - {
+        val vectorSpace0 = VectorSpace(numVectorSpace, listOf("v1", "v2"))
+        val vectorSpace1 = VectorSpace(numVectorSpace, listOf("w1", "w2", "w3"))
+        val directSum = DirectSum(
+            listOf(vectorSpace0, vectorSpace1, vectorSpace0, vectorSpace1),
+            matrixSpace
+        )
+
+        val (v1, v2) = vectorSpace0.getBasis()
+        val (w1, w2, w3) = vectorSpace1.getBasis()
+        val (d1, d2, d3, d4, d5, d6, d7, d8, d9, d10) = directSum.getBasis()
+
+        "dimension must be the sum" {
+            directSum.dim shouldBe (2 * (vectorSpace0.dim + vectorSpace1.dim))
+        }
+
+        "inclusion from the first component" {
+            val inclusion0 = directSum.inclusion(0)
+            inclusion0(v1) shouldBe d1
+            inclusion0(v2) shouldBe d2
+        }
+
+        "inclusion from the second component" {
+            val inclusion1 = directSum.inclusion(1)
+            inclusion1(w1) shouldBe d3
+            inclusion1(w2) shouldBe d4
+            inclusion1(w3) shouldBe d5
+        }
+
+        "inclusion from the third component" {
+            val inclusion2 = directSum.inclusion(2)
+            inclusion2(v1) shouldBe d6
+            inclusion2(v2) shouldBe d7
+        }
+
+        "inclusion from the fourth component" {
+            val inclusion3 = directSum.inclusion(3)
+            inclusion3(w1) shouldBe d8
+            inclusion3(w2) shouldBe d9
+            inclusion3(w3) shouldBe d10
         }
     }
 }

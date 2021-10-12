be29b3ff6132ea4e2bfa2fb8b68db7f208c95f9e
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-10-13 02:24:44 +0900

    ktlint

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/checkGAlgebraAxioms.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/checkGAlgebraAxioms.kt
@@ -1,12 +1,11 @@
 package com.github.shwaka.kohomology.dg
 
-import io.kotest.core.spec.style.scopes.FreeScope
 import com.github.shwaka.kohomology.dg.degree.Degree
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.vectsp.BasisName
-import io.kotest.inspectors.forAll
+import io.kotest.core.spec.style.scopes.FreeScope
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.element

12884825830ebceecd233c665295e04973f820a2
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-11-20 17:23:31 +0900

    merge release.sh and bump-version.sh

--- bump-version.sh
+++ bump-version.sh
@@ -2,17 +2,69 @@
 
 set -eu
 
-if [ -z "${1-}" ]; then
-    echo "Usage: ./bump-version.sh 0.5-SNAPSHOT" >&2
+VERSION_REGEX='^version = "\(.*\)"$'
+BUILD_GRADLE_KTS=kohomology/build.gradle.kts
+
+function show_usage() {
+    echo "Usage:" >&2
+    echo "  ./bump-version.sh release 99.9" >&2
+    echo "  ./bump-version.sh snapshot 99.9" >&2
+    current_version=$(cat $BUILD_GRADLE_KTS |
+                          grep "$VERSION_REGEX" |
+                          sed s/"$VERSION_REGEX"/\\1/)
+    echo "Current version: $current_version" >&2
+    tags=$(git tag | tac | tr "\n" " ")
+    echo "Known tags: $tags" >&2
+}
+
+function update_build_gradle_kts() {
+    version=$1
+
+    echo sed -i s/"$VERSION_REGEX"/"version = \"$version\""/ $BUILD_GRADLE_KTS
+    sed -i s/"$VERSION_REGEX"/"version = \"$version\""/ $BUILD_GRADLE_KTS
+}
+
+function release_version() {
+    local version=$1
+
+    if ! echo "$version" | grep -E '^[0-9.]+$'; then
+        # accepts 0.5, 1.0, 2
+        # rejects 0.5-SNAPSHOT, 1.0-snapshot, v2.0
+        echo "[Error] Invalid version number: $version"
+        exit 1
+    fi
+
+    update_build_gradle_kts "$version"
+
+    git commit -m "Release v$version"
+    git tag "v$version"
+
+    cd kohomology
+    ./gradlew publishAllPublicationsToMyMavenRepository
+}
+
+function bump_snapshot_version() {
+    local snapshot_version=$1-SNAPSHOT
+
+    update_build_gradle_kts "$snapshot_version"
+    for d in benchmark profile; do
+        local kts=$d/build.gradle.kts
+        echo sed -i 's/implementation("com.github.shwaka.kohomology:kohomology:.*")$/implementation("com.github.shwaka.kohomology:kohomology:'$snapshot_version'")/' $kts
+        sed -i 's/implementation("com.github.shwaka.kohomology:kohomology:.*")$/implementation("com.github.shwaka.kohomology:kohomology:'$snapshot_version'")/' $kts
+    done
+}
+
+if [ -z "${1-}" -o -z "${2-}"]; then
+    show_usage
     exit 1
 fi
 
-version=$1
+command=$1
+version=$2
 
-echo sed -i 's/^version = ".*"$/version = "'$version'"/' kohomology/build.gradle.kts
-sed -i 's/^version = ".*"$/version = "'$version'"/' kohomology/build.gradle.kts
-for d in benchmark profile; do
-    kts=$d/build.gradle.kts
-    echo sed -i 's/implementation("com.github.shwaka.kohomology:kohomology:.*")$/implementation("com.github.shwaka.kohomology:kohomology:'$version'")/' $kts
-    sed -i 's/implementation("com.github.shwaka.kohomology:kohomology:.*")$/implementation("com.github.shwaka.kohomology:kohomology:'$version'")/' $kts
-done
+case "$command" in
+    release) release_version "$version";;
+    snapshot) bump_snapshot_version "$version";;
+    *) echo "[Error] invalid command"
+       show_usage
+esac

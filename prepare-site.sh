54c156927bf7c0f1480bb75e67d3eaa5c88f2a54
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-12-04 18:29:43 +0900

    use prepare-site.sh in workflow

--- prepare-site.sh
+++ prepare-site.sh
@@ -5,9 +5,11 @@ cd $(dirname $0)
 
 # exapnd benchmark data to the working tree
 DESTINATION=benchmark-data
-git read-tree --prefix="$DESTINATION" -u origin/benchmark-data
+rm -r "$DESTINATION"
+git read-tree --prefix="$DESTINATION" -u remotes/origin/benchmark-data
 git reset HEAD "$DESTINATION"
 
 # build dokka
 cd kohomology
 ./gradlew dokkaHtml
+./gradlew dokkacovWriteJson

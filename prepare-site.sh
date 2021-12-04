faffba059c9649c39dcd0b010c558e9f0ff9220a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-12-04 18:37:04 +0900

    improve prepare-site.sh

--- prepare-site.sh
+++ prepare-site.sh
@@ -5,11 +5,14 @@ cd $(dirname $0)
 
 # exapnd benchmark data to the working tree
 DESTINATION=benchmark-data
+echo "Expand benchmark data to '$DESTINATION/'"
 rm -rf "$DESTINATION"
 git read-tree --prefix="$DESTINATION" -u remotes/origin/benchmark-data
 git reset HEAD "$DESTINATION"
 
 # build dokka
 cd kohomology
+echo "Generate dokka document"
 ./gradlew dokkaHtml
+echo "Generate dokka coverage"
 ./gradlew dokkacovWriteJson

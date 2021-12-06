6001fa3c4f90b3a5df573da00875ac6ecdd500a7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-12-06 15:17:55 +0900

    improve doc

--- sample/src/main/kotlin/SphereModel.kt
+++ sample/src/main/kotlin/SphereModel.kt
@@ -5,22 +5,24 @@ import com.github.shwaka.kohomology.free.monoid.Indeterminate
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverBigRational
 
 fun main() {
-    // start basic
-    val sphereDim = 4
+    // start def
+    val n = 2
     val indeterminateList = listOf(
-        Indeterminate("x", sphereDim),
-        Indeterminate("y", sphereDim * 2 - 1)
+        Indeterminate("x", 2 * n),
+        Indeterminate("y", 4 * n - 1)
     )
     val matrixSpace = SparseMatrixSpaceOverBigRational
     val sphere = FreeDGAlgebra(matrixSpace, indeterminateList) { (x, _) ->
         listOf(zeroGVector, x.pow(2)) // dx = 0, dy = x^2
     }
+    // end def
 
+    // start cohomology
     for (degree in 0 until 10) {
         val basis = sphere.cohomology[degree].getBasis()
-        println("H^$degree(S^$sphereDim) = Q$basis")
+        println("H^$degree(S^${2 * n}) = Q$basis")
     }
-    // end basic
+    // end cohomology
 
     // start context
     val (x, y) = sphere.gAlgebra.generatorList

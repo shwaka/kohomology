877ce0f5ec0542823a54e6e38f0f5d84c92ec1bf
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-12-07 18:09:31 +0900

    apply eslint:fix

--- website/src/theme/restrict.ts
+++ website/src/theme/restrict.ts
@@ -1,48 +1,48 @@
 function getLinesBetween(lines: string[], startRegExp: RegExp, endRegExp: RegExp): string[] | null {
-  const startLineNum: number = lines.findIndex(line => line.match(startRegExp));
-  const endLineNum: number = lines.findIndex(line => line.match(endRegExp));
+  const startLineNum: number = lines.findIndex(line => line.match(startRegExp))
+  const endLineNum: number = lines.findIndex(line => line.match(endRegExp))
   if (startLineNum === -1 || endLineNum === -1) {
-    return null;
+    return null
   }
-  return lines.slice(startLineNum + 1, endLineNum);
+  return lines.slice(startLineNum + 1, endLineNum)
 }
 
 function getIndent(line: string): number {
   if (line === "") {
-    return Infinity;
+    return Infinity
   }
-  const regexp = new RegExp("^ *");
+  const regexp = new RegExp("^ *")
   const matchObj: RegExpMatchArray | null = line.match(regexp)
   if (matchObj === null) {
-    throw new Error("This can't happen");
+    throw new Error("This can't happen")
   }
-  return matchObj[0].length;
+  return matchObj[0].length
 }
 
 function removeIndent(lines: string[]): string[] {
-  const indents: number[] = lines.map(line => getIndent(line));
-  const indent: number = Math.min(...indents);
-  return lines.map(line => line.substring(indent));
+  const indents: number[] = lines.map(line => getIndent(line))
+  const indent: number = Math.min(...indents)
+  return lines.map(line => line.substring(indent))
 }
 
 function createRegExp(startOrEnd: "start" | "end", key: string | true): RegExp {
   if (key === true) {
-    return new RegExp(`// ${startOrEnd}`);
+    return new RegExp(`// ${startOrEnd}`)
   } else {
-    return new RegExp(`// ${startOrEnd} +${key}`);
+    return new RegExp(`// ${startOrEnd} +${key}`)
   }
 }
 
 export function restrict(text: string, key: string | true | undefined): string | null {
   if (key === undefined) {
-    return text;
+    return text
   }
-  const startRegExp = createRegExp("start", key);
-  const endRegExp = createRegExp("end", key);
-  const lines = text.split("\n");
-  const restrictedLines: string[] | null = getLinesBetween(lines, startRegExp, endRegExp);
+  const startRegExp = createRegExp("start", key)
+  const endRegExp = createRegExp("end", key)
+  const lines = text.split("\n")
+  const restrictedLines: string[] | null = getLinesBetween(lines, startRegExp, endRegExp)
   if (restrictedLines === null) {
-    return null;
+    return null
   }
-  return removeIndent(restrictedLines).join("\n");
+  return removeIndent(restrictedLines).join("\n")
 }

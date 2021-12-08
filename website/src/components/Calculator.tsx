417e7032c7c4b03638bda7e28974e96d7abe25cd
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-12-08 11:08:13 +0900

    select from examples in Calculator

--- website/src/components/Calculator.tsx
+++ website/src/components/Calculator.tsx
@@ -20,12 +20,41 @@ function sphere(dim: number): string {
   }
 }
 
+function complexProjective(n: number): string {
+  if (!Number.isInteger(n)) {
+    throw new Error("dim must be an integer")
+  }
+  if (n <= 0) {
+    throw new Error("dim must be positive")
+  }
+  return `[
+  ["c", 2, "zero"],
+  ["x", ${2*n + 1}, "c^${n + 1}"]
+]`
+}
+
+function sevenManifold(): string {
+  return `[
+  ["a", 2, "zero"],
+  ["b", 2, "zero"],
+  ["x", 3, "a^2"],
+  ["y", 3, "a*b"],
+  ["z", 3, "b^2"]
+]`
+}
+
 type InputEvent = React.ChangeEvent<HTMLInputElement>
 type TextAreaEvent = React.ChangeEvent<HTMLTextAreaElement>
 
 export function Calculator(): JSX.Element {
   const [json, setJson] = useState(sphere(2))
   const [maxDegree, setMaxDegree] = useState("20")
+  function createButton(valueString: string, jsonString: string): JSX.Element {
+    return (
+      <input type="button" value={valueString}
+        onClick={() => setJson(jsonString)} />
+    )
+  }
   function handleSubmit(e: FormEvent) {
     e.preventDefault()
     console.log(computeCohomology(json, parseInt(maxDegree)))
@@ -38,6 +67,9 @@ export function Calculator(): JSX.Element {
   }
   return (
     <div>
+      {createButton("S^2", sphere(2))}
+      {createButton("CP^3", complexProjective(3))}
+      {createButton("7-mfd", sevenManifold())}
       <form onSubmit={handleSubmit}>
         <div>
           <span>max degree</span>

e9790e675c09285ddb6412d01812fca6bc190227
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-12-21 13:04:41 +0900

    add gVectorToDerivation, etc... to DerivationDGLieAlgebra

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/DerivationDGLieAlgebra.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/DerivationDGLieAlgebra.kt
@@ -1,9 +1,14 @@
 package com.github.shwaka.kohomology.free
 
+import com.github.shwaka.kohomology.dg.DGDerivation
 import com.github.shwaka.kohomology.dg.DGLieAlgebra
+import com.github.shwaka.kohomology.dg.Derivation
+import com.github.shwaka.kohomology.dg.GLieAlgebra
+import com.github.shwaka.kohomology.dg.GVector
 import com.github.shwaka.kohomology.dg.LieDerivation
 import com.github.shwaka.kohomology.dg.degree.Degree
 import com.github.shwaka.kohomology.free.monoid.IndeterminateName
+import com.github.shwaka.kohomology.free.monoid.Monomial
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
@@ -24,6 +29,24 @@ public class DerivationDGLieAlgebra<D : Degree, I : IndeterminateName, S : Scala
     private val factory: DerivationDGLieAlgebraFactory<D, I, S, V, M>,
 ) : DGLieAlgebra<D, DerivationBasis<D, I>, S, V, M>(factory.gLieAlgebra, factory.differential, factory.matrixSpace) {
     public val freeDGAlgebra: FreeDGAlgebra<D, I, S, V, M> = factory.freeDGAlgebra
+    override val gLieAlgebra: DerivationGLieAlgebra<D, I, S, V, M> = factory.gLieAlgebra
+
+    public fun gVectorToDerivation(gVector: GVector<D, DerivationBasis<D, I>, S, V>): Derivation<D, Monomial<D, I>, S, V, M> {
+        return this.gLieAlgebra.gVectorToDerivation(gVector)
+    }
+
+    public fun gVectorToDGDerivation(gVector: GVector<D, DerivationBasis<D, I>, S, V>): DGDerivation<D, Monomial<D, I>, S, V, M> {
+        this.context.run {
+            if (d(gVector).isNotZero()) {
+                throw IllegalArgumentException("gVector must be a cocycle, but d($gVector) = ${d(gVector)}")
+            }
+        }
+        return DGDerivation(this.freeDGAlgebra, this.gVectorToDerivation(gVector))
+    }
+
+    public fun derivationToGVector(derivation: Derivation<D, Monomial<D, I>, S, V, M>): GVector<D, DerivationBasis<D, I>, S, V> {
+        return this.gLieAlgebra.derivationToGVector(derivation)
+    }
 
     public companion object {
         public operator fun <D : Degree, I : IndeterminateName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> invoke(

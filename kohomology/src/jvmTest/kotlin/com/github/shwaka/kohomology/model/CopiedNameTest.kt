b85c1c3249bc01461b8a6910b85e9593276ca4c4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-12-22 16:04:30 +0900

    test CopiedName with MultiDegree

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/model/CopiedNameTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/model/CopiedNameTest.kt
@@ -1,5 +1,8 @@
 package com.github.shwaka.kohomology.model
 
+import com.github.shwaka.kohomology.dg.degree.DegreeGroup
+import com.github.shwaka.kohomology.dg.degree.DegreeIndeterminate
+import com.github.shwaka.kohomology.dg.degree.MultiDegreeGroup
 import com.github.shwaka.kohomology.free.monoid.Indeterminate
 import com.github.shwaka.kohomology.util.PrintType
 import com.github.shwaka.kohomology.util.Printer
@@ -69,6 +72,34 @@ class CopiedNameTest : FreeSpec({
                 plainPrinter(x1) shouldBe "x1"
                 texPrinter(x1) shouldBe "{x}_{(1)}"
             }
+        }
+    }
+    "CopiedName test (shift: MultiDegree)" - {
+        val degreeIndeterminateList = listOf(
+            DegreeIndeterminate("N", 0),
+        )
+        val degreeGroup = MultiDegreeGroup(degreeIndeterminateList)
+        val (n) = degreeGroup.generatorList
+        val sx = degreeGroup.context.run {
+            Indeterminate("x", fromInt(3)).copy(degreeGroup, shift = 1 + 2 * n)
+        }
+        "print sx when useBar = S_WITH_DEGREE" {
+            val plainPrinter = Printer(PrintType.PLAIN, useBar = UseBar.S_WITH_DEGREE)
+            val texPrinter = Printer(PrintType.TEX, useBar = UseBar.S_WITH_DEGREE)
+            plainPrinter(sx) shouldBe "s^{1 + 2N}x"
+            texPrinter(sx) shouldBe "s^{1 + 2N}{x}"
+        }
+        "print sx when useBar = S" {
+            val plainPrinter = Printer(PrintType.PLAIN, useBar = UseBar.S)
+            val texPrinter = Printer(PrintType.TEX, useBar = UseBar.S)
+            plainPrinter(sx) shouldBe "sx"
+            texPrinter(sx) shouldBe "s{x}"
+        }
+        "print sx when useBar = BAR" {
+            val plainPrinter = Printer(PrintType.PLAIN, useBar = UseBar.BAR)
+            val texPrinter = Printer(PrintType.TEX, useBar = UseBar.BAR)
+            plainPrinter(sx) shouldBe "_x"
+            texPrinter(sx) shouldBe "\\bar{x}"
         }
     }
 })

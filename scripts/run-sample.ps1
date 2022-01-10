13128d16f52983635ddb14d82c9f881c0a04afbc
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-01-10 20:12:22 +0900

    fix run-sample.ps1

--- scripts/run-sample.ps1
+++ scripts/run-sample.ps1
@@ -2,5 +2,5 @@ cd $(git rev-parse --show-toplevel) # go to the root of the repository
 
 cd sample
 Get-ChildItem -Name src/main/kotlin | Foreach-Object {
-    gradlew.bat run "-DsampleName=$_"
+    ./gradlew.bat run "-DsampleName=$_"
 }

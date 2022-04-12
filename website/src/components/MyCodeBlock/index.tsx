be354ed8ae328c01d49407c9c164e256f74b1b1e
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-04-13 00:29:17 +0900

    rewrite MyCodeBlock using the new version of CodeBlock (but kotlin not highlighted)

--- website/src/components/MyCodeBlock/index.tsx
+++ website/src/components/MyCodeBlock/index.tsx
@@ -8,21 +8,27 @@
 // docusaurus-theme-classic/src/theme/CodeBlock からコピペして
 // Props に href と linkTitle を追加した．
 
-import Translate, {translate} from "@docusaurus/Translate"
-import {useThemeConfig, parseCodeBlockTitle, usePrismTheme} from "@docusaurus/theme-common"
 // import usePrismTheme from "@theme/hooks/usePrismTheme" // 2.0.0-beta.9 -> 2.0.0-beta.18 のアプデで場所が変わったっぽい
-import type {Props} from "@theme/CodeBlock"
-import clsx from "clsx"
-import copy from "copy-text-to-clipboard"
-import rangeParser from "parse-numeric-range"
-import Highlight, {defaultProps, Language} from "prism-react-renderer"
-import React, {useEffect, useState, useRef} from "react"
+import React, {isValidElement, useEffect, useState} from 'react';
+import clsx from 'clsx';
+import Highlight, {defaultProps, type Language} from 'prism-react-renderer';
+import {
+  useThemeConfig,
+  parseCodeBlockTitle,
+  parseLanguage,
+  parseLines,
+  ThemeClassNames,
+  usePrismTheme,
+} from '@docusaurus/theme-common';
+import CopyButton from '@theme/CodeBlock/CopyButton';
+import type {Props} from '@theme/CodeBlock';
 // ↑ docusaurus-theme-classic/src/theme-classic.d.ts に定義がある
 // export interface Props {
 //   readonly children: string | ReactElement;
 //   readonly className?: string;
 //   readonly metastring?: string;
 //   readonly title?: string;
+//   readonly language?: string;
 // }
 
 import styles from "./styles.module.css"
@@ -31,101 +37,20 @@ import styles from "./styles.module.css"
 // 自分で書いたわけじゃないコードでエラーが起きるので無効化しておく
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 
-const HighlightLinesRangeRegex = /{([\d,-]+)}/
-
-const HighlightLanguages = ["js", "jsBlock", "jsx", "python", "html"] as const
-type HighlightLanguage = typeof HighlightLanguages[number]
-
-type HighlightLanguageConfig = {
-  start: string
-  end: string
-}
-
-// Supported types of highlight comments
-const HighlightComments: Record<HighlightLanguage, HighlightLanguageConfig> = {
-  js: {
-    start: "\\/\\/",
-    end: "",
-  },
-  jsBlock: {
-    start: "\\/\\*",
-    end: "\\*\\/",
-  },
-  jsx: {
-    start: "\\{\\s*\\/\\*",
-    end: "\\*\\/\\s*\\}",
-  },
-  python: {
-    start: "#",
-    end: "",
-  },
-  html: {
-    start: "<!--",
-    end: "-->",
-  },
-}
-
-// Supported highlight directives
-const HighlightDirectives = [
-  "highlight-next-line",
-  "highlight-start",
-  "highlight-end",
-]
-
-const getHighlightDirectiveRegex = (
-  languages: readonly HighlightLanguage[] = HighlightLanguages,
-): RegExp => {
-  // to be more reliable, the opening and closing comment must match
-  const commentPattern = languages
-    .map((lang) => {
-      const {start, end} = HighlightComments[lang]
-      return `(?:${start}\\s*(${HighlightDirectives.join("|")})\\s*${end})`
-    })
-    .join("|")
-  // white space is allowed, but otherwise it should be on it's own line
-  return new RegExp(`^\\s*(?:${commentPattern})\\s*$`)
-}
-
-// select comment styles based on language
-const highlightDirectiveRegex = (lang: string): RegExp => {
-  switch (lang) {
-    case "js":
-    case "javascript":
-    case "ts":
-    case "typescript":
-      return getHighlightDirectiveRegex(["js", "jsBlock"])
-
-    case "jsx":
-    case "tsx":
-      return getHighlightDirectiveRegex(["js", "jsBlock", "jsx"])
-
-    case "html":
-      return getHighlightDirectiveRegex(["js", "jsBlock", "html"])
-
-    case "python":
-    case "py":
-      return getHighlightDirectiveRegex(["python"])
-
-    default:
-      // all comment types
-      return getHighlightDirectiveRegex()
-  }
-}
-
+// 追加した
 type MyCodeBlockProps = { href?: string, linkTitle?: string } & Props
 
 export default function MyCodeBlock({
   children,
-  className: blockClassName,
+  className: blockClassName = '',
   metastring,
   title,
-  href,
-  linkTitle,
+  language: languageProp,
+  href, linkTitle, // 追加した
 }: MyCodeBlockProps): JSX.Element {
-  const {prism} = useThemeConfig()
+  const {prism} = useThemeConfig();
 
-  const [showCopied, setShowCopied] = useState(false)
-  const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false);
   // The Prism theme on SSR is always the default theme but the site theme
   // can be in a different mode. React hydration doesn't update DOM styles
   // that come from SSR. Hence force a re-render after mounting to apply the
@@ -134,96 +59,54 @@ export default function MyCodeBlock({
   // the flash will require changing the theming approach and is not worth it
   // at this point.
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  // TODO: the title is provided by MDX as props automatically
-  // so we probably don't need to parse the metastring
-  // (note: title="xyz" => title prop still has the quotes)
-  const codeBlockTitle = parseCodeBlockTitle(metastring) || title
+  // We still parse the metastring in case we want to support more syntax in the
+  // future. Note that MDX doesn't strip quotes when parsing metastring:
+  // "title=\"xyz\"" => title: "\"xyz\""
+  const codeBlockTitle = parseCodeBlockTitle(metastring) || title;
+  const prismTheme = usePrismTheme();
 
-  const button = useRef(null)
-  let highlightLines: number[] = []
+  // <pre> tags in markdown map to CodeBlocks and they may contain JSX children.
+  // When the children is not a simple string, we just return a styled block
+  // without actually highlighting.
+  if (React.Children.toArray(children).some((el) => isValidElement(el))) {
+    return (
+      <Highlight
+        {...defaultProps}
+        key={String(mounted)}
+        theme={prismTheme}
+        code=""
+        language={'text' as Language}>
+        {({className, style}) => (
+          <pre
+            /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
+            tabIndex={0}
+            className={clsx(
+              className,
+              styles.codeBlockStandalone,
+              'thin-scrollbar',
+              styles.codeBlockContainer,
+              blockClassName,
+              ThemeClassNames.common.codeBlock,
+            )}
+            style={style}>
+            <code className={styles.codeBlockLines}>{children}</code>
+          </pre>
+        )}
+      </Highlight>
+    );
+  }
 
-  const prismTheme = usePrismTheme()
-
-  // In case interleaved Markdown (e.g. when using CodeBlock as standalone component).
+  // The children is now guaranteed to be one/more plain strings
   const content = Array.isArray(children)
-    ? children.join("")
-    : (children as string)
+    ? children.join('')
+    : (children as string);
 
-  if (metastring && HighlightLinesRangeRegex.test(metastring)) {
-    // Tested above
-    // ↓元々なかったので追加
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const highlightLinesRange = metastring.match(HighlightLinesRangeRegex)![1]
-    highlightLines = rangeParser(highlightLinesRange).filter((n) => n > 0)
-  }
-
-  const languageClassName = blockClassName
-    ?.split(" ")
-    .find((str) => str.startsWith("language-"))
-  let language = languageClassName?.replace(/language-/, "") as Language
-
-  if (!language && prism.defaultLanguage) {
-    language = prism.defaultLanguage as Language
-  }
-
-  // only declaration OR directive highlight can be used for a block
-  let code = content.replace(/\n$/, "")
-  if (highlightLines.length === 0 && language !== undefined) {
-    let range = ""
-    const directiveRegex = highlightDirectiveRegex(language)
-    // go through line by line
-    const lines = content.replace(/\n$/, "").split("\n")
-    let blockStart: number
-    // loop through lines
-    for (let index = 0; index < lines.length; ) {
-      const line = lines[index]
-      // adjust for 0-index
-      const lineNumber = index + 1
-      const match = line.match(directiveRegex)
-      if (match !== null) {
-        const directive = match
-          .slice(1)
-          .reduce(
-            (final: string | undefined, item) => final || item,
-            undefined,
-          )
-        switch (directive) {
-          case "highlight-next-line":
-            range += `${lineNumber},`
-            break
-
-          case "highlight-start":
-            blockStart = lineNumber
-            break
-
-          case "highlight-end":
-            // ↓元々なかったので追加
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            range += `${blockStart!}-${lineNumber - 1},`
-            break
-
-          default:
-            break
-        }
-        lines.splice(index, 1)
-      } else {
-        // lines without directives are unchanged
-        index += 1
-      }
-    }
-    highlightLines = rangeParser(range)
-    code = lines.join("\n")
-  }
-
-  const handleCopyCode = (): void => {
-    copy(code)
-    setShowCopied(true)
-
-    setTimeout(() => setShowCopied(false), 2000)
-  }
+  const language =
+    languageProp ?? parseLanguage(blockClassName) ?? prism.defaultLanguage;
+  const {highlightLines, code} = parseLines(content, metastring, language);
 
   return (
     <Highlight
@@ -231,12 +114,17 @@ export default function MyCodeBlock({
       key={String(mounted)}
       theme={prismTheme}
       code={code}
-      language={language}>
+      language={(language ?? 'text') as Language}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
         <div
           className={clsx(
             styles.codeBlockContainer,
-            blockClassName?.replace(/language-[^ ]+/, ""),
+            blockClassName,
+            {
+              [`language-${language}`]:
+                language && !blockClassName.includes(`language-${language}`),
+            },
+            ThemeClassNames.common.codeBlock,
           )}>
           {(codeBlockTitle || href) && (
             <div style={style} className={styles.codeBlockTitle}>
@@ -244,21 +132,21 @@ export default function MyCodeBlock({
               {href && <a href={href} target="_blank" rel="noreferrer">{linkTitle ? linkTitle : href}</a>}
             </div>
           )}
-          <div className={clsx(styles.codeBlockContent, language)}>
+          <div className={styles.codeBlockContent} style={style}>
             <pre
+              /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
               tabIndex={0}
-              className={clsx(className, styles.codeBlock, "thin-scrollbar")}
-              style={style}>
+              className={clsx(className, styles.codeBlock, 'thin-scrollbar')}>
               <code className={styles.codeBlockLines}>
                 {tokens.map((line, i) => {
-                  if (line.length === 1 && line[0].content === "\n") {
-                    line[0].content = ""
+                  if (line.length === 1 && line[0]!.content === '\n') {
+                    line[0]!.content = '';
                   }
 
-                  const lineProps = getLineProps({line, key: i})
+                  const lineProps = getLineProps({line, key: i});
 
-                  if (highlightLines.includes(i + 1)) {
-                    lineProps.className += " docusaurus-highlight-code-line"
+                  if (highlightLines.includes(i)) {
+                    lineProps.className += ' docusaurus-highlight-code-line';
                   }
 
                   return (
@@ -268,38 +156,15 @@ export default function MyCodeBlock({
                       ))}
                       <br />
                     </span>
-                  )
+                  );
                 })}
               </code>
             </pre>
 
-            <button
-              ref={button}
-              type="button"
-              aria-label={translate({
-                id: "theme.CodeBlock.copyButtonAriaLabel",
-                message: "Copy code to clipboard",
-                description: "The ARIA label for copy code blocks button",
-              })}
-              className={clsx(styles.copyButton, "clean-btn")}
-              onClick={handleCopyCode}>
-              {showCopied ? (
-                <Translate
-                  id="theme.CodeBlock.copied"
-                  description="The copied button label on code blocks">
-                  Copied
-                </Translate>
-              ) : (
-                <Translate
-                  id="theme.CodeBlock.copy"
-                  description="The copy button label on code blocks">
-                  Copy
-                </Translate>
-              )}
-            </button>
+            <CopyButton code={code} />
           </div>
         </div>
       )}
     </Highlight>
-  )
+  );
 }

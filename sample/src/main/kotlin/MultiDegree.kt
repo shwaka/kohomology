b5b4550578fcbcf3d59d379cc6b9f9f815f532bd
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-04-29 20:54:55 +0900

    add MultiDegree.kt to sample/

---
+++ sample/src/main/kotlin/MultiDegree.kt
@@ -0,0 +1,44 @@
+package com.github.shwaka.kohomology.sample
+
+import com.github.shwaka.kohomology.dg.degree.DegreeIndeterminate
+import com.github.shwaka.kohomology.dg.degree.MultiDegreeGroup
+import com.github.shwaka.kohomology.free.FreeDGAlgebra
+import com.github.shwaka.kohomology.free.monoid.Indeterminate
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
+
+fun main() {
+    // \begin{degree}
+    val sphereDim = 2
+    val degreeGroup = MultiDegreeGroup(
+        listOf(
+            DegreeIndeterminate("N", sphereDim / 2),
+            DegreeIndeterminate("M", sphereDim / 2),
+        )
+    )
+    val (n, m) = degreeGroup.generatorList
+    // \end{degree}
+
+    // \begin{model}
+    val indeterminateList = degreeGroup.context.run {
+        listOf(
+            Indeterminate("x", 2 * n),
+            Indeterminate("y", 4 * n - 1),
+            Indeterminate("a", 2 * m),
+            Indeterminate("b", 4 * m - 1),
+        )
+    }
+    val matrixSpace = SparseMatrixSpaceOverRational
+    val sphere = FreeDGAlgebra(matrixSpace, degreeGroup, indeterminateList) { (x, y, a, b) ->
+        listOf(zeroGVector, x.pow(2), zeroGVector, a.pow(2))
+    }
+    // \end{model}
+
+    // \begin{cohomology}
+    degreeGroup.context.run {
+        println(sphere.cohomology.getBasis(0))
+        println(sphere.cohomology.getBasis(2 * n))
+        println(sphere.cohomology.getBasis(2 * m))
+        println(sphere.cohomology.getBasisForAugmentedDegree(sphereDim))
+    }
+    // \end{cohomology}
+}

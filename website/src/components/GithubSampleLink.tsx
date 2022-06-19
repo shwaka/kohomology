dd4efbbde5577502c79b93f2e00933850c50342b
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-06-20 01:38:41 +0900

    fix link

---
+++ website/src/components/GithubSampleLink.tsx
@@ -0,0 +1,11 @@
+import { githubMainUrl } from "@data"
+import React from "react"
+
+export function GithubSampleLink(props: { children: string }): JSX.Element {
+  const url = `${githubMainUrl}/website/sample/src/main/kotlin/`
+  return (
+    <a href={url} target="_blank" rel="noreferrer">
+      {props.children}
+    </a>
+  )
+}

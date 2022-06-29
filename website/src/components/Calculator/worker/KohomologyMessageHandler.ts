7725189901bda15431a510262f6049e61f0ed8ca
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-06-29 19:57:19 +0900

    refactoring: use ExhaustiveError instead of assertUnreachable

--- website/src/components/Calculator/worker/KohomologyMessageHandler.ts
+++ website/src/components/Calculator/worker/KohomologyMessageHandler.ts
@@ -29,7 +29,7 @@ export class KohomologyMessageHandler {
           this.computeCohomologyClass(input.targetName, input.cocycleString, input.showBasis)
           break
         default:
-          assertUnreachable(input, "Invalid command")
+          throw new ExhaustiveError(input, `Invalid command: ${input}`)
       }
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -100,6 +100,9 @@ function assertNotNull<T>(value: T | null): asserts value is T {
   }
 }
 
-function assertUnreachable(_: never, message: string): never {
-  throw new Error(`This can't happen! (${message})`)
+class ExhaustiveError extends Error {
+  // https://typescriptbook.jp/reference/statements/never#%E4%BE%8B%E5%A4%96%E3%81%AB%E3%82%88%E3%82%8B%E7%B6%B2%E7%BE%85%E6%80%A7%E3%83%81%E3%82%A7%E3%83%83%E3%82%AF (例外による網羅性チェック)
+  constructor(value: never, message = `Unsupported type: ${value}`) {
+    super(message)
+  }
 }

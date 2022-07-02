4bd5693b5d0d383e361325c0898bcb10433404af
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-02 19:00:21 +0900

    build: add commitlint

---
+++ scripts/commitlint/commitlint.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // "scope-empty": [2, "never"], // scope is not required
+    "scope-enum": [2, "always", ["core", "website"]],
+  },
+}
+
+module.exports = config

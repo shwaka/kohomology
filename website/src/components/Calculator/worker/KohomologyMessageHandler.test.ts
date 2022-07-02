4a856dcba4cbfe76c4ffdea1cd79479295687166
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-03 02:36:59 +0900

    test(website): improve KohomologyMessageHandler.test.ts

--- website/src/components/Calculator/worker/KohomologyMessageHandler.test.ts
+++ website/src/components/Calculator/worker/KohomologyMessageHandler.test.ts
@@ -1,5 +1,10 @@
 import { KohomologyMessageHandler } from "./KohomologyMessageHandler"
+import { StyledMessage } from "./styled"
 import { WorkerInput, WorkerOutput } from "./workerInterface"
+
+function formatMessage(styledMessage: StyledMessage): string {
+  return styledMessage.strings.map((styledString) => styledString.content).join("")
+}
 
 test("computeCohomology", () => {
   const outputs: WorkerOutput[] = []
@@ -13,9 +18,16 @@ test("computeCohomology", () => {
     command: "computeCohomology",
     targetName: "self",
     minDegree: 0,
-    maxDegree: 5,
+    maxDegree: 4,
     showCohomology: "basis",
   }
   messageHandler.onmessage({ data: computeCohomologyCommand } as MessageEvent<WorkerInput>)
+  expect(outputs.length).toBe(6)
   expect(outputs[0].messages[0].strings[0].content).toEqual("Cohomology of ")
+  expect(formatMessage(outputs[0].messages[0])).toEqual("Cohomology of (Λ(x, y), d) is")
+  expect(formatMessage(outputs[1].messages[0])).toEqual("H^{0} =\\ \\mathbb{Q}\\{[1]\\}")
+  expect(formatMessage(outputs[2].messages[0])).toEqual("H^{1} =\\ 0")
+  expect(formatMessage(outputs[3].messages[0])).toEqual("H^{2} =\\ \\mathbb{Q}\\{[x]\\}")
+  expect(formatMessage(outputs[4].messages[0])).toEqual("H^{3} =\\ 0")
+  expect(formatMessage(outputs[5].messages[0])).toEqual("H^{4} =\\ 0")
 })

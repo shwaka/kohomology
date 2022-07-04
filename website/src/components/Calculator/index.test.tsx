95a9f0fc19ed40b09383761536e72dd42cc28fae
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-04 19:32:43 +0900

    test(website): click button to compute cohomology groups

--- website/src/components/Calculator/index.test.tsx
+++ website/src/components/Calculator/index.test.tsx
@@ -1,4 +1,4 @@
-import { getByRole, render, screen } from "@testing-library/react"
+import { fireEvent, getByRole, render, screen } from "@testing-library/react"
 import React from "react"
 import { Calculator } from "."
 
@@ -6,8 +6,11 @@ test("Calculator", () => {
   render(<Calculator/>)
   const calculator = screen.getByTestId("Calculator")
   expect(calculator).toContainHTML("Computation results will be shown here")
+  expect(calculator).not.toContainHTML("Cohomology of ")
   const computeCohomologyForm = screen.getByTestId("ComputeCohomologyForm")
   expect(computeCohomologyForm).toContainHTML("Compute cohomology")
   const computeCohomologyButton = getByRole(computeCohomologyForm, "button")
   expect(computeCohomologyButton).toContainHTML("Compute")
+  fireEvent.click(computeCohomologyButton)
+  expect(calculator).toContainHTML("Cohomology of ")
 })

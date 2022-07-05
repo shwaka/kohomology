3bd294c893448d1f04cf39da5c45040d58ee9168
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-05 12:36:05 +0900

    test(website): fix markdownTransformer.js

--- website/markdownTransformer.js
+++ website/markdownTransformer.js
@@ -3,7 +3,10 @@
 module.exports = {
   process(sourceText, sourcePath, options) {
     return {
-      code: `import React from "react"; module.exports = function () { return React.createElement("div", {}, ${sourceText}) }`
+      code: 'const React = require("react"); module.exports = function () { return React.createElement("div", {}, "Content of a markdown file") }'
+      // "Content of a markdown file" の所は本当は sourceText を入れるべき．
+      // しかし ${sourceText} ではダメで，エスケープなどの処理をする必要がある．
+      // sourcePath を使って code 内で読み込む方が良いかも？
     }
   }
 }

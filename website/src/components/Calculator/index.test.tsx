2cff43b3c6142de27d3f2b5dfe655541176e3820
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-05 22:06:20 +0900

    style(website): use 'within' from @testing-library/react

--- website/src/components/Calculator/index.test.tsx
+++ website/src/components/Calculator/index.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, getByRole, getByTestId, getByText, render, screen, waitForElementToBeRemoved } from "@testing-library/react"
+import { fireEvent, render, screen, waitForElementToBeRemoved, within } from "@testing-library/react"
 import React from "react"
 import { Calculator } from "."
 
@@ -26,7 +26,7 @@ function expectInitialState(): void {
 function clickComputeCohomologyButton(): void {
   const computeCohomologyForm = screen.getByTestId("ComputeCohomologyForm")
   expect(computeCohomologyForm).toContainHTML("Compute cohomology")
-  const computeCohomologyButton = getByRole(computeCohomologyForm, "button")
+  const computeCohomologyButton = within(computeCohomologyForm).getByRole("button")
   expect(computeCohomologyButton).toContainHTML("Compute")
   fireEvent.click(computeCohomologyButton)
 }
@@ -34,15 +34,15 @@ function clickComputeCohomologyButton(): void {
 async function inputJson(json: string): Promise<void> {
   // Open dialog
   const calculatorFormStackItemDGA = screen.getByTestId("CalculatorForm-StackItem-DGA")
-  const editDGAButton = getByText(calculatorFormStackItemDGA, "Edit DGA")
+  const editDGAButton = within(calculatorFormStackItemDGA).getByText("Edit DGA")
   expect(screen.queryByTestId("JsonEditorDialog-input-json")).toBeNull()
   fireEvent.click(editDGAButton)
   // Input json
   const dialog = screen.getByRole("dialog")
-  const jsonTextField = getByTestId(dialog, "JsonEditorDialog-input-json")
+  const jsonTextField = within(dialog).getByTestId("JsonEditorDialog-input-json")
   fireEvent.input(jsonTextField, { target: { value: json } })
   // Click "Apply" button
-  const applyButton = getByText(dialog, "Apply")
+  const applyButton = within(dialog).getByText("Apply")
   fireEvent.click(applyButton)
   await waitForElementToBeRemoved(dialog) // It takes some time to remove the dialog.
 }

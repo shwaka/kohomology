27354f23c7e07ff07846faa4de28a9b35297410d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-13 21:31:27 +0900

    refactor(website): rename ExhaustiveError to ExhaustivityError

--- website/src/components/Calculator/worker/KohomologyMessageHandler.ts
+++ website/src/components/Calculator/worker/KohomologyMessageHandler.ts
@@ -35,7 +35,7 @@ export class KohomologyMessageHandler {
           this.computeCohomologyClass(input.targetName, input.cocycleString, input.showBasis)
           break
         default:
-          throw new ExhaustiveError(input, `Invalid command: ${input}`)
+          throw new ExhaustivityError(input, `Invalid command: ${input}`)
       }
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -106,7 +106,7 @@ function assertNotNull<T>(value: T | null): asserts value is T {
   }
 }
 
-class ExhaustiveError extends Error {
+class ExhaustivityError extends Error {
   // https://typescriptbook.jp/reference/statements/never#%E4%BE%8B%E5%A4%96%E3%81%AB%E3%82%88%E3%82%8B%E7%B6%B2%E7%BE%85%E6%80%A7%E3%83%81%E3%82%A7%E3%83%83%E3%82%AF (例外による網羅性チェック)
   constructor(value: never, message = `Unsupported type: ${value}`) {
     super(message)

7358680595918f53da0d5b02d20079f901bb8427
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-17 11:33:29 +0900

    docs(core): add comments to GAlgebraGrammar

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/GAlgebraGrammar.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/GAlgebraGrammar.kt
@@ -21,6 +21,8 @@ public class GAlgebraGrammar<D : Degree, B : BasisName, S : Scalar, V : NumVecto
     private val gAlgebra: GAlgebra<D, B, S, V, M>,
     private val generators: List<Pair<String, GVector<D, B, S, V>>>
 ) : Grammar<GVectorOrZero<D, B, S, V>>() {
+    // Previously "0" could not be used as a scalar and "zero" is used for such purpose.
+    // Currently "zero" is unnecessary but left here for compatibility reason.
     private val zero by literalToken("zero")
     private val gen by regexToken("(" + this.generators.joinToString("|") { Regex.escape(it.first) } + ")")
     private val int by regexToken("\\d+")

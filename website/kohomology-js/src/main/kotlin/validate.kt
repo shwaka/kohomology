3bfaee772bc2b1de68d985606f8ed8d2205aedf2
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-19 17:51:57 +0900

    feat(website): show error when d(d(v)) is not zero in Calculator

--- website/kohomology-js/src/main/kotlin/validate.kt
+++ website/kohomology-js/src/main/kotlin/validate.kt
@@ -1,6 +1,10 @@
 import com.github.h0tk3y.betterParse.parser.ParseException
 import com.github.shwaka.kohomology.dg.GVector
+import com.github.shwaka.kohomology.dg.ZeroGVector
+import com.github.shwaka.kohomology.dg.degree.IntDegree
+import com.github.shwaka.kohomology.free.FreeDGAlgebra
 import com.github.shwaka.kohomology.free.FreeGAlgebra
+import com.github.shwaka.kohomology.free.GeneratorOfFreeDGA
 import com.github.shwaka.kohomology.free.monoid.Indeterminate
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
 
@@ -42,13 +46,11 @@ fun validateJson(json: String): ValidationResult {
 }
 
 @ExperimentalJsExport
-@JsExport
-fun validateDifferentialValue(
-    previousGeneratorsJson: String,
+private fun assertDegreeOfDifferentialValue(
+    generatorList: List<GeneratorOfFreeDGA<IntDegree>>,
     differentialValue: String,
     expectedDegree: Int,
-): ValidationResult {
-    val generatorList = jsonToGeneratorList(previousGeneratorsJson)
+): ValidationResultInternal? {
     val indeterminateList = generatorList.map { generator ->
         Indeterminate(generator.name, generator.degree)
     }
@@ -59,15 +61,63 @@ fun validateDifferentialValue(
         val messageFromException: String = e.message ?: e.toString()
         val message = "Failed to parse the value \"$differentialValue\" of the differential " +
             "with the following error message:\n$messageFromException"
-        return ValidationResultInternal.Error(message).export()
+        return ValidationResultInternal.Error(message)
     } catch (e: Exception) {
         val message: String = e.message ?: e.toString()
-        return ValidationResultInternal.Error(message).export()
+        return ValidationResultInternal.Error(message)
     }
     if (gVector is GVector && gVector.degree.value != expectedDegree) {
         val message = "Illegal degree: the degree of $differentialValue is ${gVector.degree.value}, " +
             "but ${expectedDegree - 1}+1=$expectedDegree is expected."
-        return ValidationResultInternal.Error(message).export()
+        return ValidationResultInternal.Error(message)
+    }
+    return null
+}
+
+@ExperimentalJsExport
+fun assertSquareOfDifferentialIsZero(
+    generatorList: List<GeneratorOfFreeDGA<IntDegree>>,
+    differentialValue: String,
+): ValidationResultInternal? {
+    val freeDGAlgebra = try {
+        FreeDGAlgebra(SparseMatrixSpaceOverRational, generatorList)
+    } catch (e: Exception) {
+        val generatorsString = generatorList.joinToString(", ") { it.name }
+        val message = "Please fix errors for generators ($generatorsString) above this."
+        return ValidationResultInternal.Error(message)
+    }
+    freeDGAlgebra.context.run {
+        return when (val gVector = freeDGAlgebra.gAlgebra.parse(differentialValue)) {
+            is ZeroGVector -> null
+            is GVector -> {
+                val gVectorWhichShouldBeZero = d(gVector)
+                when (gVectorWhichShouldBeZero.isZero()) {
+                    true -> null
+                    false -> ValidationResultInternal.Error(
+                        "d($differentialValue) must be zero, but was $gVectorWhichShouldBeZero"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalJsExport
+@JsExport
+fun validateDifferentialValue(
+    previousGeneratorsJson: String,
+    differentialValue: String,
+    expectedDegree: Int,
+): ValidationResult {
+    // FreeGAlgebra を assertDegreeOfDifferentialValue と assertSquareOfDifferentialIsZero の両方で
+    // 生成しているのは無駄な気もするけど、
+    // FreeDGAlgebra(SparseMatrixSpaceOverRational, generatorList) をそのまま使うためには仕方ない。
+    val generatorList = jsonToGeneratorList(previousGeneratorsJson)
+    assertDegreeOfDifferentialValue(generatorList, differentialValue, expectedDegree)?.let {
+        return it.export()
+    }
+    assertSquareOfDifferentialIsZero(generatorList, differentialValue)?.let {
+        return it.export()
     }
     return ValidationResultInternal.Success().export()
 }

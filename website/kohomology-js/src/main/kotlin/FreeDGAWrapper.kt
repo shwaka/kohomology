67b1b580a3bd7102765f758e783bcf1e48c1b7fa
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-19 17:17:03 +0900

    refactor(website): add jsonParser.kt

--- website/kohomology-js/src/main/kotlin/FreeDGAWrapper.kt
+++ website/kohomology-js/src/main/kotlin/FreeDGAWrapper.kt
@@ -7,7 +7,6 @@ import com.github.shwaka.kohomology.dg.degree.Degree
 import com.github.shwaka.kohomology.dg.degree.IntDegreeGroup
 import com.github.shwaka.kohomology.free.DerivationDGLieAlgebra
 import com.github.shwaka.kohomology.free.FreeDGAlgebra
-import com.github.shwaka.kohomology.free.GeneratorOfFreeDGA
 import com.github.shwaka.kohomology.free.monoid.IndeterminateName
 import com.github.shwaka.kohomology.free.monoid.Monomial
 import com.github.shwaka.kohomology.linalg.Matrix
@@ -16,7 +15,6 @@ import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.model.CyclicModel
 import com.github.shwaka.kohomology.model.FreeLoopSpace
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
-import com.github.shwaka.kohomology.util.IntAsDegree
 import com.github.shwaka.kohomology.util.PrintConfig
 import com.github.shwaka.kohomology.util.PrintType
 import com.github.shwaka.kohomology.util.Printable
@@ -24,48 +22,15 @@ import com.github.shwaka.kohomology.util.Printer
 import com.github.shwaka.kohomology.util.ShowShift
 import com.github.shwaka.kohomology.vectsp.BasisName
 import com.github.shwaka.kohomology.vectsp.SubQuotBasis
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.ListSerializer
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonTransformingSerializer
-
-@Serializable
-data class SerializableGenerator(val name: String, val degree: IntAsDegree, val differentialValue: String)
-
-object GeneratorSerializer : JsonTransformingSerializer<SerializableGenerator>(SerializableGenerator.serializer()) {
-    override fun transformDeserialize(element: JsonElement): JsonElement {
-        return if (element is JsonArray) {
-            JsonObject(
-                mapOf(
-                    "name" to element[0],
-                    "degree" to element[1],
-                    "differentialValue" to element[2],
-                )
-            )
-        } else {
-            element
-        }
-    }
-}
-
-private fun jsonToGeneratorList(json: String): List<SerializableGenerator> {
-    return Json.decodeFromString(ListSerializer(GeneratorSerializer), json)
-}
 
 @ExperimentalJsExport
 @JsExport
 @Suppress("UNUSED")
 class FreeDGAWrapper(val json: String) {
-    private val freeDGAlgebra = run {
-        val serializableGeneratorList = jsonToGeneratorList(json)
-        val generatorList = serializableGeneratorList.map {
-            GeneratorOfFreeDGA(it.name, it.degree, it.differentialValue)
-        }
-        FreeDGAlgebra(SparseMatrixSpaceOverRational, generatorList)
-    }
+    private val freeDGAlgebra = FreeDGAlgebra(
+        SparseMatrixSpaceOverRational,
+        jsonToGeneratorList(json)
+    )
     private val freeLoopSpace by lazy { FreeLoopSpace.withShiftDegree(freeDGAlgebra) }
     private val cyclicModel by lazy { CyclicModel(freeDGAlgebra) }
     private val derivationLieAlgebra by lazy { DerivationDGLieAlgebra(freeDGAlgebra) }

a8588114663d6c931e14777b92143ebec037000d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-20 18:03:02 +0900

    feat(website): remove footer from the website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -76,6 +76,7 @@ const config = {
             position: "left",
             target: "_blank", // これがないと React router のせいでうまくいかないっぽい
           },
+          {to: "others", label: "Others", position: "left"},
           {
             href: "https://github.com/shwaka/kohomology",
             label: "GitHub",
@@ -83,61 +84,61 @@ const config = {
           },
         ],
       },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
-          {
-            title: "More",
-            items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog',
-              // },
-              {
-                label: "GitHub",
-                href: "https://github.com/shwaka/kohomology",
-              },
-              {
-                href: "/dokka/index.html",
-                label: "dokka",
-                target: "_blank", // これがないと React router のせいでうまくいかないっぽい
-              },
-              {
-                href: "/benchmark/index.html",
-                label: "benchmark",
-                target: "_blank", // これがないと React router のせいでうまくいかないっぽい
-              },
-            ],
-          },
-        ],
-        // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
+      // footer: {
+      //   style: "dark",
+      //   links: [
+      //     {
+      //       title: "Docs",
+      //       items: [
+      //         {
+      //           label: "Tutorial",
+      //           to: "/docs/intro",
+      //         },
+      //       ],
+      //     },
+      //     // {
+      //     //   title: 'Community',
+      //     //   items: [
+      //     //     {
+      //     //       label: 'Stack Overflow',
+      //     //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //     //     },
+      //     //     {
+      //     //       label: 'Discord',
+      //     //       href: 'https://discordapp.com/invite/docusaurus',
+      //     //     },
+      //     //     {
+      //     //       label: 'Twitter',
+      //     //       href: 'https://twitter.com/docusaurus',
+      //     //     },
+      //     //   ],
+      //     // },
+      //     {
+      //       title: "More",
+      //       items: [
+      //         // {
+      //         //   label: 'Blog',
+      //         //   to: '/blog',
+      //         // },
+      //         {
+      //           label: "GitHub",
+      //           href: "https://github.com/shwaka/kohomology",
+      //         },
+      //         {
+      //           href: "/dokka/index.html",
+      //           label: "dokka",
+      //           target: "_blank", // これがないと React router のせいでうまくいかないっぽい
+      //         },
+      //         {
+      //           href: "/benchmark/index.html",
+      //           label: "benchmark",
+      //           target: "_blank", // これがないと React router のせいでうまくいかないっぽい
+      //         },
+      //       ],
+      //     },
+      //   ],
+      //   // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // },
       prism: {
         // themes are configured in the createConfig() function below
         additionalLanguages: ["kotlin", "groovy", "latex"],
---
+++ website/src/pages/others.md
@@ -0,0 +1,9 @@
+---
+title: Others
+---
+
+## GitHub repository
+The library is an open-source software and its source code is distributed at this [GitHub repository](https://github.com/shwaka/kohomology). We welcome your contribution to the library.
+
+## Benchmark
+The performance of the library is observed by a <a href="/benchmark/index.html" target="_blank">benchmark</a> which is automatically run for every push.

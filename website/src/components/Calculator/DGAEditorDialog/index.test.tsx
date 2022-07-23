fdc253c572c5d063f672c7dfa7a4eb722a0b8c51
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-23 18:22:20 +0900

    test(website): writing test for useDGAEditorDialog (WIP)

---
+++ website/src/components/Calculator/DGAEditorDialog/index.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, screen, waitForElementToBeRemoved, within } from "@testing-library/react"
+import { renderHook, act } from "@testing-library/react-hooks"
+import React from "react"
+import { useDGAEditorDialog } from "."
+import { sphere } from "./examples"
+
+test("useDGAEditorDialog", async () => {
+  const json = sphere(2)
+  const updateDgaWrapper = (json: string): void => { console.log(json) }
+  const { result } = renderHook(() => useDGAEditorDialog(json, updateDgaWrapper))
+  const TabDialog = result.current.TabDialog
+  const { rerender } = render(<TabDialog {...result.current.tabDialogProps}/>)
+  act(() => {
+    result.current.openDialog()
+  })
+  rerender(<TabDialog {...result.current.tabDialogProps}/>)
+  const dialog = await screen.findByRole("dialog")
+  expect(dialog).toContainHTML("Array")
+  expect(dialog).toContainHTML("generator")
+})

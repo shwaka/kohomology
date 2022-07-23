94a7368d96995872657d4e46cac42a29829d91a4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-07-23 23:10:42 +0900

    test(website): test invalid input in ArrayEditor

--- website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor.test.tsx
+++ website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor.test.tsx
@@ -8,10 +8,18 @@ test("useTabItemArrayEditor", async () => {
   const json = sphere(2)
   const updateDgaWrapper = (json: string): void => { console.log(json) }
   const { result } = renderHook(() => useTabItemArrayEditor({ json, updateDgaWrapper }))
-  const { rerender } = render(result.current.render())
-  rerender(result.current.render())
+  const { rerender, container } = render(result.current.render())
   // check value of input elements
   const differentialValueInputs = screen.getAllByTestId("ArrayEditor-input-differentialValue")
   expect(differentialValueInputs[0]).toHaveValue("0")
   expect(differentialValueInputs[1]).toHaveValue("x^2")
+  // input invalid value
+  fireEvent.input(differentialValueInputs[1], { target: { value: "x" } })
+  const closeDialog = () => {}
+  await act(async () => {
+    result.current.onSubmit(closeDialog)
+  })
+  rerender(result.current.render())
+  const alert = screen.getByRole("alert")
+  expect(alert).toContainHTML("The degree of d(y) is expected to be deg(y)+1=4, but the given value x has degree 2.")
 })

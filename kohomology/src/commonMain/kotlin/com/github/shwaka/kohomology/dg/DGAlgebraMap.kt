8d94dc1b77079a5e1efd3a96a279d86640758ab1
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-08-06 00:35:20 +0900

    refactor(core): make DGAlgebraMap an interface (WIP)

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/DGAlgebraMap.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/DGAlgebraMap.kt
@@ -8,11 +8,11 @@ import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.vectsp.BasisName
 import com.github.shwaka.kohomology.vectsp.LinearMap
 
-public class DGAlgebraMap<D : Degree, BS : BasisName, BT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
-    override val source: DGAlgebra<D, BS, S, V, M>,
-    override val target: DGAlgebra<D, BT, S, V, M>,
-    override val gLinearMap: GAlgebraMap<D, BS, BT, S, V, M>,
-) : DGLinearMap<D, BS, BT, S, V, M>(source, target, gLinearMap) {
+public interface DGAlgebraMap<D : Degree, BS : BasisName, BT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> :
+    DGLinearMap<D, BS, BT, S, V, M>, GAlgebraMap<D, BS, BT, S, V, M> {
+    override val source: DGAlgebra<D, BS, S, V, M>
+    override val target: DGAlgebra<D, BT, S, V, M>
+
     public operator fun <BR : BasisName> times(other: DGAlgebraMap<D, BR, BS, S, V, M>): DGAlgebraMap<D, BR, BT, S, V, M> {
         require(other.target == this.source) {
             "Cannot composite dg linear maps since the source of $this and the target of $other are different"
@@ -20,7 +20,7 @@ public class DGAlgebraMap<D : Degree, BS : BasisName, BT : BasisName, S : Scalar
         return DGAlgebraMap(
             source = other.source,
             target = this.target,
-            gLinearMap = this.gLinearMap * other.gLinearMap,
+            gLinearMap = this * other,
         )
     }
 
@@ -28,11 +28,19 @@ public class DGAlgebraMap<D : Degree, BS : BasisName, BT : BasisName, S : Scalar
         public operator fun <D : Degree, BS : BasisName, BT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> invoke(
             source: DGAlgebra<D, BS, S, V, M>,
             target: DGAlgebra<D, BT, S, V, M>,
+            gLinearMap: GAlgebraMap<D, BS, BT, S, V, M>,
+        ): DGAlgebraMap<D, BS, BT, S, V, M> {
+            return DGAlgebraMapImpl(source, target, gLinearMap)
+        }
+
+        public operator fun <D : Degree, BS : BasisName, BT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> invoke(
+            source: DGAlgebra<D, BS, S, V, M>,
+            target: DGAlgebra<D, BT, S, V, M>,
             matrixSpace: MatrixSpace<S, V, M>,
             name: String,
             getLinearMap: (D) -> LinearMap<BS, BT, S, V, M>
         ): DGAlgebraMap<D, BS, BT, S, V, M> {
-            val gLinearMap = GAlgebraMap(source.gAlgebra, target.gAlgebra, matrixSpace, name, getLinearMap)
+            val gLinearMap = GAlgebraMap(source, target, matrixSpace, name, getLinearMap)
             return DGAlgebraMap(source, target, gLinearMap)
         }
 
@@ -43,8 +51,15 @@ public class DGAlgebraMap<D : Degree, BS : BasisName, BT : BasisName, S : Scalar
             name: String,
             getGVectors: (D) -> List<GVector<D, BT, S, V>>
         ): DGAlgebraMap<D, BS, BT, S, V, M> {
-            val gLinearMap = GAlgebraMap.fromGVectors(source.gAlgebra, target.gAlgebra, matrixSpace, name, getGVectors)
+            val gLinearMap = GAlgebraMap.fromGVectors(source, target, matrixSpace, name, getGVectors)
             return DGAlgebraMap(source, target, gLinearMap)
         }
     }
 }
+
+internal class DGAlgebraMapImpl<D : Degree, BS : BasisName, BT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
+    override val source: DGAlgebra<D, BS, S, V, M>,
+    override val target: DGAlgebra<D, BT, S, V, M>,
+    gLinearMap: GAlgebraMap<D, BS, BT, S, V, M>,
+) : DGAlgebraMap<D, BS, BT, S, V, M>,
+    DGLinearMap<D, BS, BT, S, V, M> by DGLinearMapImpl(source, target, gLinearMap)

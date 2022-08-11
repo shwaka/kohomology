9f6e4d2a64ae6ca1e4f835e403adbf329452965e
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-08-11 20:21:57 +0900

    refactor(core): improve arguments of SubQuotGAlgebraImpl.constructor (WIP)

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/SubQuotGAlgebra.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/SubQuotGAlgebra.kt
@@ -20,6 +20,15 @@ public interface SubQuotGAlgebra<D : Degree, B : BasisName, S : Scalar, V : NumV
     public companion object {
         public operator fun <D : Degree, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> invoke(
             matrixSpace: MatrixSpace<S, V, M>,
+            subQuotGVectorSpace: SubQuotGVectorSpace<D, B, S, V, M>,
+            multiplication: GBilinearMap<SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, D, S, V, M>,
+            unit: GVector<D, SubQuotBasis<B, S, V>, S, V>,
+        ): SubQuotGAlgebra<D, B, S, V, M> {
+            return SubQuotGAlgebraImpl(matrixSpace, subQuotGVectorSpace, multiplication, unit)
+        }
+
+        public operator fun <D : Degree, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> invoke(
+            matrixSpace: MatrixSpace<S, V, M>,
             degreeGroup: DegreeGroup<D>,
             name: String,
             getVectorSpace: (D) -> SubQuotVectorSpace<B, S, V, M>,
@@ -28,44 +37,47 @@ public interface SubQuotGAlgebra<D : Degree, B : BasisName, S : Scalar, V : NumV
             getInternalPrintConfig: (PrintConfig) -> InternalPrintConfig<SubQuotBasis<B, S, V>, S>,
             listDegreesForAugmentedDegree: ((Int) -> List<D>)? = null,
         ): SubQuotGAlgebra<D, B, S, V, M> {
-            return SubQuotGAlgebraImpl(
-                matrixSpace,
+            val subQuotGVectorSpace = SubQuotGVectorSpace(
+                matrixSpace.numVectorSpace,
                 degreeGroup,
                 name,
-                getVectorSpace,
-                getMultiplication,
-                unitVector,
                 getInternalPrintConfig,
                 listDegreesForAugmentedDegree,
+                getVectorSpace,
             )
+            val multiplication: GBilinearMap<SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, D, S, V, M> by lazy {
+                val bilinearMapName = "Multiplication($name)"
+                GBilinearMap(
+                    subQuotGVectorSpace,
+                    subQuotGVectorSpace,
+                    subQuotGVectorSpace,
+                    0,
+                    bilinearMapName,
+                ) { p, q -> getMultiplication(p, q) }
+            }
+            val unit = subQuotGVectorSpace.fromVector(unitVector, 0)
+            return SubQuotGAlgebraImpl(matrixSpace, subQuotGVectorSpace, multiplication, unit)
         }
     }
 }
 
 private class SubQuotGAlgebraImpl<D : Degree, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
-    matrixSpace: MatrixSpace<S, V, M>,
-    degreeGroup: DegreeGroup<D>,
-    name: String,
-    getVectorSpace: (D) -> SubQuotVectorSpace<B, S, V, M>,
-    getMultiplication: (D, D) -> BilinearMap<SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, S, V, M>,
-    unitVector: Vector<SubQuotBasis<B, S, V>, S, V>,
-    getInternalPrintConfig: (PrintConfig) -> InternalPrintConfig<SubQuotBasis<B, S, V>, S>,
-    listDegreesForAugmentedDegree: ((Int) -> List<D>)? = null,
+    override val matrixSpace: MatrixSpace<S, V, M>,
+    subQuotGVectorSpace: SubQuotGVectorSpace<D, B, S, V, M>,
+    override val multiplication: GBilinearMap<SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, D, S, V, M>,
+    override val unit: GVector<D, SubQuotBasis<B, S, V>, S, V>,
 ) : SubQuotGAlgebra<D, B, S, V, M>,
-    SubQuotGMagma<D, B, S, V, M> by SubQuotGMagma(matrixSpace, degreeGroup, name, getVectorSpace, getMultiplication, getInternalPrintConfig, listDegreesForAugmentedDegree) {
+    SubQuotGVectorSpace<D, B, S, V, M> by subQuotGVectorSpace {
     override val context: GAlgebraContext<D, SubQuotBasis<B, S, V>, S, V, M> by lazy {
         GAlgebraContextImpl(this)
     }
 
-    override val unit: GVector<D, SubQuotBasis<B, S, V>, S, V> by lazy {
-        this.fromVector(unitVector, 0)
-    }
-
     override fun getIdentity(): GAlgebraMap<D, SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, S, V, M> {
-        // If this method is implemented in the interface GAlgebra,
+        // If this method is implemented in the interface SubQuotGAlgebra,
         // a type error is thrown.
         return GAlgebraMap(this, this, this.matrixSpace, "id") { degree ->
             this[degree].getIdentity(this.matrixSpace)
         }
     }
+
 }

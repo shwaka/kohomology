c066f9c1beb047ed143cd698a2ab9afbd79022ff
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-08-12 20:37:11 +0900

    refactor(core): fix DGAlgebra (WIP)

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/DGAlgebra.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/DGAlgebra.kt
@@ -5,11 +5,8 @@ import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
-import com.github.shwaka.kohomology.util.InternalPrintConfig
-import com.github.shwaka.kohomology.util.PrintConfig
 import com.github.shwaka.kohomology.vectsp.BasisName
 import com.github.shwaka.kohomology.vectsp.SubQuotBasis
-import com.github.shwaka.kohomology.vectsp.SubQuotVectorSpace
 
 public interface DGAlgebraContext<D : Degree, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> :
     DGMagmaContext<D, B, S, V, M>, GAlgebraContext<D, B, S, V, M> {
@@ -38,6 +35,8 @@ public interface DGAlgebra<D : Degree, B : BasisName, S : Scalar, V : NumVector<
 internal open class DGAlgebraImpl<D : Degree, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
     open val gAlgebra: GAlgebra<D, B, S, V, M>,
     override val differential: Derivation<D, B, S, V, M>,
+    private val cohomologyGVectorSpace: SubQuotGVectorSpace<D, B, S, V, M>,
+    private val cohomologyMultiplication: GBilinearMap<SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, SubQuotBasis<B, S, V>, D, S, V, M>,
     matrixSpace: MatrixSpace<S, V, M>
 ) : DGMagma<D, B, S, V, M> by DGMagma(gAlgebra, differential),
     DGAlgebra<D, B, S, V, M> {
@@ -47,20 +46,12 @@ internal open class DGAlgebraImpl<D : Degree, B : BasisName, S : Scalar, V : Num
     override val unit: GVector<D, B, S, V> by lazy { gAlgebra.unit }
 
     override val cohomology: SubQuotGAlgebra<D, B, S, V, M> by lazy {
-        val cohomOfDeg0: SubQuotVectorSpace<B, S, V, M> = this.cohomology[0]
-        val cohomologyUnit = cohomOfDeg0.projection(this.gAlgebra.unit.vector)
-        val getInternalPrintConfig: (PrintConfig) -> InternalPrintConfig<SubQuotBasis<B, S, V>, S> = { printConfig: PrintConfig ->
-            SubQuotVectorSpace.convertInternalPrintConfig(printConfig, this.gAlgebra.getInternalPrintConfig(printConfig))
-        }
+        val cohomologyUnit = this.cohomologyClassOf(this.gAlgebra.unit)
         SubQuotGAlgebra(
             matrixSpace,
-            this.degreeGroup,
-            this.cohomologyName,
-            this.cohomology::get,
-            this.cohomology.multiplication,
+            this.cohomologyGVectorSpace,
+            this.cohomologyMultiplication,
             cohomologyUnit,
-            listDegreesForAugmentedDegree = this.gAlgebra.listDegreesForAugmentedDegree,
-            getInternalPrintConfig = getInternalPrintConfig
         )
     }
 

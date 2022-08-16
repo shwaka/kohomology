74dc525840dc77fe977574af6a15bd92a55966b2
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-08-16 14:04:34 +0900

    refactor(core): add tests to DGVectorSpaceTest (WIP)

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/DGVectorSpaceTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/dg/DGVectorSpaceTest.kt
@@ -9,7 +9,10 @@ import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
 import io.kotest.core.NamedTag
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.spec.style.freeSpec
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
 
 val dgVectorSpaceTag = NamedTag("DGVectorSpace")
 
@@ -21,9 +24,37 @@ fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> dgVectorSpaceTest(matrixSpa
         }
         val dgVectorSpace = DGVectorSpace.fromGVectorSpace(matrixSpace, gVectorSpace)
 
-        "dimension of the cohomology should be the same as that of the dg vector space" {
-            (-10 until 10).forAll { degree ->
-                dgVectorSpace.cohomology[degree].dim shouldBe dgVectorSpace[degree].dim
+        dgVectorSpace.context.run {
+            "dimension of the cohomology should be the same as that of the dg vector space" {
+                (-10 until 10).forAll { degree ->
+                    dgVectorSpace.cohomology[degree].dim shouldBe dgVectorSpace[degree].dim
+                }
+            }
+
+            "check classes" {
+                dgVectorSpace::class.simpleName shouldBe "DGVectorSpaceImpl"
+                dgVectorSpace.underlyingGVectorSpace::class.simpleName shouldBe "GVectorSpaceImpl"
+                dgVectorSpace.cohomology::class.simpleName shouldBe "SubQuotGVectorSpaceImpl"
+            }
+
+            "gVector should be an element of the dg vector space" {
+                val v = dgVectorSpace.getZero(0)
+                (v in dgVectorSpace).shouldBeTrue()
+                v.gVectorSpace shouldBeSameInstanceAs dgVectorSpace.underlyingGVectorSpace
+                v.gVectorSpace shouldNotBe dgVectorSpace
+            }
+
+            "gVector should be an element of the underlying graded vector space" {
+                val v = dgVectorSpace.getZero(0)
+                (v in dgVectorSpace.underlyingGVectorSpace).shouldBeTrue()
+                v.gVectorSpace.underlyingGVectorSpace shouldBeSameInstanceAs
+                    dgVectorSpace.underlyingGVectorSpace
+            }
+
+            "cohomology class should be an element of the cohomology" {
+                val v = dgVectorSpace.getZero(0)
+                (v.cohomologyClass() in dgVectorSpace.cohomology).shouldBeTrue()
+                v.cohomologyClass().gVectorSpace shouldBeSameInstanceAs dgVectorSpace.cohomology
             }
         }
     }

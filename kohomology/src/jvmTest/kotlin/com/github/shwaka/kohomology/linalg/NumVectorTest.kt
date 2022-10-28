df48ddac429c22b7f7094e1d05ea7b11e1db4913
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-10-28 18:42:17 +0900

    test(core): refactor and fix tests for positive characteristic

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/linalg/NumVectorTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/linalg/NumVectorTest.kt
@@ -6,6 +6,9 @@ import com.github.shwaka.kohomology.rationalTag
 import com.github.shwaka.kohomology.specific.DenseNumVectorSpaceOverF7
 import com.github.shwaka.kohomology.specific.DenseNumVectorSpaceOverIntRational
 import com.github.shwaka.kohomology.specific.DenseNumVectorSpaceOverRational
+import com.github.shwaka.kohomology.specific.SparseNumVectorSpaceOverF2
+import com.github.shwaka.kohomology.specific.SparseNumVectorSpaceOverF3
+import com.github.shwaka.kohomology.specific.SparseNumVectorSpaceOverF5
 import com.github.shwaka.kohomology.specific.SparseNumVectorSpaceOverF7
 import com.github.shwaka.kohomology.specific.SparseNumVectorSpaceOverIntRational
 import com.github.shwaka.kohomology.specific.SparseNumVectorSpaceOverRational
@@ -30,153 +33,161 @@ val sparseNumVectorTag = NamedTag("SparseNumVector")
 val kococoDebug = (System.getProperty("kococo.debug") != null)
 
 fun <S : Scalar> denseNumVectorTest(numVectorSpace: DenseNumVectorSpace<S>) = freeSpec {
-    "factory should return the cache if exists" {
-        val field = numVectorSpace.field
-        DenseNumVectorSpace.from(field) shouldBeSameInstanceAs numVectorSpace
+    "denseNumVectorTest for $numVectorSpace" - {
+        "factory should return the cache if exists" {
+            val field = numVectorSpace.field
+            DenseNumVectorSpace.from(field) shouldBeSameInstanceAs numVectorSpace
+        }
     }
 }
 
 fun <S : Scalar> sparseNumVectorTest(numVectorSpace: SparseNumVectorSpace<S>) = freeSpec {
-    "factory should return the cache if exists" {
-        val field = numVectorSpace.field
-        SparseNumVectorSpace.from(field) shouldBeSameInstanceAs numVectorSpace
-    }
+    "sparseNumVectorTest for $numVectorSpace" - {
+        "factory should return the cache if exists" {
+            val field = numVectorSpace.field
+            SparseNumVectorSpace.from(field) shouldBeSameInstanceAs numVectorSpace
+        }
 
-    "fromReducedValueMap should throw if zero is explicitly given (and if debug mode is enabled)".config(enabled = kococoDebug) {
-        numVectorSpace.field.context.run {
-            val valueMap: Map<Int, S> = mapOf(
-                1 to one,
-                3 to -two,
-                4 to zero,
-            )
-            shouldThrow<IllegalArgumentException> {
-                numVectorSpace.fromReducedValueMap(valueMap, 7)
+        "fromReducedValueMap should throw if zero is explicitly given (and if debug mode is enabled)".config(enabled = kococoDebug) {
+            numVectorSpace.field.context.run {
+                val valueMap: Map<Int, S> = mapOf(
+                    1 to one,
+                    3 to -two,
+                    4 to zero,
+                )
+                shouldThrow<IllegalArgumentException> {
+                    numVectorSpace.fromReducedValueMap(valueMap, 7)
+                }
             }
         }
-    }
 
-    numVectorSpace.context.run {
-        "valueMap for the vector (0, 0, 0) should be empty" {
-            val v = listOf(zero, zero, zero).toNumVector()
-            v.valueMap.shouldBeEmpty()
-            v.dim shouldBe 3
-        }
+        numVectorSpace.context.run {
+            "valueMap for the vector (0, 0, 0) should be empty" {
+                val v = listOf(zero, zero, zero).toNumVector()
+                v.valueMap.shouldBeEmpty()
+                v.dim shouldBe 3
+            }
 
-        "valueMap for the vector (0, 1, 0) should have size 1" {
-            val v = listOf(zero, one, zero).toNumVector()
-            v.valueMap.size shouldBe 1
-            v.dim shouldBe 3
-        }
+            "valueMap for the vector (0, 1, 0) should have size 1" {
+                val v = listOf(zero, one, zero).toNumVector()
+                v.valueMap.size shouldBe 1
+                v.dim shouldBe 3
+            }
 
-        "valueMap for (1, 0) + (-1, 0) should be empty" {
-            val v = listOf(one, zero).toNumVector()
-            val w = listOf(-one, zero).toNumVector()
-            (v + w).valueMap.shouldBeEmpty()
-        }
+            "valueMap for (1, 0) + (-1, 0) should be empty" {
+                val v = listOf(one, zero).toNumVector()
+                val w = listOf(-one, zero).toNumVector()
+                (v + w).valueMap.shouldBeEmpty()
+            }
 
-        "valueMap for (2, 1) * 0 should be empty" {
-            val v = listOf(two, one).toNumVector()
-            (v * zero).valueMap.shouldBeEmpty()
+            "valueMap for (2, 1) * 0 should be empty" {
+                val v = listOf(two, one).toNumVector()
+                (v * zero).valueMap.shouldBeEmpty()
+            }
         }
     }
 }
 
 fun <S : Scalar, V : NumVector<S>> numVectorTest(numVectorSpace: NumVectorSpace<S, V>) = freeSpec {
     numVectorSpace.context.run {
-        "numVectors with same values should have the same hashCode" {
-            val v1 = listOf(one, two).toNumVector()
-            val v2 = listOf(one, two).toNumVector()
-            v1 shouldNotBeSameInstanceAs v2
-            v1.hashCode() shouldBe v2.hashCode()
-        }
-        "(0, 1) + (0, 1) should be (0, 2)" {
-            val v = listOf(zero, one).toNumVector()
-            val w = listOf(zero, two).toNumVector()
-            (v + v) shouldBe w
-        }
-        "(2, 0) + (-2, 0) should be (0, 0)" {
-            val v = listOf(two, zero).toNumVector()
-            val w = listOf(-two, zero).toNumVector()
-            (v + w) shouldBe numVectorSpace.getZero(2)
-        }
-        "(-1, 0) - (-1, 0) should be (0, 0)" {
-            val v = listOf(-one, zero).toNumVector()
-            (v - v) shouldBe numVectorSpace.getZero(2)
-        }
-        "(1, 0) * 2 should be (2, 0)" {
-            val v = listOf(one, zero).toNumVector()
-            val w = listOf(two, zero).toNumVector()
-            (v * two) shouldBe w
-            (v * 2) shouldBe w
-        }
-        "2 * (1, 0) should be (2, 0)" {
-            val v = listOf(one, zero).toNumVector()
-            val w = listOf(two, zero).toNumVector()
-            (two * v) shouldBe w
-            (2 * v) shouldBe w
-        }
-        "-(1, -2, 0) should be (-1, 2, 0)" {
-            val v = listOf(one, -two, zero).toNumVector()
-            (-v) shouldBe listOf(-one, two, zero).toNumVector()
-        }
-        "multiplication with Sign" {
-            val v = listOf(one, -two, zero).toNumVector()
-            (v * Sign.PLUS) shouldBe v
-            (Sign.PLUS * v) shouldBe v
-            (v * Sign.MINUS) shouldBe -v
-            (Sign.MINUS * v) shouldBe -v
-        }
-        "(1, 0).dim should be 2" {
-            val v = listOf(one, zero).toNumVector()
-            v.dim shouldBe 2
-        }
-        "vectorSpace.getZero(3) should be (0, 0, 0)" {
-            val v = numVectorSpace.getZero(3)
-            val w = listOf(zero, zero, zero).toNumVector()
-            v shouldBe w
-        }
-        "(0, 0, 0) should be different from (0, 0)" {
-            val v = numVectorSpace.getZero(3)
-            val w = numVectorSpace.getZero(2)
-            v shouldNotBe w
-        }
-        "vectorSpace.fromValueList(emptyList()) and vectorSpace.getZero(0) should return the same element" {
-            val v = numVectorSpace.fromValueList(emptyList())
-            val w = numVectorSpace.getZero(0)
-            v shouldBe w
-        }
-        "(1,2) dot (-1, 3) should be 5" {
-            val v = listOf(one, two).toNumVector()
-            val w = listOf(-one, three).toNumVector()
-            (v dot w) shouldBe five
-        }
-        "(0, 0, 0).isZero() should be true" {
-            val v = listOf(zero, zero, zero).toNumVector()
-            v.isZero().shouldBeTrue()
-        }
-        "(0, 1, 0).isZero() should be false" {
-            val v = listOf(zero, one, zero).toNumVector()
-            v.isZero().shouldBeFalse()
-        }
-        "Map.toNumVector() should return the same numVector as List.toNumVector()" {
-            val v1 = mapOf(1 to one, 2 to two).toNumVector(4)
-            val v2 = listOf(zero, one, two, zero).toNumVector()
-            v1 shouldBe v2
-        }
-        "(0, 1, 2).toList() should be [0, 1, 2]" {
-            val v = listOf(zero, one, two).toNumVector()
-            v.toList() shouldBe listOf(zero, one, two)
-        }
-        "(0, 1, 2).toMap() should be {1: 1, 2: 2}" {
-            val v = listOf(zero, one, two).toNumVector()
-            v.toMap() shouldBe mapOf(1 to one, 2 to two)
-            v.toMap()[0].shouldBeNull()
-        }
-        "test index access" {
-            val v = listOf(zero, one, -two).toNumVector()
-            v[0] shouldBe zero
-            v[1] shouldBe one
-            v[2] shouldBe (-two)
+        "numVectorTest for $numVectorSpace" - {
+            "numVectors with same values should have the same hashCode" {
+                val v1 = listOf(one, two).toNumVector()
+                val v2 = listOf(one, two).toNumVector()
+                v1 shouldNotBeSameInstanceAs v2
+                v1.hashCode() shouldBe v2.hashCode()
+            }
+            "(0, 1) + (0, 1) should be (0, 2)" {
+                val v = listOf(zero, one).toNumVector()
+                val w = listOf(zero, two).toNumVector()
+                (v + v) shouldBe w
+            }
+            "(2, 0) + (-2, 0) should be (0, 0)" {
+                val v = listOf(two, zero).toNumVector()
+                val w = listOf(-two, zero).toNumVector()
+                (v + w) shouldBe numVectorSpace.getZero(2)
+            }
+            "(-1, 0) - (-1, 0) should be (0, 0)" {
+                val v = listOf(-one, zero).toNumVector()
+                (v - v) shouldBe numVectorSpace.getZero(2)
+            }
+            "(1, 0) * 2 should be (2, 0)" {
+                val v = listOf(one, zero).toNumVector()
+                val w = listOf(two, zero).toNumVector()
+                (v * two) shouldBe w
+                (v * 2) shouldBe w
+            }
+            "2 * (1, 0) should be (2, 0)" {
+                val v = listOf(one, zero).toNumVector()
+                val w = listOf(two, zero).toNumVector()
+                (two * v) shouldBe w
+                (2 * v) shouldBe w
+            }
+            "-(1, -2, 0) should be (-1, 2, 0)" {
+                val v = listOf(one, -two, zero).toNumVector()
+                (-v) shouldBe listOf(-one, two, zero).toNumVector()
+            }
+            "multiplication with Sign" {
+                val v = listOf(one, -two, zero).toNumVector()
+                (v * Sign.PLUS) shouldBe v
+                (Sign.PLUS * v) shouldBe v
+                (v * Sign.MINUS) shouldBe -v
+                (Sign.MINUS * v) shouldBe -v
+            }
+            "(1, 0).dim should be 2" {
+                val v = listOf(one, zero).toNumVector()
+                v.dim shouldBe 2
+            }
+            "vectorSpace.getZero(3) should be (0, 0, 0)" {
+                val v = numVectorSpace.getZero(3)
+                val w = listOf(zero, zero, zero).toNumVector()
+                v shouldBe w
+            }
+            "(0, 0, 0) should be different from (0, 0)" {
+                val v = numVectorSpace.getZero(3)
+                val w = numVectorSpace.getZero(2)
+                v shouldNotBe w
+            }
+            "vectorSpace.fromValueList(emptyList()) and vectorSpace.getZero(0) should return the same element" {
+                val v = numVectorSpace.fromValueList(emptyList())
+                val w = numVectorSpace.getZero(0)
+                v shouldBe w
+            }
+            "(1,2) dot (-1, 3) should be 5" {
+                val v = listOf(one, two).toNumVector()
+                val w = listOf(-one, three).toNumVector()
+                (v dot w) shouldBe five
+            }
+            "(0, 0, 0).isZero() should be true" {
+                val v = listOf(zero, zero, zero).toNumVector()
+                v.isZero().shouldBeTrue()
+            }
+            "(0, 1, 0).isZero() should be false" {
+                val v = listOf(zero, one, zero).toNumVector()
+                v.isZero().shouldBeFalse()
+            }
+            "Map.toNumVector() should return the same numVector as List.toNumVector()" {
+                val v1 = mapOf(1 to one, 2 to two).toNumVector(4)
+                val v2 = listOf(zero, one, two, zero).toNumVector()
+                v1 shouldBe v2
+            }
+            "(0, 1, 2).toList() should be [0, 1, 2]" {
+                val v = listOf(zero, one, two).toNumVector()
+                v.toList() shouldBe listOf(zero, one, two)
+            }
+            if (numVectorSpace.field.characteristic != 2) {
+                "(0, 1, 2).toMap() should be {1: 1, 2: 2}" {
+                    val v = listOf(zero, one, two).toNumVector()
+                    v.toMap() shouldBe mapOf(1 to one, 2 to two)
+                    v.toMap()[0].shouldBeNull()
+                }
+            }
+            "test index access" {
+                val v = listOf(zero, one, -two).toNumVector()
+                v[0] shouldBe zero
+                v[1] shouldBe one
+                v[2] shouldBe (-two)
+            }
         }
     }
 }
@@ -233,6 +244,12 @@ class RationalSparseNumVectorTest : FreeSpec({
 
 class IntModpSparseNumVectorTest : FreeSpec({
     tags(numVectorTag, sparseNumVectorTag, intModpTag)
+    include(sparseNumVectorTest(SparseNumVectorSpaceOverF2))
+    include(numVectorTest(SparseNumVectorSpaceOverF2))
+    include(sparseNumVectorTest(SparseNumVectorSpaceOverF3))
+    include(numVectorTest(SparseNumVectorSpaceOverF3))
+    include(sparseNumVectorTest(SparseNumVectorSpaceOverF5))
+    include(numVectorTest(SparseNumVectorSpaceOverF5))
     include(sparseNumVectorTest(SparseNumVectorSpaceOverF7))
     include(numVectorTest(SparseNumVectorSpaceOverF7))
 

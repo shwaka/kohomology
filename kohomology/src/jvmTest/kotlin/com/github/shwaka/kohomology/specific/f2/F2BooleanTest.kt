876933d3518b616ea889aab224f47c4fdfbe3109
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-10-29 16:38:42 +0900

    test(core): improve F2BooleanTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/specific/f2/F2BooleanTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/specific/f2/F2BooleanTest.kt
@@ -5,10 +5,23 @@ import com.github.shwaka.kohomology.specific.fieldTag
 import com.github.shwaka.kohomology.specific.fieldTest
 import com.github.shwaka.kohomology.specific.fromIntTest
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 
 class F2BooleanTest : FreeSpec({
     tags(fieldTag, intMod2BooleanTag)
 
     include(fromIntTest(F2Boolean))
     include(fieldTest(F2Boolean))
+
+    F2Boolean.context.run {
+        "fromInt(n).isZero() should be true iff n is even" {
+            fromInt(-2).isZero().shouldBeTrue()
+            fromInt(-1).isZero().shouldBeFalse()
+            fromInt(0).isZero().shouldBeTrue()
+            fromInt(1).isZero().shouldBeFalse()
+            fromInt(2).isZero().shouldBeTrue()
+            fromInt(3).isZero().shouldBeFalse()
+        }
+    }
 })

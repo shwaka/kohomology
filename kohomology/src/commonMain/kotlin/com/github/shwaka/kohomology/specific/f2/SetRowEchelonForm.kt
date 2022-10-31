bdd5188b6182c70bd1a26d0cdb5aabf8ef1c9e40
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-10-31 11:48:11 +0900

    feat(core): add SetRowEchelonForm (WIP)

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/specific/f2/SetRowEchelonForm.kt
@@ -0,0 +1,72 @@
+package com.github.shwaka.kohomology.specific.f2
+
+import com.github.shwaka.kohomology.linalg.RowEchelonForm
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.util.Sign
+
+internal data class SetRowEchelonFormData<S : Scalar>(
+    val rowMap: Map<Int, Set<Int>>,
+    val pivots: List<Int>,
+    val exchangeCount: Int,
+)
+
+internal class SetRowEchelonForm<S : Scalar>(
+    matrixSpace: SetMatrixSpace<S>,
+    originalMatrix: SetMatrix<S>,
+) : RowEchelonForm<S, SetNumVector<S>, SetMatrix<S>>(matrixSpace, originalMatrix) {
+    override fun computeRowEchelonForm(): SetMatrix<S> {
+        TODO("Not yet implemented")
+    }
+
+    override fun computePivots(): List<Int> {
+        TODO("Not yet implemented")
+    }
+
+    override fun computeSign(): Sign {
+        TODO("Not yet implemented")
+    }
+
+    override fun computeReducedRowEchelonForm(): SetMatrix<S> {
+        TODO("Not yet implemented")
+    }
+
+    private tailrec fun Map<Int, Set<Int>>.rowEchelonFormInternal(
+        currentColInd: Int,
+        pivots: List<Int>,
+        exchangeCount: Int,
+        colCount: Int,
+    ): SetRowEchelonFormData<S> {
+        TODO()
+    }
+
+    private fun Map<Int, Set<Int>>.exchangeRows(i1: Int, i2: Int): Map<Int, Set<Int>> {
+        require(i1 != i2) { "Row numbers must be distinct" }
+        return this.mapKeys { (i, _) ->
+            when (i) {
+                i1 -> i2
+                i2 -> i1
+                else -> i
+            }
+        }
+    }
+
+    private operator fun Set<Int>.minus(other: Set<Int>): Set<Int> {
+        return this xor other
+    }
+
+    private operator fun Set<Int>.times(scalar: S): Set<Int> {
+        return if (scalar.isZero()) {
+            emptySet()
+        } else {
+            this
+        }
+    }
+
+    private fun Map<Int, Set<Int>>.eliminateOtherRows(rowInd: Int, colInd: Int): Map<Int, Set<Int>> {
+        TODO()
+    }
+
+    private fun Map<Int, Set<Int>>.findNonZero(colInd: Int, rowIndFrom: Int): Int? {
+        TODO()
+    }
+}

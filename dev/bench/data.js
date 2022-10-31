50ca48afe750e10c5502969fb4d532db6eb81188
github-action-benchmark <github@users.noreply.github.com>
2022-10-31 11:17:55 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8d00c83f4011f2932b493e380878ff5f715c449a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667209144874,
+  "lastUpdate": 1667215075618,
   "repoUrl": "https://github.com/shwaka/kohomology",
   "entries": {
     "Benchmark": [
@@ -8051,6 +8051,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "com.github.shwaka.kohomology.profile.KohomologyBenchmark.isomorphismToCohomologyOfFreePathSpace",
             "value": 0.1858947437485119,
+            "unit": "s/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shun.wakatsuki@gmail.com",
+            "name": "Shun Wakatsuki",
+            "username": "shwaka"
+          },
+          "committer": {
+            "email": "shun.wakatsuki@gmail.com",
+            "name": "Shun Wakatsuki",
+            "username": "shwaka"
+          },
+          "distinct": true,
+          "id": "8d00c83f4011f2932b493e380878ff5f715c449a",
+          "message": "perf(core): improve performance of Set<T>.xor",
+          "timestamp": "2022-10-31T19:26:53+09:00",
+          "tree_id": "00b7e009e9c8651b04b81f6be320a6ddf231f40b",
+          "url": "https://github.com/shwaka/kohomology/commit/8d00c83f4011f2932b493e380878ff5f715c449a"
+        },
+        "date": 1667215075036,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "com.github.shwaka.kohomology.profile.KohomologyBenchmark.cohomologyOfFreeLoopSpace",
+            "value": 0.08043752308696735,
+            "unit": "s/op"
+          },
+          {
+            "name": "com.github.shwaka.kohomology.profile.KohomologyBenchmark.cohomologyOfFreeLoopSpaceWithMultiDegree",
+            "value": 0.10367683578402709,
+            "unit": "s/op"
+          },
+          {
+            "name": "com.github.shwaka.kohomology.profile.KohomologyBenchmark.cohomologyOfFreeLoopSpaceWithMultiDegreeWithShiftDegree",
+            "value": 0.13164018481309522,
+            "unit": "s/op"
+          },
+          {
+            "name": "com.github.shwaka.kohomology.profile.KohomologyBenchmark.isomorphismToCohomologyOfFreePathSpace",
+            "value": 0.14239901989353446,
             "unit": "s/op"
           }
         ]

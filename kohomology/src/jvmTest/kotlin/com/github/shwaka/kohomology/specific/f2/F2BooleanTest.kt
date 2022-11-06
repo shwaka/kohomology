de9efddd8ec5b90a4076cab65d5225b6aa9aa4a8
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-11-06 18:44:18 +0900

    test(core): add finiteFieldTest to F2BooleanTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/specific/f2/F2BooleanTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/specific/f2/F2BooleanTest.kt
@@ -3,6 +3,7 @@ package com.github.shwaka.kohomology.specific.f2
 import com.github.shwaka.kohomology.intMod2BooleanTag
 import com.github.shwaka.kohomology.specific.fieldTag
 import com.github.shwaka.kohomology.specific.fieldTest
+import com.github.shwaka.kohomology.specific.finiteFieldTest
 import com.github.shwaka.kohomology.specific.fromIntTest
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
@@ -13,6 +14,7 @@ class F2BooleanTest : FreeSpec({
 
     include(fromIntTest(F2Boolean))
     include(fieldTest(F2Boolean))
+    include(finiteFieldTest(F2Boolean))
 
     F2Boolean.context.run {
         "fromInt(n).isZero() should be true iff n is even" {

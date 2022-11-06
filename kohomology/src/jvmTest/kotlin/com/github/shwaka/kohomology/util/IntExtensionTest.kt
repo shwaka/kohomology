9cadeb3d5dd95c946f503ee8c9cbeab4c67bcff5
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-11-06 18:35:52 +0900

    test(core): add tests to IntExtensionTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/util/IntExtensionTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/util/IntExtensionTest.kt
@@ -4,6 +4,7 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.NamedTag
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.inspectors.forAll
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
@@ -15,6 +16,75 @@ val intExtensionTag = NamedTag("IntExtension")
 
 class IntExtensionTest : FreeSpec({
     tags(intExtensionTag)
+
+    "test Int.pow(exponent)" - {
+        "1.pow(exponent) should be 1" {
+            Arb.int(0, Int.MAX_VALUE).forAll { exponent ->
+                1.pow(exponent) == 1
+            }
+        }
+        "n.pow(0) should be 1" {
+            Arb.int(0, Int.MAX_VALUE).forAll { base ->
+                base.pow(0) == 1
+            }
+        }
+        "n.pow(1) should be n" {
+            Arb.int(0, Int.MAX_VALUE).forAll { base ->
+                base.pow(1) == base
+            }
+        }
+        "check for valid arguments" {
+            listOf(
+                Triple(2, 0, 1),
+                Triple(2, 2, 4),
+                Triple(2, 3, 8),
+                Triple(3, 3, 27),
+                Triple(3, 4, 81),
+                Triple(-2, 0, 1),
+                Triple(-2, 1, -2),
+                Triple(-2, 2, 4),
+                Triple(-2, 3, -8),
+            ).forAll { (base, exponent, expected) ->
+                base.pow(exponent) shouldBe expected
+            }
+        }
+        "check error" {
+            shouldThrow<ArithmeticException> {
+                1.pow(-1)
+            }
+            shouldThrow<ArithmeticException> {
+                1.pow(-2)
+            }
+        }
+    }
+
+    "test Int.isEven() and Int.isOdd()" {
+        Arb.int().forAll { n -> (2 * n).isEven() }
+        Arb.int().forAll { n -> !(2 * n).isOdd() }
+        Arb.int().forAll { n -> !(2 * n + 1).isEven() }
+        Arb.int().forAll { n -> (2 * n + 1).isOdd() }
+    }
+
+    "test Int.isPrime()" {
+        listOf(
+            Pair(0, false),
+            Pair(1, false),
+            Pair(2, true),
+            Pair(3, true),
+            Pair(4, false),
+            Pair(5, true),
+            Pair(6, false),
+            Pair(7, true),
+            Pair(8, false),
+            Pair(9, false),
+            Pair(11, true),
+            Pair(13, true),
+            Pair(17, true),
+            Pair(57, false),
+        ).forAll { (p, expected) ->
+            p.isPrime() shouldBe expected
+        }
+    }
 
     "test Int.isPowerOf(n)" - {
         "n.isPowerOf(n) must be true" {

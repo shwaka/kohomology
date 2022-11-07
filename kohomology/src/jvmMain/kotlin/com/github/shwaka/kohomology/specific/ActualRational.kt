7ef724ea943cd6bbabbea12b8fec08bc14938ae6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-11-07 17:50:08 +0900

    feat(core): use expect/actual to select JavaRational in JVM (close #247)

---
+++ kohomology/src/jvmMain/kotlin/com/github/shwaka/kohomology/specific/ActualRational.kt
@@ -0,0 +1,4 @@
+package com.github.shwaka.kohomology.specific
+
+public actual typealias Rational = JavaRational
+public actual typealias RationalField = JavaRationalField
---
+++ kohomology/src/nativeMain/kotlin/com/github/shwaka/kohomology/specific/ActualRational.kt
@@ -0,0 +1,4 @@
+package com.github.shwaka.kohomology.specific
+
+public actual typealias Rational = KotlinRational
+public actual typealias RationalField = KotlinRationalField

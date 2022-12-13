4a6039b8f41a8b188721cf7978d9c43668010ddc
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-12-14 02:52:27 +0900

    style(website): eslint

--- website/src/components/ComparisonChart/colors.ts
+++ website/src/components/ComparisonChart/colors.ts
@@ -1,17 +1,17 @@
 // Copied from https://github.com/chartjs/Chart.js/blob/master/src/plugins/plugin.colors.ts
 
 const BORDER_COLORS = [
-  'rgb(54, 162, 235)', // blue
-  'rgb(255, 99, 132)', // red
-  'rgb(255, 159, 64)', // orange
-  'rgb(255, 205, 86)', // yellow
-  'rgb(75, 192, 192)', // green
-  'rgb(153, 102, 255)', // purple
-  'rgb(201, 203, 207)' // grey
+  "rgb(54, 162, 235)", // blue
+  "rgb(255, 99, 132)", // red
+  "rgb(255, 159, 64)", // orange
+  "rgb(255, 205, 86)", // yellow
+  "rgb(75, 192, 192)", // green
+  "rgb(153, 102, 255)", // purple
+  "rgb(201, 203, 207)" // grey
 ]
 
 // Border colors with 50% transparency
-const BACKGROUND_COLORS = BORDER_COLORS.map(color => color.replace('rgb(', 'rgba(').replace(')', ', 0.5)'))
+const BACKGROUND_COLORS = BORDER_COLORS.map(color => color.replace("rgb(", "rgba(").replace(")", ", 0.5)"))
 
 export function getBorderColor(i: number): string {
   return BORDER_COLORS[i % BORDER_COLORS.length]

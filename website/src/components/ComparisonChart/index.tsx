ad48cde6254093620b2b6b80453aa07bca14e3a1
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-12-14 19:05:15 +0900

    refactor(website): use Scatter Chart in ComparisonChart

--- website/src/components/ComparisonChart/index.tsx
+++ website/src/components/ComparisonChart/index.tsx
@@ -8,21 +8,28 @@ import { Target, tools, Tool } from "./comparisonKeys"
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend, Title, LineController)
 
-function getData(target: Target): ChartData<"line", number[], string> {
+function zip<T1, T2>(array1: T1[], array2: T2[]): [T1, T2][] {
+  return array1.map((t1, i) => [t1, array2[i]])
+}
+
+function getData(target: Target): ChartData<"scatter", { x: number, y: number }[], string> {
   return {
-    labels: comparisonData.kohomology.benchmark[target].degrees.map((degree) => `n=${degree}`),
-    datasets: tools.map((tool, i) => ({
-      label: tool,
-      data: comparisonData[tool].benchmark[target].time,
-      borderColor: getBorderColor(i),
-      backgroundColor: getBackgroundColor(i),
-    })),
+    datasets: tools.map((tool, i) => {
+      const data: { time: number[], degrees: number[] } = comparisonData[tool].benchmark[target]
+      return {
+        label: tool,
+        data: zip(data.degrees, data.time).map(([x, y]) => ({ x, y })),
+        showLine: true,
+        borderColor: getBorderColor(i),
+        backgroundColor: getBackgroundColor(i),
+      }
+    }),
   }
 }
 
 export function ComparisonChart({ target }: { target: Target }): JSX.Element {
   const data = getData(target)
-  const options: ChartProps<"line", number[], string>["options"] = {
+  const options: ChartProps<"scatter", { x: number, y: number }[], string>["options"] = {
     plugins: {
       title: {
         display: true,
@@ -40,7 +47,7 @@ export function ComparisonChart({ target }: { target: Target }): JSX.Element {
   }
   return (
     <Chart
-      type="line"
+      type="scatter"
       data={data}
       options={options}
     />

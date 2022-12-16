ca151e14c84c1813779a29c790a797060c1ad959
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-12-16 23:56:30 +0900

    chore: show current versions in update-gradle.sh

--- scripts/update-gradle.sh
+++ scripts/update-gradle.sh
@@ -4,11 +4,6 @@ set -euo pipefail
 cd "$(git rev-parse --show-toplevel)" # go to the root of the repository
 
 version=${1-}
-if [ -z "$version" ]; then
-    echo "[Error] Input gradle version" >&2
-    echo "Usage: ./update-gradle.sh 99.9.9" >&2
-    exit 1
-fi
 
 gradle_projects=(
     kohomology
@@ -19,7 +14,27 @@ gradle_projects=(
     website/sample
 )
 
-for d in "${gradle_projects[@]}"; do
-    f=$d/gradle/wrapper/gradle-wrapper.properties
-    sed -i -E "s#(distributionUrl=.*/gradle-)[0-9.]+(-bin.zip)#\\1$version\\2#" "$f"
-done
+function show_versions() {
+    echo "Current versions are:"
+    for d in "${gradle_projects[@]}"; do
+        f=$d/gradle/wrapper/gradle-wrapper.properties
+        current_ver=$(sed -n -E 's#^.*/gradle-([0-9.]+)-bin\.zip#\1#p' < "$f")
+        echo "$current_ver in $d"
+    done
+}
+
+function update() {
+    for d in "${gradle_projects[@]}"; do
+        f=$d/gradle/wrapper/gradle-wrapper.properties
+        sed -i -E "s#(distributionUrl=.*/gradle-)[0-9.]+(-bin.zip)#\\1$version\\2#" "$f"
+    done
+}
+
+if [ -z "$version" ]; then
+    echo "[Error] Input gradle version" >&2
+    echo "Usage: ./update-gradle.sh 99.9.9" >&2
+    show_versions >&2
+    exit 1
+else
+    update
+fi

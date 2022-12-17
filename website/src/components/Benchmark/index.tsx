80723d68cacedfc5a68451e8d93c82dd9a531677
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-12-18 00:23:33 +0900

    feat(website): add declare global { interface Window { } } to Benchmark/index.tsx

--- website/src/components/Benchmark/index.tsx
+++ website/src/components/Benchmark/index.tsx
@@ -1,11 +1,18 @@
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend, Title, LineController, ScatterController } from "chart.js"
 import React from "react"
 import { Chart } from "react-chartjs-2"
+import { BenchmarkData } from "./BenchmarkData"
 import { getChartArgument, collectBenchesPerTestCase } from "./benchmark"
 
 import "@benchmark/data"
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend, Title, LineController, ScatterController)
+
+declare global {
+  interface Window {
+    BENCHMARK_DATA: BenchmarkData
+  }
+}
 
 function Bench({ name, dataset }): JSX.Element {
   const arg = getChartArgument(name, dataset)
@@ -29,10 +36,10 @@ function Benchset({ name, benchset }): JSX.Element {
 }
 
 export function Benchmark(): JSX.Element {
-  const data = window.BENCHMARK_DATA
-  const benchmarks = Object.keys(data.entries).map(name => ({
+  const benchmarkData = window.BENCHMARK_DATA
+  const benchmarks = Object.keys(benchmarkData.entries).map(name => ({
     name,
-    benchset: collectBenchesPerTestCase(data.entries[name]),
+    benchset: collectBenchesPerTestCase(benchmarkData.entries[name]),
   }))
   return (
     <div>

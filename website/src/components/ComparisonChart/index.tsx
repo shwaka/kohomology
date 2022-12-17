38dcd89ea2fb1e06fe6751ec9393b8b3d27fa47e
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-12-17 18:12:27 +0900

    refactor(website): split getData to getData and getDataForTarget in ComparisonChart

--- website/src/components/ComparisonChart/index.tsx
+++ website/src/components/ComparisonChart/index.tsx
@@ -12,23 +12,28 @@ function zip<T1, T2>(array1: T1[], array2: T2[]): [T1, T2][] {
   return array1.map((t1, i) => [t1, array2[i]])
 }
 
-function getData(target: Target): ChartData<"scatter", { x: number, y: number }[], string> {
+function getData(
+  target: Target, tool: Tool, borderColor: string, backgroundColor: string
+): ChartData<"scatter", { x: number, y: number }[], string>["datasets"][number] {
+  const dataForLine: { time: number[], degrees: number[] } = comparisonData[tool].benchmark[target]
   return {
-    datasets: tools.map((tool, i) => {
-      const dataForLine: { time: number[], degrees: number[] } = comparisonData[tool].benchmark[target]
-      return {
-        label: tool,
-        data: zip(dataForLine.degrees, dataForLine.time).map(([x, y]) => ({ x, y })),
-        showLine: true,
-        borderColor: getBorderColor(i),
-        backgroundColor: getBackgroundColor(i),
-      }
-    }),
+    label: tool,
+    data: zip(dataForLine.degrees, dataForLine.time).map(([x, y]) => ({ x, y })),
+    showLine: true,
+    borderColor: borderColor,
+    backgroundColor: backgroundColor,
   }
 }
 
+function getDataForTarget(target: Target): ChartData<"scatter", { x: number, y: number }[], string> {
+  const datasets = tools.map((tool, i) => getData(
+    target, tool, getBorderColor(i), getBackgroundColor(i)
+  ))
+  return { datasets }
+}
+
 export function ComparisonChart({ target }: { target: Target }): JSX.Element {
-  const data = getData(target)
+  const data = getDataForTarget(target)
   const options: ChartProps<"scatter", { x: number, y: number }[], string>["options"] = {
     plugins: {
       title: {

9f220fd67642ea637337b4ae1126933b444e21c9
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-12-18 12:18:44 +0900

    refactor(website): adding types to benchmark.ts

--- website/src/components/BenchmarkChart/BenchmarkData.ts
+++ website/src/components/BenchmarkChart/BenchmarkData.ts
@@ -1,30 +1,32 @@
-type Tool = "cargo" | "go" | "benchmarkjs" | "pytest" | "googlecpp" | "catch2" | "julia" | "jmh" | "benchmarkdotnet" |"benchmarkluau" | "customBiggerIsBetter" | "customSmallerIsBetter"
+export type Tool = "cargo" | "go" | "benchmarkjs" | "pytest" | "googlecpp" | "catch2" | "julia" | "jmh" | "benchmarkdotnet" |"benchmarkluau" | "customBiggerIsBetter" | "customSmallerIsBetter"
 
-interface Bench {
+export interface Bench {
   name: string
   value: number
   unit: string
 }
 
-interface Benchmark {
-  commit: {
-    author: {
-      email: string
-      name: string
-      username: string
-    }
-    committer: {
-      email: string
-      name: string
-      username: string
-    }
-    distinct: boolean
-    id: string
-    message: string
-    timestamp: string
-    tree_id: string
-    url: string
+export interface Commit {
+  author: {
+    email: string
+    name: string
+    username: string
   }
+  committer: {
+    email: string
+    name: string
+    username: string
+  }
+  distinct: boolean
+  id: string
+  message: string
+  timestamp: string
+  tree_id: string
+  url: string
+}
+
+export interface Benchmark {
+  commit: Commit
   date: number
   tool: Tool
   benches: Bench[]

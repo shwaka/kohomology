69fb28e9fbe7f5216b34448235fe2964ce215fa4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2022-12-27 09:46:57 +0900

    fix(website): fix GoToPublishedPage

--- website/src/components/NavbarItems/GoToPublishedPage.tsx
+++ website/src/components/NavbarItems/GoToPublishedPage.tsx
@@ -1,6 +1,7 @@
-import Link from "@docusaurus/Link"
 import { useLocation } from "@docusaurus/router"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import NavbarItem from "@theme-original/NavbarItem"
+import type { Props } from "@theme/NavbarItem"
 import React from "react"
 
 function isDevelopmentMode(): boolean {
@@ -8,22 +9,26 @@ function isDevelopmentMode(): boolean {
   return process.env.NODE_ENV === "development"
 }
 
-function GoToPublishedPageOnDevMode(): JSX.Element {
+function GoToPublishedPageOnDevMode(props: Props): JSX.Element {
+  // On mobile devices, props contains { mobile: true, onClick: (some function) }.
+  // We need to pass these to NavbarItem to render the item correctly.
   const context = useDocusaurusContext()
   const location = useLocation()
   const url = context.siteConfig.url
   const pathname = location.pathname
   return (
-    <Link to={`${url}${pathname}`}>
-      Published page
-    </Link>
+    <NavbarItem
+      {...props}
+      href={`${url}${pathname}`}
+      label="Published page"
+    />
   )
 }
 
-export function GoToPublishedPage(): JSX.Element {
+export function GoToPublishedPage(props: Props): JSX.Element {
   return (
     isDevelopmentMode()
-      ? <GoToPublishedPageOnDevMode/>
+      ? <GoToPublishedPageOnDevMode {...props}/>
       : <></>
   )
 }

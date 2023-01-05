40807fab6a41160d167021c9c9bc842d310904ca
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-01-05 12:00:16 +0900

    test(website): add @ts-expect-error to the line importing jest-extended

--- website/jest-setup.ts
+++ website/jest-setup.ts
@@ -1,4 +1,8 @@
 import "@testing-library/jest-dom"
+// @ts-expect-error because the following error occurs:
+//   error TS2306: File '/path/to/website/node_modules/jest-extended/types/index.d.ts' is not a module.
+// Workarounds in https://github.com/jest-community/jest-extended/issues/367 did not work here. (Why?)
+// This error is ignored since LSP works well in test files.
 import * as matchers from "jest-extended"
 
 expect.extend(matchers)

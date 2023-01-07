3b0411cb282f00e76fe5a7261072ba71f8bc3f4a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-01-07 19:54:26 +0900

    refactor(website): use sx prop of Paper instead of css

--- website/src/components/Calculator/index.tsx
+++ website/src/components/Calculator/index.tsx
@@ -57,10 +57,20 @@ export function Calculator(): JSX.Element {
       <div className={styles.calculator} data-testid="Calculator">
         <CalculatorForm printMessages={addMessages} defaultDGAJson={defaultDGAJson}/>
         <Paper
-          elevation={0}
-          classes={{ root: styles.calculatorResults }}
+          elevation={0} variant="outlined"
           ref={scrollRef}
           data-testid="calculator-results"
+          sx={{
+            width: "700px",
+            height: "700px",
+            overflowY: "scroll",
+            padding: "5px",
+            "@media screen and (max-width: 700px)": {
+              width: "100%",
+              height: "50vh",
+              margin: "3px",
+            }
+          }}
         >
           {messages.map((message, index) => <ShowStyledMessage styledMessage={message} key={index}/>)}
         </Paper>

79e8bad2fb3a42392e418b12ee2a295824531895
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-03-21 13:57:51 +0900

    fix: use FreeDGAlgebra.fromList in template/

--- template/src/main/kotlin/main.kt
+++ template/src/main/kotlin/main.kt
@@ -10,7 +10,7 @@ fun main() {
         Indeterminate("y", sphereDim * 2 - 1)
     )
     val matrixSpace = SparseMatrixSpaceOverRational
-    val sphere = FreeDGAlgebra(matrixSpace, indeterminateList) { (x, y) ->
+    val sphere = FreeDGAlgebra.fromList(matrixSpace, indeterminateList) { (x, y) ->
         listOf(zeroGVector, x.pow(2))
     }
     val freeLoopSpace = FreeLoopSpace(sphere)

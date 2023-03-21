c64b39018be28cdcf684c9c949cfaf3ba0b698f7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-03-21 13:41:19 +0900

    refactor: fix bump-version.sh according to shellcheck

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-cd $(git rev-parse --show-toplevel) # go to the root of the repository
+cd "$(git rev-parse --show-toplevel)" # go to the root of the repository
 
 VERSION_REGEX='^version = "\(.*\)"$'
 BUILD_GRADLE_KTS=kohomology/build.gradle.kts
@@ -12,8 +12,7 @@ function show_usage() {
     echo "Usage:" >&2
     echo "  ./bump-version.sh release 99.9" >&2
     echo "  ./bump-version.sh snapshot 99.9" >&2
-    current_version=$(cat $BUILD_GRADLE_KTS |
-                          grep "$VERSION_REGEX" |
+    current_version=$(grep "$VERSION_REGEX" < $BUILD_GRADLE_KTS |
                           sed s/"$VERSION_REGEX"/\\1/)
     echo "Current version: $current_version" >&2
     tags=$(git tag | tac | tr "\n" " ")
@@ -23,8 +22,8 @@ function show_usage() {
 function update_build_gradle_kts() {
     version=$1
 
-    echo sed -i s/"$VERSION_REGEX"/"version = \"$version\""/ $BUILD_GRADLE_KTS
-    sed -i s/"$VERSION_REGEX"/"version = \"$version\""/ $BUILD_GRADLE_KTS
+    echo sed -i "s/$VERSION_REGEX/version = \"$version\"/" $BUILD_GRADLE_KTS
+    sed -i "s/$VERSION_REGEX/version = \"$version\"/" $BUILD_GRADLE_KTS
     git add $BUILD_GRADLE_KTS
 }
 
@@ -35,17 +34,19 @@ function update_implementation() {
         echo "[ERROR] File does not exist: $file" >&2
         exit 1
     fi
-    echo sed -i 's/implementation("com.github.shwaka.kohomology:kohomology:.*")$/implementation("com.github.shwaka.kohomology:kohomology:'$version'")/g' $file
-    sed -i 's/implementation("com.github.shwaka.kohomology:kohomology:.*")$/implementation("com.github.shwaka.kohomology:kohomology:'$version'")/g' $file
-    echo sed -i "s/implementation 'com.github.shwaka.kohomology:kohomology:.*'$/implementation 'com.github.shwaka.kohomology:kohomology:$version'/g" $file
-    sed -i "s/implementation 'com.github.shwaka.kohomology:kohomology:.*'$/implementation 'com.github.shwaka.kohomology:kohomology:$version'/g" $file
-    git add $file
+    echo sed -i "s/implementation(\"com.github.shwaka.kohomology:kohomology:.*\")$/implementation(\"com.github.shwaka.kohomology:kohomology:$version\")/g" "$file"
+    sed -i "s/implementation(\"com.github.shwaka.kohomology:kohomology:.*\")$/implementation(\"com.github.shwaka.kohomology:kohomology:$version\")/g" "$file"
+    echo sed -i "s/implementation 'com.github.shwaka.kohomology:kohomology:.*'$/implementation 'com.github.shwaka.kohomology:kohomology:$version'/g" "$file"
+    sed -i "s/implementation 'com.github.shwaka.kohomology:kohomology:.*'$/implementation 'com.github.shwaka.kohomology:kohomology:$version'/g" "$file"
+    git add "$file"
 }
 
 function setup_java() {
     local sdkman_init="$HOME/.sdkman/bin/sdkman-init.sh"
     local java_version=8.0.302-open
     set +u
+    # https://www.shellcheck.net/wiki/SC1090
+    # shellcheck source=/dev/null
     source "$sdkman_init"
     sdk use java $java_version
     set -u
@@ -60,7 +61,7 @@ function release_version() {
 
     for d in profile website/sample website/comparison/kohomology template; do
         local kts=$d/build.gradle.kts
-        update_implementation $version $kts
+        update_implementation "$version" $kts
     done
 
     if ! select_yn "Do you want to git-commit? (y/n)"; then
@@ -69,7 +70,7 @@ function release_version() {
     git commit --no-verify -m "Release v$version"
     git tag -a "v$version" -m "Version $version"
 
-    read -p "Do you want to publish? (y/n)" answer
+    read -rp "Do you want to publish? (y/n)" answer
     if [ "$answer" = n ]; then return; fi
     cd kohomology
     setup_java
@@ -82,7 +83,7 @@ function bump_snapshot_version() {
     update_build_gradle_kts "$snapshot_version"
     for d in profile website/kohomology-js; do
         local kts=$d/build.gradle.kts
-        update_implementation $snapshot_version $kts
+        update_implementation "$snapshot_version" $kts
     done
 
     if ! select_yn "Do you want to git-commit? (y/n)"; then
@@ -94,7 +95,7 @@ function bump_snapshot_version() {
 function select_yn() {
     local msg=$1
     while true; do
-        read -p "$msg" yn
+        read -rp "$msg" yn
         case $yn in
             [Yy]* ) return 0;;
             [Nn]* ) return 1;;
@@ -103,7 +104,7 @@ function select_yn() {
     done
 }
 
-if [ -z "${1-}" -o -z "${2-}" ]; then
+if [ -z "${1-}" ] || [ -z "${2-}" ]; then
     show_usage
     exit 1
 fi

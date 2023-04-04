226c549ef784d5f1d613c90e82a05047586d9c51
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-04-04 18:39:21 +0900

    test(core): add test for product of simplicial complexes

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/simplicial/ProductSimplicialComplexTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/simplicial/ProductSimplicialComplexTest.kt
@@ -1,6 +1,8 @@
 package com.github.shwaka.kohomology.simplicial
 
 import com.github.shwaka.kohomology.specific.DenseMatrixSpaceOverRational
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
+import com.github.shwaka.kohomology.specific.f2.SetMatrixSpaceOverF2Boolean
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
@@ -19,6 +21,34 @@ class ProductSimplicialComplexTest : FreeSpec({
             row(2, 1),
             row(3, 0),
             row(4, 0),
+        ) { minusDegree, expectedDim ->
+            dgVectorSpace.cohomology[-minusDegree].dim shouldBe expectedDim
+        }
+    }
+
+    "check homology of RP^2 × ∂Δ^2 with rational coefficient" {
+        val boundaryDelta2 = boundaryDelta(2)
+        val product = productOf(projectivePlane(), boundaryDelta2)
+        val dgVectorSpace = product.dgVectorSpace(SparseMatrixSpaceOverRational)
+        forAll(
+            row(0, 1),
+            row(1, 1),
+            row(2, 0),
+            row(3, 0),
+        ) { minusDegree, expectedDim ->
+            dgVectorSpace.cohomology[-minusDegree].dim shouldBe expectedDim
+        }
+    }
+
+    "check homology of RP^2 × ∂Δ^2 with Z/2 coefficient" {
+        val boundaryDelta2 = boundaryDelta(2)
+        val product = productOf(projectivePlane(), boundaryDelta2)
+        val dgVectorSpace = product.dgVectorSpace(SetMatrixSpaceOverF2Boolean)
+        forAll(
+            row(0, 1),
+            row(1, 2),
+            row(2, 2),
+            row(3, 1),
         ) { minusDegree, expectedDim ->
             dgVectorSpace.cohomology[-minusDegree].dim shouldBe expectedDim
         }

2760568a1f5de4fec72522e0f766f85e3368ba9d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-04-04 18:25:36 +0900

    feat(core): implement product of simplicial complexes

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/simplicial/ProductSimplicialComplexTest.kt
@@ -0,0 +1,27 @@
+package com.github.shwaka.kohomology.simplicial
+
+import com.github.shwaka.kohomology.specific.DenseMatrixSpaceOverRational
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+
+class ProductSimplicialComplexTest : FreeSpec({
+    tags(simplicialComplexTag)
+
+    "check homology of ∂Δ^2 × ∂Δ^2" {
+        val boundaryDelta2 = boundaryDelta(2)
+        val product = productOf(boundaryDelta2, boundaryDelta2)
+        val dgVectorSpace = product.dgVectorSpace(DenseMatrixSpaceOverRational)
+        forAll(
+            row(0, 1),
+            row(1, 2),
+            row(2, 1),
+            row(3, 0),
+            row(4, 0),
+        ) { minusDegree, expectedDim ->
+            dgVectorSpace.cohomology[-minusDegree].dim shouldBe expectedDim
+        }
+    }
+})

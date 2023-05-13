9492d7db6f1580138e0cc838fe73417ef2e1aad4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-05-13 19:58:18 +0900

    style(website): eslint

--- website/src/components/Calculator/computing.test.tsx
+++ website/src/components/Calculator/computing.test.tsx
@@ -1,10 +1,9 @@
 import { useLocation } from "@docusaurus/router"
-import { fireEvent, render, screen, waitForElementToBeRemoved, within } from "@testing-library/react"
+import { fireEvent, render } from "@testing-library/react"
 import React from "react"
-import { clickComputeCohomologyButton, expectInitialState, expectResultsToContainHTML, getComputeCohomologyButton } from "./__testutils__/utilsOnCalculator"
-import { Calculator } from "."
-import { WorkerWrapper } from "./WorkerContext/WorkerWrapper"
+import { expectInitialState, getComputeCohomologyButton } from "./__testutils__/utilsOnCalculator"
 import { WorkerOutput } from "./worker/workerInterface"
+import { Calculator } from "."
 
 const mockUseLocation = useLocation as unknown as jest.Mock
 mockUseLocation.mockReturnValue({

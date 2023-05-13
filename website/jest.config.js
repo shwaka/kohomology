897fad63865eca9f3103b5efe6c4b8a8aa17ea20
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-05-13 18:52:31 +0900

    test(website): add option testPathIgnorePatterns to jest.config.js

--- website/jest.config.js
+++ website/jest.config.js
@@ -24,5 +24,6 @@ module.exports = {
     "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
     "^worker-loader!.*/kohomology.worker$": "<rootDir>/src/components/Calculator/worker/__mocks__/kohomology.worker.ts",
     "@site/(.*)$": "<rootDir>/$1",
-  }
+  },
+  testPathIgnorePatterns: ["/node_nodules/", "/__testutils__/"]
 }

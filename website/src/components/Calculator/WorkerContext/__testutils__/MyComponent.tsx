3e5941735699e81feef7569cc582a7157d1cf438
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-05-20 15:11:49 +0900

    test(website): add button for test to MyComponent

--- website/src/components/Calculator/WorkerContext/__testutils__/MyComponent.tsx
+++ website/src/components/Calculator/WorkerContext/__testutils__/MyComponent.tsx
@@ -9,12 +9,14 @@ export function MyComponent(): JSX.Element {
     addListener("MyComponent", (workerOutput) => console.log(workerOutput))
   })
 
-  useEffect(() => {
-    postMessage({ value: 3 })
-  }, [postMessage])
-
   return (
     <div data-testid="my-component">
+      <button
+        onClick={() => postMessage({ value: 3 })}
+        data-testid="add3"
+      >
+        Add 3
+      </button>
       {workerOutputLog.map((workerOutput, index) => (
         <div key={index}>{workerOutput.result}</div>
       ))}
--- website/src/components/Calculator/WorkerContext/index.test.tsx
+++ website/src/components/Calculator/WorkerContext/index.test.tsx
@@ -5,5 +5,7 @@ import { TestApp } from "./__testutils__/TestApp"
 test("WorkerContext", () => {
   render(<TestApp/>)
   const div = screen.getByTestId("my-component")
+  const button = screen.getByTestId("add3")
+  button.click()
   expect(div).toContainHTML("value=3")
 })

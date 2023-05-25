1a642c02e2db358e9107ae96775f383000bfbca7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-05-25 18:17:24 +0900

    refactor(core): add reflection.kt in jvmTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/vectsp/SubVectorSpaceTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/vectsp/SubVectorSpaceTest.kt
@@ -1,5 +1,7 @@
 package com.github.shwaka.kohomology.vectsp
 
+import com.github.shwaka.kohomology.PrivateMemberAccessor
+import com.github.shwaka.kohomology.isLazyInitialized
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
@@ -11,28 +13,8 @@ import io.kotest.core.spec.style.freeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
-import kotlin.reflect.KProperty0
-import kotlin.reflect.KProperty1
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.jvm.isAccessible
 
 val subVectorSpaceTag = NamedTag("SubVectorSpace")
-
-// https://stackoverflow.com/questions/42522739/kotlin-check-if-lazy-val-has-been-initialised
-val KProperty0<*>.isLazyInitialized: Boolean
-    get() {
-        // Prevent IllegalAccessException from JVM access check on private properties.
-        val originalAccessLevel = this.isAccessible
-        this.isAccessible = true
-        val delegate: Any = this.getDelegate() ?: throw Exception("Not delegate!")
-        if (delegate !is Lazy<*>) {
-            throw Exception("Not Lazy!")
-        }
-        val isLazyInitialized = delegate.isInitialized()
-        // Reset access level.
-        this.isAccessible = originalAccessLevel
-        return isLazyInitialized
-    }
 
 fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>>
 subVectorSpaceTest(matrixSpace: MatrixSpace<S, V, M>) = freeSpec {
@@ -79,34 +61,11 @@ subVectorSpaceTest(matrixSpace: MatrixSpace<S, V, M>) = freeSpec {
             vectorSpace.context.run {
                 val generator = listOf(u + v, u, v)
                 val subVectorSpace = SubVectorSpace(matrixSpace, vectorSpace, generator)
-                // val factory = subVectorSpace.javaClass.getDeclaredField("factory")
-                fun getProperty(target: Any, name: String): KProperty1<out Any, *> {
-                    for (property in target::class.declaredMemberProperties) {
-                        if (property.name == name) {
-                            return property
-                        }
-                    }
-                    throw Exception("Property not found: $name")
-                }
-                fun getPropertyValue(target: Any, name: String): Any? {
-                    val property = getProperty(target, name)
-                    property.isAccessible = true
-                    return property.call(target)
-                }
-                fun isLazyInitialized(target: Any, name: String): Boolean {
-                    val property = getProperty(target, name) as KProperty1<Any, *>
-                    property.isAccessible = true
-                    val delegate: Any = property.getDelegate(target) ?: throw Exception("Not delegate!")
-                    if (delegate !is Lazy<*>) {
-                        throw Exception("Not Lazy!")
-                    }
-                    return delegate.isInitialized()
-                }
-                val factory = getPropertyValue(subVectorSpace, "factory")
+                val factory = PrivateMemberAccessor.getPropertyValue(subVectorSpace, "factory")
                     ?: throw Exception("factory is null")
-                isLazyInitialized(factory, "rowEchelonForm").shouldBeFalse()
+                PrivateMemberAccessor.isLazyInitialized(factory, "rowEchelonForm").shouldBeFalse()
                 subVectorSpace.dim shouldBe 2
-                isLazyInitialized(factory, "rowEchelonForm").shouldBeTrue()
+                PrivateMemberAccessor.isLazyInitialized(factory, "rowEchelonForm").shouldBeTrue()
             }
         }
     }

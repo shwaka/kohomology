84e81a7cbc4fe701d98a739749aaf313da4d762c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-05-27 20:26:47 +0900

    refactor(core): change DGVectorSpace.getCohomology() from internal to private

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/DGVectorSpace.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/DGVectorSpace.kt
@@ -86,7 +86,7 @@ public interface DGVectorSpace<D : Degree, B : BasisName, S : Scalar, V : NumVec
             return DGVectorSpace(gVectorSpace, differential)
         }
 
-        internal fun <D : Degree, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> getCohomology(
+        private fun <D : Degree, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> getCohomology(
             gVectorSpace: GVectorSpace<D, B, S, V>,
             differential: GLinearMap<D, B, B, S, V, M>,
         ): SubQuotGVectorSpace<D, B, S, V, M> {

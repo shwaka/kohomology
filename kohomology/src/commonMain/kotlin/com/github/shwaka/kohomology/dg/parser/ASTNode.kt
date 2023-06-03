187f7e21586ec1499b75b23e0bbb38c2430e684c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-03 20:17:26 +0900

    feat(core): add GAlgebraElementASTGrammar (WIP)

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/parser/ASTNode.kt
@@ -0,0 +1,13 @@
+package com.github.shwaka.kohomology.dg.parser
+
+// Node of Abstract Syntax Tree
+internal sealed interface ASTNode {
+    object Zero : ASTNode
+    data class Generator(val name: String) : ASTNode
+    data class Scalar(val numerator: Int, val denominator: Int) : ASTNode
+    data class UnaryMinus(val value: ASTNode) : ASTNode
+    data class Power(val base: ASTNode, val exponent: Int) : ASTNode
+    data class Multiply(val left: ASTNode, val right: ASTNode) : ASTNode
+    data class Subtract(val left: ASTNode, val right: ASTNode) : ASTNode
+    data class Sum(val left: ASTNode, val right: ASTNode) : ASTNode
+}
---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/dg/parser/GAlgebraElementASTGrammar.kt
@@ -0,0 +1,69 @@
+package com.github.shwaka.kohomology.dg.parser
+
+import com.github.h0tk3y.betterParse.combinators.and
+import com.github.h0tk3y.betterParse.combinators.leftAssociative
+import com.github.h0tk3y.betterParse.combinators.map
+import com.github.h0tk3y.betterParse.combinators.or
+import com.github.h0tk3y.betterParse.combinators.skip
+import com.github.h0tk3y.betterParse.combinators.use
+import com.github.h0tk3y.betterParse.grammar.Grammar
+import com.github.h0tk3y.betterParse.grammar.parser
+import com.github.h0tk3y.betterParse.lexer.literalToken
+import com.github.h0tk3y.betterParse.parser.Parser
+
+internal class GAlgebraElementASTGrammar(val generators: List<String>) : Grammar<ASTNode>() {
+    // Previously, "0" could not be used as a scalar and "zero" is used for such purpose.
+    // Currently, "zero" is unnecessary but left here for compatibility reason.
+    private val zero by literalToken("zero")
+    private val gen by literalListToken(generators)
+    private val int by regexToken("\\d+")
+    private val lpar by literalToken("(")
+    private val rpar by literalToken(")")
+    private val mul by literalToken("*")
+    private val pow by literalToken("^")
+    private val div by literalToken("/")
+    private val minus by literalToken("-")
+    private val plus by literalToken("+")
+
+    // ws is not used as a property,
+    // but Grammar registers it to the list of parsers by observing "by"-delegations.
+    @Suppress("UNUSED")
+    private val ws by regexToken("\\s*", ignore = true)
+
+    private val genParser: Parser<ASTNode>
+        by (gen use { ASTNode.Generator(text) }) or
+            (zero use { ASTNode.Zero })
+    private val intParser: Parser<Int>
+        by int use { text.toInt() }
+    private val parenParser: Parser<ASTNode>
+        by skip(lpar) and parser(::rootParser) and skip(rpar)
+    private val minusParser: Parser<ASTNode.UnaryMinus>
+        by (skip(minus) and parenParser map { ASTNode.UnaryMinus(it) }) or
+            (skip(minus) and parser(::mulChain) map { ASTNode.UnaryMinus(it) })
+    // The order to take 'or' is important in scalarParser.
+    // In "1/2*x", the whole "1/2" should be considered as a scalar.
+    // If 'or' is taken in the other order, only "1" is considered as a scalar
+    // and a ParseException is thrown at "/".
+    private val scalarParser: Parser<ASTNode.Scalar>
+        by (intParser and skip(div) and intParser map { (p, q) -> ASTNode.Scalar(p, q) }) or
+            (intParser map { n -> ASTNode.Scalar(n, 1) })
+    private val termParser: Parser<ASTNode>
+        by scalarParser or genParser or minusParser or parenParser
+    private val powerParser: Parser<ASTNode>
+        by (termParser and skip(pow) and intParser map { (node, n) -> ASTNode.Power(node, n) }) or
+            termParser
+    private val mulChain: Parser<ASTNode>
+        by leftAssociative(powerParser, mul) { left, _, right ->
+            ASTNode.Multiply(left, right)
+        }
+    private val subSumChain: Parser<ASTNode>
+        by leftAssociative(mulChain, plus or minus use { type }) { left, op, right ->
+            when (op){
+                plus -> ASTNode.Sum(left, right)
+                minus -> ASTNode.Subtract(left, right)
+                else -> throw Exception("This can't happen!")
+            }
+        }
+
+    override val rootParser: Parser<ASTNode> by subSumChain
+}

5ca40923ed3bc982b2a298b37de12f768ffd95ea
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-04 19:10:23 +0900

    test(core): add test to check CopiedName.identifier.value

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/model/CopiedNameTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/model/CopiedNameTest.kt
@@ -1,8 +1,10 @@
 package com.github.shwaka.kohomology.model
 
 import com.github.shwaka.kohomology.dg.degree.DegreeIndeterminate
+import com.github.shwaka.kohomology.dg.degree.IntDegree
 import com.github.shwaka.kohomology.dg.degree.MultiDegreeGroup
 import com.github.shwaka.kohomology.free.monoid.Indeterminate
+import com.github.shwaka.kohomology.free.monoid.StringIndeterminateName
 import com.github.shwaka.kohomology.util.PrintType
 import com.github.shwaka.kohomology.util.Printer
 import com.github.shwaka.kohomology.util.ShowShift
@@ -100,5 +102,111 @@ class CopiedNameTest : FreeSpec({
             plainPrinter(sx) shouldBe "_x"
             texPrinter(sx) shouldBe "\\bar{X}"
         }
+    }
+
+    "test equality of CopiedName" - {
+        "Two CopiedName should be equal if the given arguments are equal" {
+            CopiedName(
+                original = StringIndeterminateName("x"),
+                shift = IntDegree(3),
+            ) shouldBe CopiedName(
+                original = StringIndeterminateName("x"),
+                shift = IntDegree(3),
+            )
+
+            CopiedName(
+                original = StringIndeterminateName("y"),
+                shift = IntDegree(1),
+                index = 1,
+            ) shouldBe CopiedName(
+                original = StringIndeterminateName("y"),
+                shift = IntDegree(1),
+                index = 1,
+            )
+
+            val multiDegreeGroup = MultiDegreeGroup(
+                listOf(
+                    DegreeIndeterminate("N", 1)
+                )
+            )
+            CopiedName(
+                original = StringIndeterminateName("z"),
+                shift = multiDegreeGroup.fromList(listOf(3, 4)),
+                index = 2,
+            ) shouldBe CopiedName(
+                original = StringIndeterminateName("z"),
+                shift = multiDegreeGroup.fromList(listOf(3, 4)),
+                index = 2,
+            )
+        }
+
+        "Equality of two CopiedName should not depend on showShiftExponentInIdentifier" {
+            CopiedName(
+                original = StringIndeterminateName("a"),
+                shift = IntDegree(3),
+                showShiftExponentInIdentifier = true,
+            ) shouldBe CopiedName(
+                original = StringIndeterminateName("a"),
+                shift = IntDegree(3),
+                showShiftExponentInIdentifier = true,
+            )
+        }
+    }
+
+    "test CopiedName.identifier.value" {
+        CopiedName(
+            original = StringIndeterminateName("x"),
+            shift = IntDegree(3),
+        ).identifier.value shouldBe "s_3x"
+
+        CopiedName(
+            original = StringIndeterminateName("x"),
+            shift = IntDegree(-3),
+        ).identifier.value shouldBe "s_m3x"
+
+        CopiedName(
+            original = StringIndeterminateName("x"),
+            shift = IntDegree(3),
+            showShiftExponentInIdentifier = true,
+        ).identifier.value shouldBe "s_3x"
+
+        CopiedName(
+            original = StringIndeterminateName("x"),
+            shift = IntDegree(3),
+            showShiftExponentInIdentifier = false,
+        ).identifier.value shouldBe "sx"
+
+        CopiedName(
+            original = StringIndeterminateName("y"),
+            shift = IntDegree(1),
+            index = 1,
+        ).identifier.value shouldBe "sy1"
+
+        val multiDegreeGroup = MultiDegreeGroup(
+            listOf(
+                DegreeIndeterminate("N", 1)
+            )
+        )
+        CopiedName(
+            original = StringIndeterminateName("z"),
+            shift = multiDegreeGroup.fromList(listOf(3, 4)),
+            index = 2,
+        ).identifier.value shouldBe "s_3_4z2"
+        CopiedName(
+            original = StringIndeterminateName("z"),
+            shift = multiDegreeGroup.fromList(listOf(1, -2)),
+        ).identifier.value shouldBe "s_1_m2z"
+        CopiedName(
+            original = StringIndeterminateName("z"),
+            shift = multiDegreeGroup.fromList(listOf(3, 4)),
+            index = 2,
+            showShiftExponentInIdentifier = true,
+        ).identifier.value shouldBe "s_3_4z2"
+        CopiedName(
+            original = StringIndeterminateName("z"),
+            shift = multiDegreeGroup.fromList(listOf(3, 4)),
+            index = 2,
+            showShiftExponentInIdentifier = false,
+        ).identifier.value shouldBe "sz2"
     }
 })

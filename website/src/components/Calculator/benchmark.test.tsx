4a7408203c922829393006f413ded1af80541cf9
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-05 17:32:48 +0900

    test(website): add inputJson to benchmark.test.tsx

--- website/src/components/Calculator/benchmark.test.tsx
+++ website/src/components/Calculator/benchmark.test.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "@docusaurus/router"
 import { render } from "@testing-library/react"
 import React from "react"
+import { InputJson } from "./__testutils__/InputJson"
 import { Calculator } from "."
 
 const mockUseLocation = useLocation as unknown as jest.Mock
@@ -12,4 +13,14 @@ beforeEach(() => {
 
 test("renderCalculator", async () => {
   render(<Calculator/>)
+})
+
+test("inputJson", async () => {
+  render(<Calculator/>)
+  const json = `[
+  ["x", 3, "zero"],
+  ["y", 3, "zero"],
+  ["z", 5, "x * y"]
+]`
+  await InputJson.inputValidJson(json)
 })

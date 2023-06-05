3880908006784f68c75753d1b39c78f46b73f29e
github-action-benchmark <github@users.noreply.github.com>
2023-06-05 06:49:14 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 077c6fd0efccf9c4903a2f47d113aea804adf774

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1685947754494,
+  "repoUrl": "https://github.com/shwaka/kohomology",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Shun Wakatsuki",
+            "username": "shwaka",
+            "email": "shun.wakatsuki@gmail.com"
+          },
+          "committer": {
+            "name": "Shun Wakatsuki",
+            "username": "shwaka",
+            "email": "shun.wakatsuki@gmail.com"
+          },
+          "id": "077c6fd0efccf9c4903a2f47d113aea804adf774",
+          "message": "test(website): save website's benchmark result using benchmark-action",
+          "timestamp": "2023-06-05T06:12:19Z",
+          "url": "https://github.com/shwaka/kohomology/commit/077c6fd0efccf9c4903a2f47d113aea804adf774"
+        },
+        "date": 1685947753999,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "renderCalculator",
+            "value": 0.452,
+            "unit": "s/op"
+          }
+        ]
+      }
+    ]
+  }
+}

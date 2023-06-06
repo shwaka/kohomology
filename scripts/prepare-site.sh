4bd4623c0c44a49e6b5daac67a4c1e28346f6d63
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-06 11:10:52 +0900

    build(website): add subcommands to prepare-site.sh

--- scripts/prepare-site.sh
+++ scripts/prepare-site.sh
@@ -5,26 +5,45 @@ set -eu
 cd $(git rev-parse --show-toplevel) # go to the root of the repository
 ROOT_DIR=$(pwd)
 
-# exapnd benchmark data to the working tree
-cd "$ROOT_DIR"
-for NAME in benchmark-data benchmark-data-website; do
-    DESTINATION=$NAME
-    BRANCH=remotes/origin/$NAME
-    echo "--- Expand benchmark data to '$DESTINATION/'---"
-    rm -rf "$DESTINATION"
-    git read-tree --prefix="$DESTINATION" -u $BRANCH
-    git reset HEAD "$DESTINATION"
-    sed "s/window.BENCHMARK_DATA = //" < "$DESTINATION/dev/bench/data.js" > "$DESTINATION/dev/bench/benchmarkData.json"
-done
+command=${1-all}
 
-# build dokka
-cd "$ROOT_DIR"/kohomology
-echo "--- Generate dokka document ---"
-./gradlew dokkaHtml
-echo "--- Generate dokka coverage ---"
-./gradlew dokkacovWriteJson
+function prepare_benchmark_data() {
+    # exapnd benchmark data to the working tree
+    cd "$ROOT_DIR"
+    for NAME in benchmark-data benchmark-data-website; do
+        DESTINATION=$NAME
+        BRANCH=remotes/origin/$NAME
+        echo "--- Expand benchmark data to '$DESTINATION/'---"
+        rm -rf "$DESTINATION"
+        git read-tree --prefix="$DESTINATION" -u $BRANCH
+        git reset HEAD "$DESTINATION"
+        sed "s/window.BENCHMARK_DATA = //" < "$DESTINATION/dev/bench/data.js" > "$DESTINATION/dev/bench/benchmarkData.json"
+    done
+}
 
-# build kohomology-js
-cd "$ROOT_DIR"/website/kohomology-js
-echo "--- build kohomology-js ---"
-./gradlew build
+function prepare_dokka() {
+    # build dokka
+    cd "$ROOT_DIR"/kohomology
+    echo "--- Generate dokka document ---"
+    ./gradlew dokkaHtml
+    echo "--- Generate dokka coverage ---"
+    ./gradlew dokkacovWriteJson
+}
+
+function prepare_kohomology_js() {
+    # build kohomology-js
+    cd "$ROOT_DIR"/website/kohomology-js
+    echo "--- build kohomology-js ---"
+    ./gradlew build
+}
+
+case "$command" in
+    all)
+        prepare_benchmark_data
+        prepare_dokka
+        prepare_kohomology_js;;
+    benchmark-data) prepare_benchmark_data;;
+    dokka) prepare_dokka;;
+    kohomology-js) prepare_kohomology_js;;
+    *) echo "Invalid command: $command"
+esac

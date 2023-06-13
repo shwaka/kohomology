a15dbdc6763069e886e4762ddb2366d270a37f4f
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-13 14:03:42 +0900

    refactor(website): remove StateFromOutput and OutputFromState

--- website/src/components/Calculator/WorkerContext/OutputFromState.ts
+++
@@ -1,5 +0,0 @@
-export type OutputFromState<S, K = keyof S> = K extends keyof S ? {
-  command: "updateState"
-  key: K
-  value: S[K]
-} : never
--- website/src/components/Calculator/WorkerContext/index.ts
+++ website/src/components/Calculator/WorkerContext/index.ts
@@ -1,4 +1,2 @@
 export { useWorker } from "./useWorker"
 export { createWorkerContext } from "./WorkerContext"
-export type { StateFromOutput } from "./StateFromOutput"
-export type { OutputFromState } from "./OutputFromState"

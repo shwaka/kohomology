63eff92b190339b3ec2bba19047f7558e38f0dbf
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-15 19:41:47 +0900

    refactor(website): add useValueOfURLQueryResult to urlQuery/index.ts

--- website/src/components/Calculator/urlQuery/index.ts
+++ website/src/components/Calculator/urlQuery/index.ts
@@ -1,3 +1,4 @@
 export { createURLSearchParams } from "./createURLSearchParams"
 export { useJsonFromURLQuery } from "./useJsonFromURLQuery"
 export { useTargetNameFromURLQuery } from "./useTargetNameFromURLQuery"
+export { useValueOfURLQueryResult } from "./useValueOfURLQueryResult"

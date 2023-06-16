9fc899c5c45e0688a7dfa9f8c1cd56fdcda8bf77
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-16 18:51:51 +0900

    test(website): improve tests for dgaDsv.ts and idealDsv.ts

--- website/src/components/Calculator/urlQuery/dgaDsv.test.ts
+++ website/src/components/Calculator/urlQuery/dgaDsv.test.ts
@@ -1,14 +1,33 @@
 import { dgaDsvToJson, dgaJsonToDsv } from "./dgaDsv"
 
-test("dgaJsonToDsv with valid json", () => {
-  const json = `[
+describe("dgaJsonToDsv", () => {
+  it("should return correct dsv for valid json", () => {
+    const json = `[
   ["x", 2, "0"],
   ["y", 3, "x^2"]
 ]`
-  expect(dgaJsonToDsv(json)).toBe("x.2.0.y.3.x^2")
+    expect(dgaJsonToDsv(json)).toBe("x.2.0.y.3.x^2")
+  })
+
+  it("should return null for invalid json", () => {
+    const json = `[
+  ["x", 2, "0"],
+  ["y", 3, null]
+]`
+    expect(dgaJsonToDsv(json)).toBe(null)
+  })
 })
 
-test("dgaDsvToJson with valid dsv", () => {
-  const dsv = "x.2.0.y.3.x^2"
-  expect(dgaDsvToJson(dsv)).toBe('[["x",2,"0"],["y",3,"x^2"]]')
+describe("dgaDsvToJson", () => {
+  it("should return correct json for valid dsv", () => {
+    const dsv = "x.2.0.y.3.x^2"
+    expect(dgaDsvToJson(dsv)).toBe('[["x",2,"0"],["y",3,"x^2"]]')
+  })
+
+  it("should throw an error for invalid dsv", () => {
+    const dsv = "x.2.0.y.3"
+    expect(() => {
+      dgaDsvToJson(dsv)
+    }).toThrow("must be divisible by 3")
+  })
 })
--- website/src/components/Calculator/urlQuery/idealDsv.test.ts
+++ website/src/components/Calculator/urlQuery/idealDsv.test.ts
@@ -1,11 +1,20 @@
 import { idealDsvToJson, idealJsonToDsv } from "./idealDsv"
 
-test("idealJsonToDsv with valid json", () => {
-  const json = '["x^2", "y"]'
-  expect(idealJsonToDsv(json)).toBe("x^2.y")
+describe("idealJsonToDsv", () => {
+  it("should return correct dsv for valid json", () => {
+    const json = '["x^2", "y"]'
+    expect(idealJsonToDsv(json)).toBe("x^2.y")
+  })
+
+  it("should return null for invalid json", () => {
+    const json = '["x^2", 1]'
+    expect(idealJsonToDsv(json)).toBe(null)
+  })
 })
 
-test("idealDsvToJson with valid dsv", () => {
-  const dsv = "x^2.y"
-  expect(idealDsvToJson(dsv)).toBe('["x^2","y"]')
+describe("idealDsvToJson", () => {
+  it("should return correct json for valid dsv", () => {
+    const dsv = "x^2.y"
+    expect(idealDsvToJson(dsv)).toBe('["x^2","y"]')
+  })
 })

dc9f23c5908e42ae4aebe62cead496718cd64593
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-20 16:01:14 +0900

    refactor(core): add MMIndeterminateName.kt

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/MMIndeterminateName.kt
@@ -0,0 +1,50 @@
+package com.github.shwaka.kohomology.free
+
+import com.github.shwaka.kohomology.free.monoid.IndeterminateName
+import com.github.shwaka.kohomology.util.Identifier
+import com.github.shwaka.kohomology.util.PrintConfig
+import com.github.shwaka.kohomology.util.PrintType
+
+public enum class MMIndeterminateType {
+    COCYCLE, COCHAIN,
+}
+
+public data class MMIndeterminateName(
+    val degree: Int,
+    val index: Int,
+    val totalNumberInDegree: Int,
+    val type: MMIndeterminateType,
+) : IndeterminateName {
+    init {
+        require(index >= 0) {
+            "index must be non-negative, but $index was given"
+        }
+        require(index < totalNumberInDegree) {
+            "index must be less than totalNumberInDegree, " +
+                "but the given values were $index and $totalNumberInDegree"
+        }
+    }
+
+    override val identifier: Identifier
+        get() {
+            val printConfig = PrintConfig(printType = PrintType.PLAIN)
+            return Identifier(this.toString(printConfig))
+        }
+
+    override fun toString(printConfig: PrintConfig): String {
+        val char = when (this.type) {
+            MMIndeterminateType.COCYCLE -> "v"
+            MMIndeterminateType.COCHAIN -> "w"
+        }
+        return when (this.totalNumberInDegree) {
+            1 -> when (printConfig.printType) {
+                PrintType.PLAIN -> "${char}_${this.degree}"
+                PrintType.TEX -> "${char}_{${this.degree}}"
+            }
+            else -> when (printConfig.printType) {
+                PrintType.PLAIN -> "${char}_${this.degree}_${this.index}"
+                PrintType.TEX -> "${char}_{${this.degree},${this.index}}"
+            }
+        }
+    }
+}
--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/MinimalModel.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/MinimalModel.kt
@@ -7,59 +7,11 @@ import com.github.shwaka.kohomology.dg.GAlgebraMap
 import com.github.shwaka.kohomology.dg.GVector
 import com.github.shwaka.kohomology.dg.degree.IntDegree
 import com.github.shwaka.kohomology.free.monoid.Indeterminate
-import com.github.shwaka.kohomology.free.monoid.IndeterminateName
 import com.github.shwaka.kohomology.free.monoid.Monomial
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
-import com.github.shwaka.kohomology.util.Identifier
-import com.github.shwaka.kohomology.util.PrintConfig
-import com.github.shwaka.kohomology.util.PrintType
 import com.github.shwaka.kohomology.vectsp.BasisName
-
-public enum class MMIndeterminateType {
-    COCYCLE, COCHAIN,
-}
-
-public data class MMIndeterminateName(
-    val degree: Int,
-    val index: Int,
-    val totalNumberInDegree: Int,
-    val type: MMIndeterminateType,
-) : IndeterminateName {
-    init {
-        require(index >= 0) {
-            "index must be non-negative, but $index was given"
-        }
-        require(index < totalNumberInDegree) {
-            "index must be less than totalNumberInDegree, " +
-                "but the given values were $index and $totalNumberInDegree"
-        }
-    }
-
-    override val identifier: Identifier
-        get() {
-            val printConfig = PrintConfig(printType = PrintType.PLAIN)
-            return Identifier(this.toString(printConfig))
-        }
-
-    override fun toString(printConfig: PrintConfig): String {
-        val char = when (this.type) {
-            MMIndeterminateType.COCYCLE -> "v"
-            MMIndeterminateType.COCHAIN -> "w"
-        }
-        return when (this.totalNumberInDegree) {
-            1 -> when (printConfig.printType) {
-                PrintType.PLAIN -> "${char}_${this.degree}"
-                PrintType.TEX -> "${char}_{${this.degree}}"
-            }
-            else -> when (printConfig.printType) {
-                PrintType.PLAIN -> "${char}_${this.degree}_${this.index}"
-                PrintType.TEX -> "${char}_{${this.degree},${this.index}}"
-            }
-        }
-    }
-}
 
 public data class MinimalModel<B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
     val targetDGAlgebra: DGAlgebra<IntDegree, B, S, V, M>,
@@ -207,7 +159,10 @@ public data class MinimalModel<B : BasisName, S : Scalar, V : NumVector<S>, M : 
             IntDegree,
             Monomial<IntDegree, MMIndeterminateName>,
             Monomial<IntDegree, MMIndeterminateName>,
-            S, V, M> {
+            S,
+            V,
+            M
+            > {
             val valueList = currentFreeGAlgebra.generatorList.slice(
                 previousFreeGAlgebra.generatorList.indices
             )

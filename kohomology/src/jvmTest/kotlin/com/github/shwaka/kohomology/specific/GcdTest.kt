b8753f3b9c8ff20fca35d9b2c65cd4c57b548662
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-06-21 22:56:36 +0900

    test(core): improve GcdTest by using Arb.int

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/specific/GcdTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/specific/GcdTest.kt
@@ -5,7 +5,11 @@ import io.kotest.core.NamedTag
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.spec.style.freeSpec
 import io.kotest.inspectors.forAll
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.checkAll
 import java.math.BigInteger as JavaBigInteger
 
 val gcdTag = NamedTag("Gcd")
@@ -14,23 +18,61 @@ data class GcdData(val a: Int, val b: Int, val gcd: Int)
 
 fun <N> gcdTest(
     gcdFunction: (N, N) -> N,
-    convertInt: (Int) -> N,
+    fromInt: (Int) -> N,
+    isPositive: N.() -> Boolean,
 ) = freeSpec {
+    "gcd should always return positive integer" {
+        // When N=Int, this fails for a=b=-2147483648
+        // Hence we restrict the range.
+        val range = -10000..10000
+        checkAll(Arb.int(range), Arb.int(range)) { a, b ->
+            if ((a != 0) && (b != 0)) {
+                gcdFunction(
+                    fromInt(a),
+                    fromInt(b),
+                ).isPositive().shouldBeTrue()
+            }
+        }
+    }
+
+    val dataList = listOf(
+        GcdData(1, 1, 1),
+        GcdData(2, 1, 1),
+        GcdData(1, 2, 1),
+        GcdData(4, 2, 2),
+        GcdData(2, 4, 2),
+        GcdData(10, 15, 5),
+        GcdData(90, 36, 18),
+    )
+
     "gcd for positive integers" {
-        val dataList = listOf(
-            GcdData(1, 1, 1),
-            GcdData(2, 1, 1),
-            GcdData(1, 2, 1),
-            GcdData(4, 2, 2),
-            GcdData(2, 4, 2),
-            GcdData(10, 15, 5),
-            GcdData(90, 36, 18),
-        )
         dataList.forAll { gcdData ->
             gcdFunction(
-                convertInt(gcdData.a),
-                convertInt(gcdData.b),
-            ) shouldBe convertInt(gcdData.gcd)
+                fromInt(gcdData.a),
+                fromInt(gcdData.b),
+            ) shouldBe fromInt(gcdData.gcd)
+        }
+    }
+
+    "gcd for positive and negative integers" {
+        dataList.forAll { gcdData ->
+            gcdFunction(
+                fromInt(-gcdData.a),
+                fromInt(gcdData.b),
+            ) shouldBe fromInt(gcdData.gcd)
+            gcdFunction(
+                fromInt(gcdData.a),
+                fromInt(-gcdData.b),
+            ) shouldBe fromInt(gcdData.gcd)
+        }
+    }
+
+    "gcd for negative integers" {
+        dataList.forAll { gcdData ->
+            gcdFunction(
+                fromInt(-gcdData.a),
+                fromInt(-gcdData.b),
+            ) shouldBe fromInt(gcdData.gcd)
         }
     }
 }
@@ -38,23 +80,47 @@ fun <N> gcdTest(
 class GcdTest : FreeSpec({
     tags(gcdTag)
 
-    include(gcdTest(::gcd) { BigInteger(it) })
+    include(
+        gcdTest(
+            ::gcd,
+            fromInt = { BigInteger(it) },
+            isPositive = { this.isPositive }
+        )
+    )
 })
 
 class IntGcdTest : FreeSpec({
     tags(gcdTag)
 
-    include(gcdTest(::intGcd) { it })
+    include(
+        gcdTest(
+            ::intGcd,
+            fromInt = { it },
+            isPositive = { this > 0 },
+        )
+    )
 })
 
 class LongGcdTest : FreeSpec({
     tags(gcdTag)
 
-    include(gcdTest(::longGcd) { it.toLong() })
+    include(
+        gcdTest(
+            ::longGcd,
+            fromInt = { it.toLong() },
+            isPositive = { this > 0 },
+        )
+    )
 })
 
 class JavaGcdTest : FreeSpec({
     tags(gcdTag)
 
-    include(gcdTest(::javaGcd) { JavaBigInteger.valueOf(it.toLong()) })
+    include(
+        gcdTest(
+            ::javaGcd,
+            fromInt = { JavaBigInteger.valueOf(it.toLong()) },
+            isPositive = { this > JavaBigInteger.valueOf(0L) }
+        )
+    )
 })

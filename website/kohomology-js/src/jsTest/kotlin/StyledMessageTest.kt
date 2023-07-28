28f13b4ab9e24a40a06d2c3c5a8dd0bef9cedd76
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-07-28 20:07:53 +0900

    test(website): add StyledMessageTest.kt

---
+++ website/kohomology-js/src/jsTest/kotlin/StyledMessageTest.kt
@@ -0,0 +1,32 @@
+import styled.MessageType
+import styled.styledMessage
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@ExperimentalJsExport
+class StyledMessageTest {
+    @Test
+    fun exportMath() {
+        val styledMessageKt = styledMessage(MessageType.SUCCESS) {
+            "a^2 + b^2 = c^2".math
+        }.export()
+        assertEquals(styledMessageKt.plainString, "\$a^2 + b^2 = c^2\$")
+    }
+
+    @Test
+    fun exportMultipleMath() {
+        val styledMessageKt = styledMessage(MessageType.SUCCESS) {
+            "x = \\cos\\theta".math + "y = \\sin\\theta".math
+        }.export()
+        assertEquals(styledMessageKt.plainString, "\$x = \\cos\\theta\$\$y = \\sin\\theta\$")
+    }
+
+    @Test
+    fun exportTextAndMath() {
+        val styledMessageKt = styledMessage(MessageType.SUCCESS) {
+            "Let ".text + "x = 3".math + ".".text
+        }.export()
+        assertEquals(styledMessageKt.plainString, "Let \$x = 3\$.")
+    }
+
+}

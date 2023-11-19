ecd59242420629d1813d2822280a9145d819a676
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-19 21:46:25 +0900

    feat(core): add GenericUnionFind

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/util/UnionFind.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/util/UnionFind.kt
@@ -27,3 +27,44 @@ internal class UnionFind(private val size: Int) {
         return (0 until this.size).groupBy { this.rootOf(it) }.values.toList()
     }
 }
+
+internal class GenericUnionFind<T>(private val elements: List<T>) {
+    init {
+        require(this.elements.distinct().size == elements.size) {
+            "elements must be a list without duplicates"
+        }
+    }
+
+    private val unionFind = UnionFind(elements.size)
+    private val indices: Map<T, Int> = elements.mapIndexed { index, element ->
+        Pair(element, index)
+    }.toMap()
+
+    private fun indexOf(element: T): Int {
+        return this.indices.getValue(element)
+    }
+
+    fun rootOf(element: T): T {
+        val index = this.indexOf(element)
+        return this.elements[this.unionFind.rootOf(index)]
+    }
+
+    fun same(element1: T, element2: T): Boolean {
+        val index1 = this.indexOf(element1)
+        val index2 = this.indexOf(element2)
+        return this.unionFind.same(index1, index2)
+        // return this.rootOf(element1) == this.rootOf(element2)
+    }
+
+    fun unite(element1: T, element2: T) {
+        val index1 = this.indexOf(element1)
+        val index2 = this.indexOf(element2)
+        this.unionFind.unite(index1, index2)
+    }
+
+    fun groups(): List<List<T>> {
+        return this.unionFind.groups().map { group ->
+            group.map { index -> this.elements[index] }
+        }
+    }
+}

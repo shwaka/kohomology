91397efb107f8441706ac477d2ef22ead5020639
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-19 18:39:08 +0900

    test(core): add tests for CyclicGroup

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/bar/CyclicGroupTest.kt
@@ -0,0 +1,38 @@
+package com.github.shwaka.kohomology.bar
+
+import io.kotest.core.NamedTag
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+val cyclicGroupTag = NamedTag("CyclicGroup")
+
+class CyclicGroupTest : FreeSpec({
+    tags(cyclicGroupTag)
+
+    "test cyclic group of order 5" - {
+        val cyclicGroup = CyclicGroup(5)
+
+        "cyclicGroup.unit.value should be 0" {
+            cyclicGroup.unit.value shouldBe 0
+        }
+
+        "t^1 * t^2 should be t^3" {
+            cyclicGroup.multiply(
+                cyclicGroup.elements[1],
+                cyclicGroup.elements[2]
+            ) shouldBe cyclicGroup.elements[3]
+        }
+
+        "t^1 * t^4 should be the unit" {
+            cyclicGroup.multiply(
+                cyclicGroup.elements[1],
+                cyclicGroup.elements[4]
+            ) shouldBe cyclicGroup.unit
+        }
+
+        "(t^2)^(-1) should be t^3" {
+            cyclicGroup.invert(cyclicGroup.elements[2]) shouldBe
+                cyclicGroup.elements[3]
+        }
+    }
+})

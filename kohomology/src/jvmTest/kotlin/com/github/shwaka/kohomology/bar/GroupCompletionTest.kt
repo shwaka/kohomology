9a51a4e305e2fb97d22d4b389284856e912cf6be
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-19 22:14:23 +0900

    test(core): add GroupCompletionTest.kt

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/bar/GroupCompletionTest.kt
@@ -0,0 +1,32 @@
+package com.github.shwaka.kohomology.bar
+
+import io.kotest.core.NamedTag
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+val groupCompletionTag = NamedTag("GroupCompletion")
+
+class GroupCompletionTest : FreeSpec({
+    tags(groupCompletionTag)
+    "group completion of Z/2 should be itself" {
+        val cyclicGroup = CyclicGroup(2)
+        GroupCompletion(cyclicGroup).size shouldBe cyclicGroup.size
+    }
+
+    "group completion of Z/6 should be itself" {
+        val cyclicGroup = CyclicGroup(6)
+        GroupCompletion(cyclicGroup).size shouldBe cyclicGroup.size
+    }
+
+    "group completion of monoid with 0 should be trivial" {
+        val elements = listOf("1", "t", "0").map { SimpleFiniteMonoidElement(it) }
+        val (one, t, zero) = elements
+        val multiplicationTable = listOf(
+            listOf(one, t, zero), // one*(-)
+            listOf(t, zero, zero), // t*(-)
+            listOf(zero, zero, zero), // zero*(-)
+        )
+        val monoid = FiniteMonoidFromList(elements, multiplicationTable)
+        GroupCompletion(monoid).size shouldBe 1
+    }
+})

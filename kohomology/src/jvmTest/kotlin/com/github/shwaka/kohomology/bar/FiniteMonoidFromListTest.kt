81a09842d76ebdcec16fac57502fa5bcbec671c3
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-19 21:27:06 +0900

    test(core): add FiniteMonoidFromListTest.kt

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/bar/FiniteMonoidFromListTest.kt
@@ -0,0 +1,32 @@
+package com.github.shwaka.kohomology.bar
+
+import io.kotest.core.NamedTag
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+val finiteMonoidFromListTag = NamedTag("FiniteMonoidFromList")
+
+class FiniteMonoidFromListTest : FreeSpec({
+    tags(finiteMonoidFromListTag)
+
+    "monoid with 3 elements containing 0" - {
+        val elements = listOf("1", "t", "0").map { SimpleFiniteMonoidElement(it) }
+        val (one, t, zero) = elements
+        val multiplicationTable = listOf(
+            listOf(one, t, zero), // one*(-)
+            listOf(t, zero, zero), // t*(-)
+            listOf(zero, zero, zero), // zero*(-)
+        )
+        val monoid = FiniteMonoidFromList(elements, multiplicationTable)
+
+        "monoid.size should be 3" {
+            monoid.size shouldBe 3
+        }
+
+        "check multiplication" {
+            monoid.multiply(one, t) shouldBe t
+            monoid.multiply(t, t) shouldBe zero
+            monoid.multiply(zero, one) shouldBe zero
+        }
+    }
+})

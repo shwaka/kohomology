67cb157997e543a82d570055cba9d1478880bf22
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-19 18:16:39 +0900

    feat(core): add FiniteMonoid.kt

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/bar/FiniteMonoid.kt
@@ -0,0 +1,12 @@
+package com.github.shwaka.kohomology.bar
+
+public interface FiniteMonoidElement
+
+public interface FiniteMonoid<E : FiniteMonoidElement> {
+    public val unit: E
+    public val elements: List<E>
+    public fun multiply(monoidElement1: E, monoidElement2: E): E
+
+    public val size: Int
+        get() = elements.size
+}

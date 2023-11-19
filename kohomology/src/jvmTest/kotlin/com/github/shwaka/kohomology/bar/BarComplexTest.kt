21ce62d88439f8dde489023a5081c4a8a63d142d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-19 21:06:05 +0900

    feat(core): add BarComplexTest.kt

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/bar/BarComplexTest.kt
@@ -0,0 +1,56 @@
+package com.github.shwaka.kohomology.bar
+
+import com.github.shwaka.kohomology.forAll
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverF2
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverF3
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
+import com.github.shwaka.kohomology.util.pow
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class BarComplexTest : FreeSpec({
+    tags(barTag)
+
+    "compute homology of Z/2" - {
+        val cyclicGroup = CyclicGroup(2)
+
+        "over F2" {
+            val dgVectorSpace = barComplex(cyclicGroup, SparseMatrixSpaceOverF2)
+            (1..5).forAll { degree ->
+                dgVectorSpace[degree].dim shouldBe 0
+            }
+            (0..5).forAll { n ->
+                dgVectorSpace[-n].dim shouldBe 2.pow(n)
+                dgVectorSpace.cohomology[-n].dim shouldBe 1
+            }
+        }
+
+        "over F3" {
+            val dgVectorSpace = barComplex(cyclicGroup, SparseMatrixSpaceOverF3)
+            (1..5).forAll { degree ->
+                dgVectorSpace[degree].dim shouldBe 0
+            }
+            (0..5).forAll { n ->
+                dgVectorSpace[-n].dim shouldBe 2.pow(n)
+                dgVectorSpace.cohomology[-n].dim shouldBe when (n) {
+                    0 -> 1
+                    else -> 0
+                }
+            }
+        }
+
+        "over Q" {
+            val dgVectorSpace = barComplex(cyclicGroup, SparseMatrixSpaceOverRational)
+            (1..5).forAll { degree ->
+                dgVectorSpace[degree].dim shouldBe 0
+            }
+            (0..5).forAll { n ->
+                dgVectorSpace[-n].dim shouldBe 2.pow(n)
+                dgVectorSpace.cohomology[-n].dim shouldBe when (n) {
+                    0 -> 1
+                    else -> 0
+                }
+            }
+        }
+    }
+})

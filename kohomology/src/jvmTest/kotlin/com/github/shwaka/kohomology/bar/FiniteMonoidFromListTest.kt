2726be8f530820063a229606f7601a2ccf11d7b4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-21 17:02:18 +0900

    test(core): add test for FiniteMonoidFromList with non-commutative monoid

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/bar/FiniteMonoidFromListTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/bar/FiniteMonoidFromListTest.kt
@@ -2,6 +2,7 @@ package com.github.shwaka.kohomology.bar
 
 import io.kotest.core.NamedTag
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 
@@ -32,6 +33,32 @@ class FiniteMonoidFromListTest : FreeSpec({
             monoid.multiply(one, t) shouldBe t
             monoid.multiply(t, t) shouldBe zero
             monoid.multiply(zero, one) shouldBe zero
+        }
+    }
+
+    "non-commutative monoid with 3 elements" - {
+        val elements = listOf("1", "x", "y").map { SimpleFiniteMonoidElement(it) }
+        val (one, x, y) = elements
+        val multiplicationTable = listOf(
+            listOf(one, x, y), // one*(-)
+            listOf(x, x, y), // x*(-)
+            listOf(y, x, y), // y*(-)
+        )
+        val monoid = FiniteMonoidFromList(elements, multiplicationTable)
+
+        "monoid.isCommutative should be false" {
+            monoid.isCommutative.shouldBeFalse()
+        }
+
+        "monoid.size should be 3" {
+            monoid.size shouldBe 3
+        }
+
+        "check multiplication" {
+            monoid.multiply(one, x) shouldBe x
+            monoid.multiply(x, x) shouldBe x
+            monoid.multiply(x, y) shouldBe y
+            monoid.multiply(y, x) shouldBe x
         }
     }
 })

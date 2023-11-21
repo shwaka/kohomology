41b3a2c984a353d1301b7df41ed21f558e365ddd
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-21 17:56:58 +0900

    build(core): remove deprecated config from buildSrc/build.gradle.kts

--- kohomology/buildSrc/build.gradle.kts
+++ kohomology/buildSrc/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     `kotlin-dsl`
 }
 
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
-}
+// kotlinDslPluginOptions {
+//     experimentalWarning.set(false) // This has no effect in Gradle 7.4.2 (and will be removed in 8.0)
+// }
 
 repositories {
     mavenCentral()

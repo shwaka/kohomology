3aeeb0e924c4d22ef3a82f4000b09231628da420
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-26 01:49:41 +0900

    feat(core): add MonoidRing.getModuleWithTrivialAction

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/MonoidRing.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/MonoidRing.kt
@@ -6,6 +6,7 @@ import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.resol.monoid.FiniteMonoid
 import com.github.shwaka.kohomology.resol.monoid.FiniteMonoidElement
+import com.github.shwaka.kohomology.vectsp.BasisName
 import com.github.shwaka.kohomology.vectsp.ValueBilinearMap
 import com.github.shwaka.kohomology.vectsp.VectorSpace
 
@@ -34,4 +35,22 @@ private fun <E : FiniteMonoidElement, S : Scalar, V : NumVector<S>, M : Matrix<S
 public class MonoidRing<E : FiniteMonoidElement, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>(
     public val monoid: FiniteMonoid<E>,
     matrixSpace: MatrixSpace<S, V, M>,
-) : Algebra<E, S, V, M> by createMonoidRing(monoid, matrixSpace)
+) : Algebra<E, S, V, M> by createMonoidRing(monoid, matrixSpace) {
+    public fun <B : BasisName> getModuleWithTrivialAction(
+        vectorSpace: VectorSpace<B, S, V>
+    ): Module<E, B, S, V, M> {
+        val action = ValueBilinearMap(
+            source1 = this,
+            source2 = vectorSpace,
+            target = vectorSpace,
+            matrixSpace = this.matrixSpace,
+            values = List(this.dim) { vectorSpace.getBasis() },
+        )
+        return Module(
+            matrixSpace = this.matrixSpace,
+            underlyingVectorSpace = vectorSpace,
+            coeffAlgebra = this,
+            action = action,
+        )
+    }
+}

bea925b43352e261ffd058ccfd7d2a73ce0ed569
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-02 19:58:42 +0900

    test(core): add SymmetricGroupTest.kt

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/monoid/SymmetricGroupTest.kt
@@ -0,0 +1,54 @@
+package com.github.shwaka.kohomology.resol.monoid
+
+import io.kotest.core.NamedTag
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+
+val symmetricGroupTag = NamedTag("SymmetricGroup")
+
+class SymmetricGroupTest : FreeSpec({
+    tags(symmetricGroupTag)
+
+    "test symmetric group of order 0" - {
+        val symmetricGroup = SymmetricGroup(0)
+
+        "size should be 1" {
+            symmetricGroup.size shouldBe 1
+        }
+    }
+
+    "test symmetric group of order 1" - {
+        val symmetricGroup = SymmetricGroup(1)
+
+        "size should be 1" {
+            symmetricGroup.size shouldBe 1
+        }
+    }
+
+    "test symmetric group of order 2" - {
+        val symmetricGroup = SymmetricGroup(2)
+
+        "size should be 2" {
+            symmetricGroup.size shouldBe 2
+        }
+    }
+
+    "test symmetric group of order 3" - {
+        val symmetricGroup = SymmetricGroup(3)
+
+        "size should be 6" {
+            symmetricGroup.size shouldBe 6
+        }
+
+        "all elements should have order 1, 2 or 3" {
+            val unit = symmetricGroup.unit
+            symmetricGroup.elements.forAll { permutation ->
+                val square = symmetricGroup.multiply(permutation, permutation)
+                val cube = symmetricGroup.multiply(square, permutation)
+                ((permutation == unit) || (square == unit) || (cube == unit)).shouldBeTrue()
+            }
+        }
+    }
+})

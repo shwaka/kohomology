bf4867a0214b1b909d127cffbb91509c5e79f293
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-03 10:29:03 +0900

    feat(core): add FiniteGroupContext

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteGroup.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteGroup.kt
@@ -1,5 +1,18 @@
 package com.github.shwaka.kohomology.resol.monoid
 
+public interface FiniteGroupContext<E : FiniteMonoidElement> : FiniteMonoidContext<E> {
+    override val finiteMonoid: FiniteGroup<E>
+
+    public fun E.inv(): E {
+        return this@FiniteGroupContext.finiteMonoid.invert(this)
+    }
+}
+
+internal class FiniteGroupContextImpl<E : FiniteMonoidElement>(
+    override val finiteMonoid: FiniteGroup<E>,
+) : FiniteGroupContext<E>
+
 public interface FiniteGroup<E : FiniteMonoidElement> : FiniteMonoid<E> {
+    override val context: FiniteGroupContext<E>
     public fun invert(monoidElement: E): E
 }

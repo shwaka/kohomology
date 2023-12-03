40980920bfe866f222a25ba9fd2c478bf7bed29d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-03 11:21:49 +0900

    feat(core): add FiniteGroup.checkGroupAxioms

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteGroup.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteGroup.kt
@@ -15,4 +15,25 @@ internal class FiniteGroupContextImpl<E : FiniteMonoidElement>(
 public interface FiniteGroup<E : FiniteMonoidElement> : FiniteMonoid<E> {
     override val context: FiniteGroupContext<E>
     public fun invert(monoidElement: E): E
+
+    public fun checkInverse() {
+        val elements = this.elements
+        val unit = this.unit
+        this.context.run {
+            for (element in elements) {
+                val inverse = element.inv()
+                check(element * inverse == unit) {
+                    "Not inverse: $element * $inverse must be $unit, but was ${element * inverse}"
+                }
+                check(inverse * element == unit) {
+                    "Not inverse: $inverse * $element must be $unit, but was ${inverse * element}"
+                }
+            }
+        }
+    }
+
+    public fun checkGroupAxioms() {
+        this.checkMonoidAxioms()
+        this.checkInverse()
+    }
 }

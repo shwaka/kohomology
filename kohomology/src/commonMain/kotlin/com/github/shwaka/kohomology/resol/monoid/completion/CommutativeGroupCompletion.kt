6a4abfff18e3c14d94a1f64157a3a72da5e7f4be
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-03 16:33:24 +0900

    refactor(core): add CommutativeGroupCompletion.kt

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/completion/CommutativeGroupCompletion.kt
@@ -0,0 +1,63 @@
+package com.github.shwaka.kohomology.resol.monoid.completion
+
+import com.github.shwaka.kohomology.resol.monoid.FiniteGroupContext
+import com.github.shwaka.kohomology.resol.monoid.FiniteGroupContextImpl
+import com.github.shwaka.kohomology.resol.monoid.FiniteMonoid
+import com.github.shwaka.kohomology.resol.monoid.FiniteMonoidElement
+import com.github.shwaka.kohomology.util.GenericUnionFind
+import com.github.shwaka.kohomology.util.directProductOf
+
+internal class CommutativeGroupCompletion<E : FiniteMonoidElement>(
+    override val monoid: FiniteMonoid<E>
+) : GroupCompletion<E> {
+    init {
+        require(monoid.isCommutative) {
+            "GroupCompletion can be applied only to commutative monoid, but $monoid is not commutative"
+        }
+    }
+
+    override val context: FiniteGroupContext<Division<E>> = FiniteGroupContextImpl(this)
+
+    private val unionFind = monoid.elements.let { elements ->
+        val divisions = directProductOf(elements, elements).map { Division.fromPair(it) }
+        val unionFind = GenericUnionFind(divisions)
+        for (division in divisions) {
+            for (element in elements) {
+                val divisionToBeIdentified = Division(
+                    numerator = this.monoid.multiply(division.numerator, element),
+                    denominator = this.monoid.multiply(division.denominator, element),
+                )
+                unionFind.unite(division, divisionToBeIdentified)
+            }
+        }
+        unionFind
+    }
+
+    override val elements: List<Division<E>> = unionFind.groups().map { group ->
+        unionFind.rootOf(group[0])
+    }
+    override val unit: Division<E> = unionFind.rootOf(Division(monoid.unit, monoid.unit))
+    override val isCommutative: Boolean = true
+
+    override fun multiply(monoidElement1: Division<E>, monoidElement2: Division<E>): Division<E> {
+        return this.unionFind.rootOf(
+            Division(
+                numerator = this.monoid.multiply(monoidElement1.numerator, monoidElement2.numerator),
+                denominator = this.monoid.multiply(monoidElement1.denominator, monoidElement2.denominator),
+            )
+        )
+    }
+
+    override val multiplicationTable: List<List<Division<E>>> by lazy {
+        FiniteMonoid.getMultiplicationTable(this.elements, this::multiply)
+    }
+
+    override fun invert(monoidElement: Division<E>): Division<E> {
+        return this.unionFind.rootOf(
+            Division(
+                numerator = monoidElement.denominator,
+                denominator = monoidElement.numerator,
+            )
+        )
+    }
+}
--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/completion/GroupCompletion.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/completion/GroupCompletion.kt
@@ -1,12 +1,8 @@
 package com.github.shwaka.kohomology.resol.monoid.completion
 
 import com.github.shwaka.kohomology.resol.monoid.FiniteGroup
-import com.github.shwaka.kohomology.resol.monoid.FiniteGroupContext
-import com.github.shwaka.kohomology.resol.monoid.FiniteGroupContextImpl
 import com.github.shwaka.kohomology.resol.monoid.FiniteMonoid
 import com.github.shwaka.kohomology.resol.monoid.FiniteMonoidElement
-import com.github.shwaka.kohomology.util.GenericUnionFind
-import com.github.shwaka.kohomology.util.directProductOf
 
 public data class Division<E : FiniteMonoidElement>(val numerator: E, val denominator: E) : FiniteMonoidElement {
     override fun toString(): String {
@@ -29,60 +25,5 @@ public interface GroupCompletion<E : FiniteMonoidElement> : FiniteGroup<Division
         ): GroupCompletion<E> {
             return CommutativeGroupCompletion(monoid)
         }
-    }
-}
-
-private class CommutativeGroupCompletion<E : FiniteMonoidElement>(
-    override val monoid: FiniteMonoid<E>
-) : GroupCompletion<E> {
-    init {
-        require(monoid.isCommutative) {
-            "GroupCompletion can be applied only to commutative monoid, but $monoid is not commutative"
-        }
-    }
-
-    override val context: FiniteGroupContext<Division<E>> = FiniteGroupContextImpl(this)
-
-    private val unionFind = monoid.elements.let { elements ->
-        val divisions = directProductOf(elements, elements).map { Division.fromPair(it) }
-        val unionFind = GenericUnionFind(divisions)
-        for (division in divisions) {
-            for (element in elements) {
-                val divisionToBeIdentified = Division(
-                    numerator = this.monoid.multiply(division.numerator, element),
-                    denominator = this.monoid.multiply(division.denominator, element),
-                )
-                unionFind.unite(division, divisionToBeIdentified)
-            }
-        }
-        unionFind
-    }
-
-    override val elements: List<Division<E>> = unionFind.groups().map { group ->
-        unionFind.rootOf(group[0])
-    }
-    override val unit: Division<E> = unionFind.rootOf(Division(monoid.unit, monoid.unit))
-    override val isCommutative: Boolean = true
-
-    override fun multiply(monoidElement1: Division<E>, monoidElement2: Division<E>): Division<E> {
-        return this.unionFind.rootOf(
-            Division(
-                numerator = this.monoid.multiply(monoidElement1.numerator, monoidElement2.numerator),
-                denominator = this.monoid.multiply(monoidElement1.denominator, monoidElement2.denominator),
-            )
-        )
-    }
-
-    override val multiplicationTable: List<List<Division<E>>> by lazy {
-        FiniteMonoid.getMultiplicationTable(this.elements, this::multiply)
-    }
-
-    override fun invert(monoidElement: Division<E>): Division<E> {
-        return this.unionFind.rootOf(
-            Division(
-                numerator = monoidElement.denominator,
-                denominator = monoidElement.numerator,
-            )
-        )
     }
 }

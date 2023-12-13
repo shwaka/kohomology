990237451b2924b534605dc66875bd2e82a4b39f
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-13 23:09:59 +0900

    style(core): detekt

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/bar/ProjectiveResol.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/bar/ProjectiveResol.kt
@@ -5,8 +5,8 @@ import com.github.shwaka.kohomology.dg.degree.IntDegreeGroup
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
-import com.github.shwaka.kohomology.resol.module.Algebra
 import com.github.shwaka.kohomology.resol.complex.ComplexOfFreeModules
+import com.github.shwaka.kohomology.resol.module.Algebra
 import com.github.shwaka.kohomology.resol.module.FreeModule
 import com.github.shwaka.kohomology.resol.module.FreeModuleBasisName
 import com.github.shwaka.kohomology.resol.module.FreeModuleMap

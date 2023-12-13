fc44b3452fbd0891cda9b0943b3acb432d536c5c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-13 22:35:47 +0900

    feat(core): add ModuleMapAlongAlgebraMap.kt

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/ModuleMapAlongAlgebraMap.kt
@@ -0,0 +1,75 @@
+package com.github.shwaka.kohomology.resol.module
+
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.vectsp.BasisName
+import com.github.shwaka.kohomology.vectsp.LinearMap
+import com.github.shwaka.kohomology.vectsp.Vector
+
+public interface ModuleMapAlongAlgebraMap<
+    BAS : BasisName,
+    BAT : BasisName,
+    BS : BasisName,
+    BT : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    > {
+
+    public val source: Module<BAS, BS, S, V, M>
+    public val target: Module<BAT, BT, S, V, M>
+    public val algebraMap: AlgebraMap<BAS, BAT, S, V, M>
+    public val moduleMap: ModuleMap<BAS, BS, BT, S, V, M>
+    public val underlyingLinearMap: LinearMap<BS, BT, S, V, M>
+        get() = moduleMap.underlyingLinearMap
+
+    public fun invoke(vector: Vector<BS, S, V>): Vector<BT, S, V> {
+        return this.moduleMap(vector)
+    }
+
+    public fun kernel(): SubModule<BAS, BS, S, V, M> {
+        return this.moduleMap.kernel()
+    }
+
+    public companion object {
+        public operator fun <
+            BAS : BasisName,
+            BAT : BasisName,
+            BS : BasisName,
+            BT : BasisName,
+            S : Scalar,
+            V : NumVector<S>,
+            M : Matrix<S, V>,
+            > invoke(
+            source: Module<BAS, BS, S, V, M>,
+            target: Module<BAT, BT, S, V, M>,
+            algebraMap: AlgebraMap<BAS, BAT, S, V, M>,
+            underlyingLinearMap: LinearMap<BS, BT, S, V, M>
+        ): ModuleMapAlongAlgebraMap<BAS, BAT, BS, BT, S, V, M> {
+            return ModuleMapAlongAlgebraMapImpl(source, target, algebraMap, underlyingLinearMap)
+        }
+    }
+}
+
+private class ModuleMapAlongAlgebraMapImpl<
+    BAS : BasisName,
+    BAT : BasisName,
+    BS : BasisName,
+    BT : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    >(
+    override val source: Module<BAS, BS, S, V, M>,
+    override val target: Module<BAT, BT, S, V, M>,
+    override val algebraMap: AlgebraMap<BAS, BAT, S, V, M>,
+    underlyingLinearMap: LinearMap<BS, BT, S, V, M>
+) : ModuleMapAlongAlgebraMap<BAS, BAT, BS, BT, S, V, M> {
+    override val moduleMap: ModuleMap<BAS, BS, BT, S, V, M> =
+        ModuleMap(
+            source = source,
+            target = RestrictedModule(target, algebraMap),
+            underlyingLinearMap = underlyingLinearMap,
+        )
+}

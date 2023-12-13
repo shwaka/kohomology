0817171b5f253478961bdebd081aaa2d8590e312
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-13 20:24:13 +0900

    test(core): add AlgebraMapTest.kt

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/module/AlgebraMapTest.kt
@@ -0,0 +1,31 @@
+package com.github.shwaka.kohomology.resol.module
+
+import com.github.shwaka.kohomology.resol.monoid.CyclicGroup
+import com.github.shwaka.kohomology.resol.monoid.CyclicGroupElement
+import com.github.shwaka.kohomology.resol.monoid.FiniteMonoidMap
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class AlgebraMapTest : FreeSpec({
+    tags(algebraTag)
+
+    "test AlgebraMap induced by FiniteMonoidMap" - {
+        val matrixSpace = SparseMatrixSpaceOverRational
+        val monoidMap = run {
+            val source = CyclicGroup(6)
+            val target = CyclicGroup(2)
+            val values = (0 until 6).map { CyclicGroupElement(it % 2, 2) }
+            FiniteMonoidMap(source, target, values)
+        }
+        val algebraMap = AlgebraMap.fromFiniteMonoidMap(monoidMap, matrixSpace)
+        val sourceAlgebra = algebraMap.source
+        val (_, x) = sourceAlgebra.getBasis() // x is the generator
+        val targetAlgebra = algebraMap.target
+        val (_, y) = targetAlgebra.getBasis() // y is the generator
+
+        "test invoke" {
+            algebraMap(x) shouldBe y
+        }
+    }
+})

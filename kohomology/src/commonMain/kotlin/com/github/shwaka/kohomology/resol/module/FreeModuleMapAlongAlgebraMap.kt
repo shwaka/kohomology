d921189c25bb3433a432cf86090ca6d3e7183622
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-21 18:36:23 +0900

    feat(core): add FreeModuleMapAlongAlgebraMap.kt

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/FreeModuleMapAlongAlgebraMap.kt
@@ -0,0 +1,70 @@
+package com.github.shwaka.kohomology.resol.module
+
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.MatrixSpace
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.vectsp.BasisName
+import com.github.shwaka.kohomology.vectsp.LinearMap
+import com.github.shwaka.kohomology.vectsp.Vector
+
+public interface FreeModuleMapAlongAlgebraMap<
+    BAS : BasisName,
+    BAT : BasisName,
+    BVS : BasisName,
+    BVT : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    > : ModuleMapAlongAlgebraMapFromFreeModule<BAS, BAT, BVS, FreeModuleBasisName<BAT, BVT>, S, V, M> {
+
+    override val target: FreeModule<BAT, BVT, S, V, M>
+    public val matrixSpace: MatrixSpace<S, V, M>
+        get() = source.matrixSpace
+    public val tensorWithBaseField: LinearMap<BVS, BVT, S, V, M>
+
+    public companion object {
+        public fun <BAS : BasisName, BAT : BasisName, BVS : BasisName, BVT : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>>
+        fromValuesOnGeneratingBasis(
+            source: FreeModule<BAS, BVS, S, V, M>,
+            target: FreeModule<BAT, BVT, S, V, M>,
+            algebraMap: AlgebraMap<BAS, BAT, S, V, M>,
+            values: List<Vector<FreeModuleBasisName<BAT, BVT>, S, V>>
+        ): FreeModuleMapAlongAlgebraMap<BAS, BAT, BVS, BVT, S, V, M> {
+            val restrictedTarget = RestrictedModule(target, algebraMap)
+            val moduleMap = ModuleMapFromFreeModule.fromValuesOnGeneratingBasis(source, restrictedTarget, values)
+            return FreeModuleMapAlongAlgebraMapImpl(source, target, algebraMap, moduleMap)
+        }
+    }
+}
+
+private class FreeModuleMapAlongAlgebraMapImpl<
+    BAS : BasisName,
+    BAT : BasisName,
+    BVS : BasisName,
+    BVT : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    >(
+    override val source: FreeModule<BAS, BVS, S, V, M>,
+    override val target: FreeModule<BAT, BVT, S, V, M>,
+    override val algebraMap: AlgebraMap<BAS, BAT, S, V, M>,
+    override val moduleMap: ModuleMap<BAS, FreeModuleBasisName<BAS, BVS>, FreeModuleBasisName<BAT, BVT>, S, V, M>
+) : FreeModuleMapAlongAlgebraMap<BAS, BAT, BVS, BVT, S, V, M> {
+    override val underlyingLinearMap: LinearMap<FreeModuleBasisName<BAS, BVS>, FreeModuleBasisName<BAT, BVT>, S, V, M>
+        get() = moduleMap.underlyingLinearMap
+
+    override val tensorWithBaseField: LinearMap<BVS, BVT, S, V, M> by lazy {
+        val proj = this.target.projection
+        val vectors = this.source.getGeneratingBasis().map { vector ->
+            proj(this(vector))
+        }
+        LinearMap.fromVectors(
+            source = this.source.tensorWithBaseField,
+            target = this.target.tensorWithBaseField,
+            matrixSpace = this.matrixSpace,
+            vectors = vectors,
+        )
+    }
+}

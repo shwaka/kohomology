6221f31096f0a5bda25a9add2a633ea28103e1fa
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-21 12:17:04 +0900

    feat(core): add ChainMapAlongAlgebraMapImpl

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/complex/ChainMapAlongAlgebraMap.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/complex/ChainMapAlongAlgebraMap.kt
@@ -21,6 +21,7 @@ public interface ChainMapAlongAlgebraMap<
     M : Matrix<S, V>,
     > {
 
+    public val name: String
     public val source: Complex<D, BAS, BS, S, V, M>
     public val target: Complex<D, BAT, BT, S, V, M>
     public val algebraMap: AlgebraMap<BAS, BAT, S, V, M>
@@ -29,8 +30,64 @@ public interface ChainMapAlongAlgebraMap<
     public fun getModuleMap(degree: D): ModuleMapAlongAlgebraMap<BAS, BAT, BS, BT, S, V, M>
 
     public val underlyingDGLinearMap: DGLinearMap<D, BS, BT, S, V, M>
+        get() = this.chainMap.underlyingDGLinearMap
 
     public operator fun invoke(gVector: GVector<D, BS, S, V>): GVector<D, BT, S, V> {
         return this.underlyingDGLinearMap(gVector)
+    }
+
+    public companion object {
+        public operator fun <
+            D : Degree,
+            BAS : BasisName,
+            BAT : BasisName,
+            BS : BasisName,
+            BT : BasisName,
+            S : Scalar,
+            V : NumVector<S>,
+            M : Matrix<S, V>,
+            > invoke(
+            source: Complex<D, BAS, BS, S, V, M>,
+            target: Complex<D, BAT, BT, S, V, M>,
+            algebraMap: AlgebraMap<BAS, BAT, S, V, M>,
+            name: String,
+            getModuleMap: (degree: D) -> ModuleMapAlongAlgebraMap<BAS, BAT, BS, BT, S, V, M>,
+        ): ChainMapAlongAlgebraMap<D, BAS, BAT, BS, BT, S, V, M> {
+            return ChainMapAlongAlgebraMapImpl(source, target, algebraMap, name, getModuleMap)
+        }
+    }
+}
+
+private class ChainMapAlongAlgebraMapImpl<
+    D : Degree,
+    BAS : BasisName,
+    BAT : BasisName,
+    BS : BasisName,
+    BT : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    >
+(
+    override val source: Complex<D, BAS, BS, S, V, M>,
+    override val target: Complex<D, BAT, BT, S, V, M>,
+    override val algebraMap: AlgebraMap<BAS, BAT, S, V, M>,
+    override val name: String,
+    getModuleMap: (degree: D) -> ModuleMapAlongAlgebraMap<BAS, BAT, BS, BT, S, V, M>,
+) : ChainMapAlongAlgebraMap<D, BAS, BAT, BS, BT, S, V, M> {
+    private val _getModuleMap: (degree: D) -> ModuleMapAlongAlgebraMap<BAS, BAT, BS, BT, S, V, M> = getModuleMap
+
+    override fun getModuleMap(degree: D): ModuleMapAlongAlgebraMap<BAS, BAT, BS, BT, S, V, M> {
+        return this._getModuleMap(degree)
+    }
+
+    override val chainMap: ChainMap<D, BAS, BS, BT, S, V, M> by lazy {
+        val restrictedTarget = RestrictedComplex(this.target, this.algebraMap)
+        ChainMap(
+            source = this.source,
+            target = restrictedTarget,
+            name = this.name,
+            getModuleMap = { this.getModuleMap(it).moduleMap },
+        )
     }
 }

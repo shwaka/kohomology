d5be21392af0af0147907b76b852212024667b8e
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-21 17:03:56 +0900

    test(core): add tests to ChainMapAlongAlgebraMapTest (failing)

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/complex/ChainMapAlongAlgebraMapTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/complex/ChainMapAlongAlgebraMapTest.kt
@@ -1,6 +1,8 @@
 package com.github.shwaka.kohomology.resol.complex
 
+import com.github.shwaka.kohomology.dg.GVector
 import com.github.shwaka.kohomology.dg.degree.IntDegree
+import com.github.shwaka.kohomology.forAll
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
@@ -11,22 +13,24 @@ import com.github.shwaka.kohomology.resol.module.MonoidRing
 import com.github.shwaka.kohomology.resol.monoid.CyclicGroup
 import com.github.shwaka.kohomology.resol.monoid.CyclicGroupElement
 import com.github.shwaka.kohomology.resol.monoid.FiniteMonoidMap
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverF2
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverF3
 import com.github.shwaka.kohomology.vectsp.LinearMap
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.spec.style.freeSpec
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
 
-private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithQuotientOfCyclicGroup(
+private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithCyclicGroupQuot(
     targetOrder: Int,
-    sourceOrderFactor: Int,
+    orderFactor: Int,
     matrixSpace: MatrixSpace<S, V, M>,
     maxDeg: Int = 10,
 ) = freeSpec {
     val field = matrixSpace.field
     val p = field.characteristic
 
-    val sourceOrder = targetOrder * sourceOrderFactor
+    val sourceOrder = targetOrder * orderFactor
     val sourceGroup = CyclicGroup(sourceOrder)
     val sourceAlgebra = MonoidRing(sourceGroup, matrixSpace)
     val sourceResol = freeResolutionOverCyclicGroup(sourceAlgebra, matrixSpace)
@@ -51,6 +55,13 @@ private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithQuotientOfC
         target = targetAlgebra,
     )
 
+    val weightedNorm = targetAlgebra.context.run {
+        val s = targetAlgebra.getBasis()[1]
+        (0 until targetOrder).map { i ->
+            s.pow(i * orderFactor)
+        }.sum()
+    }
+
     val chainMap = ChainMapAlongAlgebraMap(
         source = sourceResol,
         target = targetResol,
@@ -59,12 +70,6 @@ private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithQuotientOfC
     ) { degree: IntDegree ->
         val sourceModule = sourceResol.getModule(degree)
         val targetModule = targetResol.getModule(degree)
-        val norm = targetAlgebra.context.run {
-            val s = targetAlgebra.getBasis()[1]
-            (0 until targetOrder).map { i ->
-                s.pow(i * sourceOrderFactor)
-            }.sum()
-        }
         val underlyingLinearMap = LinearMap.fromVectors(
             source = sourceModule.underlyingVectorSpace,
             target = targetModule.underlyingVectorSpace,
@@ -72,7 +77,7 @@ private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithQuotientOfC
             vectors = sourceModule.underlyingVectorSpace.getBasis().indices.map { index ->
                 val vector = targetModule.underlyingVectorSpace.getBasis()[index % targetOrder]
                 targetModule.context.run {
-                    norm * vector
+                    weightedNorm * vector
                 }
             }
         )
@@ -88,17 +93,29 @@ private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithQuotientOfC
         "it should induce isomorphism on homology at degree 0" {
             chainMap.underlyingDGLinearMap.inducedMapOnCohomology[0].isIsomorphism().shouldBeTrue()
         }
+
+        "test invoke" {
+            (-maxDeg..0).forAll { n ->
+                val x = sourceResol.underlyingDGVectorSpace.getBasis(n)[0]
+                chainMap(x) shouldBe targetResol.underlyingDGVectorSpace.context.run {
+                    val y = targetResol.underlyingDGVectorSpace.getBasis(n)[0]
+                    val degree = IntDegree(n)
+                    GVector(
+                        vector = targetResol.getModule(degree).context.run {
+                            weightedNorm * y.vector
+                        },
+                        degree = y.degree,
+                        gVectorSpace = targetResol.underlyingDGVectorSpace,
+                    )
+                }
+            }
+        }
     }
 }
 
 class ChainMapAlongAlgebraMapTest : FreeSpec({
     tags(chainMapTag)
 
-    include(
-        testWithQuotientOfCyclicGroup(
-            targetOrder = 2,
-            sourceOrderFactor = 3,
-            matrixSpace = SparseMatrixSpaceOverF3
-        )
-    )
+    include(testWithCyclicGroupQuot(targetOrder = 2, orderFactor = 3, SparseMatrixSpaceOverF3))
+    include(testWithCyclicGroupQuot(targetOrder = 2, orderFactor = 3, SparseMatrixSpaceOverF2))
 })

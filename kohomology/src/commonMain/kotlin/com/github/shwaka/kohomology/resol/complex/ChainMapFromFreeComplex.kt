5d521fe3abcee3671d91c11b942b52255c49af36
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-21 17:53:12 +0900

    feat(core): add ChainMapFromFreeComplexImpl

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/complex/ChainMapFromFreeComplex.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/complex/ChainMapFromFreeComplex.kt
@@ -1,10 +1,13 @@
 package com.github.shwaka.kohomology.resol.complex
 
+import com.github.shwaka.kohomology.dg.DGLinearMap
 import com.github.shwaka.kohomology.dg.degree.Degree
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.resol.module.FreeModuleBasisName
+import com.github.shwaka.kohomology.resol.module.ModuleMap
+import com.github.shwaka.kohomology.resol.module.ModuleMapFromFreeModule
 import com.github.shwaka.kohomology.vectsp.BasisName
 
 public interface ChainMapFromFreeComplex<
@@ -18,4 +21,56 @@ public interface ChainMapFromFreeComplex<
     > : ChainMap<D, BA, FreeModuleBasisName<BA, BVS>, BT, S, V, M> {
 
     override val source: FreeComplex<D, BA, BVS, S, V, M>
+
+    public companion object {
+        public operator fun <
+            D : Degree,
+            BA : BasisName,
+            BVS : BasisName,
+            BT : BasisName,
+            S : Scalar,
+            V : NumVector<S>,
+            M : Matrix<S, V>,
+            > invoke(
+            source: FreeComplex<D, BA, BVS, S, V, M>,
+            target: Complex<D, BA, BT, S, V, M>,
+            name: String,
+            getModuleMap: (degree: D) -> ModuleMapFromFreeModule<BA, BVS, BT, S, V, M>,
+        ): ChainMapFromFreeComplex<D, BA, BVS, BT, S, V, M> {
+            return ChainMapFromFreeComplexImpl(source, target, name, getModuleMap)
+        }
+    }
+}
+
+private class ChainMapFromFreeComplexImpl<
+    D : Degree,
+    BA : BasisName,
+    BVS : BasisName,
+    BT : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    >(
+    override val source: FreeComplex<D, BA, BVS, S, V, M>,
+    override val target: Complex<D, BA, BT, S, V, M>,
+    override val name: String,
+    getModuleMap: (degree: D) -> ModuleMapFromFreeModule<BA, BVS, BT, S, V, M>,
+) : ChainMapFromFreeComplex<D, BA, BVS, BT, S, V, M> {
+    private val _getModuleMap: (degree: D) -> ModuleMapFromFreeModule<BA, BVS, BT, S, V, M> = getModuleMap
+
+    override fun getModuleMap(degree: D): ModuleMap<BA, FreeModuleBasisName<BA, BVS>, BT, S, V, M> {
+        return this._getModuleMap(degree)
+    }
+
+    override val underlyingDGLinearMap: DGLinearMap<D, FreeModuleBasisName<BA, BVS>, BT, S, V, M> by lazy {
+        DGLinearMap(
+            source = this.source.underlyingDGVectorSpace,
+            target = this.target.underlyingDGVectorSpace,
+            degree = this.source.degreeGroup.zero,
+            matrixSpace = this.source.matrixSpace,
+            name = this.name,
+        ) { degree ->
+            this.getModuleMap(degree).underlyingLinearMap
+        }
+    }
 }

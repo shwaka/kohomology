5053fba899e935cccea15a3166dba8a982e7c8c7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-12-22 17:44:40 +0900

    style(core): detekt

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/module/MonoidRingTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/module/MonoidRingTest.kt
@@ -2,7 +2,6 @@ package com.github.shwaka.kohomology.resol.module
 
 import com.github.shwaka.kohomology.resol.monoid.CyclicGroup
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
-import com.github.shwaka.kohomology.vectsp.VectorSpace
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe

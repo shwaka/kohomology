dc0f0de72054474948425bf581295167b0e36fda
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-01-19 10:25:19 +0900

    refactor(core): make SelfModule extends FreeModule

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/SelfModule.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/SelfModule.kt
@@ -1,19 +1,22 @@
 package com.github.shwaka.kohomology.resol.module
 
 import com.github.shwaka.kohomology.linalg.Matrix
-import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.vectsp.BasisName
-import com.github.shwaka.kohomology.vectsp.BilinearMap
-import com.github.shwaka.kohomology.vectsp.VectorSpace
+
+public object SelfModuleBasisName : BasisName {
+    override fun toString(): String {
+        return "1"
+    }
+}
 
 public interface SelfModule<
     B : BasisName,
     S : Scalar,
     V : NumVector<S>,
     M : Matrix<S, V>,
-    > : Module<B, B, S, V, M> {
+    > : FreeModule<B, SelfModuleBasisName, S, V, M> {
 
     public companion object {
         public operator fun <B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> invoke(
@@ -29,10 +32,7 @@ private class SelfModuleImpl<
     S : Scalar,
     V : NumVector<S>,
     M : Matrix<S, V>,
-    >(override val coeffAlgebra: Algebra<B, S, V, M>) : SelfModule<B, S, V, M> {
-
-    override val context: ModuleContext<B, B, S, V, M> = ModuleContextImpl(this)
-    override val matrixSpace: MatrixSpace<S, V, M> = coeffAlgebra.matrixSpace
-    override val underlyingVectorSpace: VectorSpace<B, S, V> = coeffAlgebra
-    override val action: BilinearMap<B, B, B, S, V, M> = coeffAlgebra.multiplication
-}
+    >(
+    override val coeffAlgebra: Algebra<B, S, V, M>
+) : SelfModule<B, S, V, M>,
+    FreeModule<B, SelfModuleBasisName, S, V, M> by FreeModule(coeffAlgebra, listOf(SelfModuleBasisName))

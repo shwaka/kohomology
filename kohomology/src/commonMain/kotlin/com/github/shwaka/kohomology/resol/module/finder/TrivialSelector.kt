ab8ca400dbfc1bb8cc80e17000b51a639b0684d6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-01-21 02:16:47 +0900

    feat(core): add TrivialSelector

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/finder/TrivialSelector.kt
@@ -0,0 +1,23 @@
+package com.github.shwaka.kohomology.resol.module.finder
+
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.resol.algebra.Algebra
+import com.github.shwaka.kohomology.resol.module.Module
+import com.github.shwaka.kohomology.vectsp.BasisName
+import com.github.shwaka.kohomology.vectsp.Vector
+
+public class TrivialSelector<
+    BA : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    > : SmallGeneratorSelector<BA, S, V, M, Algebra<BA, S, V, M>> {
+    override fun <BA : BasisName, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> select(
+        module: Module<BA, B, S, V, M>,
+        generator: List<Vector<B, S, V>>
+    ): List<Vector<B, S, V>> {
+        return generator
+    }
+}

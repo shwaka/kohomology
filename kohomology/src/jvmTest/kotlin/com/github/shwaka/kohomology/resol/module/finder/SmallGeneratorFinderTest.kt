210be653239fbe546ce5cc7428bb40510e3cc361
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-01-20 20:25:33 +0900

    test(core): add SmallGeneratorFinderTest.kt

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/module/finder/SmallGeneratorFinderTest.kt
@@ -0,0 +1,88 @@
+package com.github.shwaka.kohomology.resol.module.finder
+
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.MatrixSpace
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.resol.module.Module
+import com.github.shwaka.kohomology.resol.module.MonoidRing
+import com.github.shwaka.kohomology.resol.module.moduleTag
+import com.github.shwaka.kohomology.resol.monoid.CyclicGroup
+import com.github.shwaka.kohomology.resol.monoid.CyclicGroupElement
+import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
+import com.github.shwaka.kohomology.vectsp.ValueBilinearMap
+import com.github.shwaka.kohomology.vectsp.VectorSpace
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.freeSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+
+private typealias GetFinder<BA, S, V, M, Alg> =
+    (coeffAlgebra: Alg) -> SmallGeneratorFinder<BA, S, V, M, Alg>
+
+private fun <
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    > smallGeneratorFinderForMonoidRingTest(
+    matrixSpace: MatrixSpace<S, V, M>,
+    finderName: String,
+    getFinder: GetFinder<CyclicGroupElement, S, V, M, MonoidRing<CyclicGroupElement, S, V, M>>,
+) = freeSpec {
+    "test SmallGeneratorSelector implementation: $finderName" - {
+        val underlyingVectorSpace = VectorSpace(matrixSpace.numVectorSpace, listOf("x", "y"))
+        val coeffAlgebra = MonoidRing(CyclicGroup(2), matrixSpace)
+        val finder = getFinder(coeffAlgebra)
+        val (x, y) = underlyingVectorSpace.getBasis()
+        // val (one, t) = coeffAlgebra.getBasis()
+
+        "finder" {
+            // Z/2 acting on Q{x, y} by
+            //   t*x = y, t*y = x
+            val action = ValueBilinearMap(
+                source1 = coeffAlgebra,
+                source2 = underlyingVectorSpace,
+                target = underlyingVectorSpace,
+                matrixSpace = matrixSpace,
+                values = listOf(
+                    listOf(x, y), // one*(-)
+                    listOf(y, x), // t*(-)
+                )
+            )
+            val module = Module(matrixSpace, underlyingVectorSpace, coeffAlgebra, action)
+            val smallGenerator = finder.find(module)
+            smallGenerator shouldHaveSize 1
+            smallGenerator shouldBe listOf(x)
+        }
+    }
+}
+
+class SmallGeneratorFinderTest : FreeSpec({
+    tags(moduleTag)
+
+    val matrixSpace = SparseMatrixSpaceOverRational
+    include(
+        smallGeneratorFinderForMonoidRingTest(
+            matrixSpace = matrixSpace,
+            finderName = SimpleSelector::class.simpleName ?: "null",
+        ) { coeffAlgebra -> SimpleSelector(coeffAlgebra) }
+    )
+    include(
+        smallGeneratorFinderForMonoidRingTest(
+            matrixSpace = matrixSpace,
+            finderName = FilteredSelector::class.simpleName ?: "null",
+        ) { coeffAlgebra -> FilteredSelector(coeffAlgebra) }
+    )
+    include(
+        smallGeneratorFinderForMonoidRingTest(
+            matrixSpace = matrixSpace,
+            finderName = EarlyReturnSelector::class.simpleName ?: "null",
+        ) { coeffAlgebra -> EarlyReturnSelector(coeffAlgebra) }
+    )
+    include(
+        smallGeneratorFinderForMonoidRingTest(
+            matrixSpace = matrixSpace,
+            finderName = MonoidRingFinder::class.simpleName ?: "null",
+        ) { coeffAlgebra -> MonoidRingFinder(coeffAlgebra) }
+    )
+})

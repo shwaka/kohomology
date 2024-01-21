df0f9f1f694e43f151be35d9c5e9531fb52048c8
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-01-21 18:37:41 +0900

    feat(core): add arguments to constructor of MonoidRingFilter

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/finder/MonoidRingFinder.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/finder/MonoidRingFinder.kt
@@ -3,8 +3,10 @@ package com.github.shwaka.kohomology.resol.module.finder
 import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.resol.algebra.Algebra
 import com.github.shwaka.kohomology.resol.algebra.Augmentation
 import com.github.shwaka.kohomology.resol.algebra.MonoidRing
+import com.github.shwaka.kohomology.resol.algebra.TrivialAlgebraBasisName
 import com.github.shwaka.kohomology.resol.monoid.FiniteMonoidElement
 
 public class MonoidRingFinder<
@@ -13,10 +15,14 @@ public class MonoidRingFinder<
     V : NumVector<S>,
     M : Matrix<S, V>,
     >(
-    private val coeffAlgebra: MonoidRing<E, S, V, M>,
+    coeffAlgebra: MonoidRing<E, S, V, M>,
+    finderOnQuot: SmallGeneratorFinder<TrivialAlgebraBasisName, S, V, M, Algebra<TrivialAlgebraBasisName, S, V, M>> =
+        TrivialSelector(),
+    additionalSelector: SmallGeneratorSelector<E, S, V, M, MonoidRing<E, S, V, M>> =
+        EarlyReturnSelector(coeffAlgebra),
 ) : SmallGeneratorFinder<E, S, V, M, MonoidRing<E, S, V, M>> by QuotFinder(
     coeffAlgebraMap = Augmentation(coeffAlgebra),
     sourceCoeffAlgebra = coeffAlgebra,
-    finderOnQuot = TrivialSelector(),
-    additionalSelector = EarlyReturnSelector(coeffAlgebra),
+    finderOnQuot = finderOnQuot,
+    additionalSelector = additionalSelector,
 )

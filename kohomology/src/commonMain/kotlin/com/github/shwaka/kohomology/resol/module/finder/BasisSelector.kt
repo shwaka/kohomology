586cbd061dfda421ec39f1e819fb94ee2a9706cc
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-01-21 18:05:35 +0900

    feat(core): add BasisSelector.kt

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/finder/BasisSelector.kt
@@ -0,0 +1,24 @@
+package com.github.shwaka.kohomology.resol.module.finder
+
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.resol.algebra.Algebra
+import com.github.shwaka.kohomology.resol.module.Module
+import com.github.shwaka.kohomology.vectsp.BasisName
+import com.github.shwaka.kohomology.vectsp.Vector
+
+public class BasisSelector<
+    BA : BasisName,
+    S : Scalar,
+    V : NumVector<S>,
+    M : Matrix<S, V>,
+    > : SmallGeneratorSelector<BA, S, V, M, Algebra<BA, S, V, M>> {
+    override fun <BA : BasisName, B : BasisName, S : Scalar, V : NumVector<S>, M : Matrix<S, V>> select(
+        module: Module<BA, B, S, V, M>,
+        candidates: List<Vector<B, S, V>>,
+        alreadySelected: List<Vector<B, S, V>>
+    ): List<Vector<B, S, V>> {
+        return module.underlyingVectorSpace.selectIndependently(alreadySelected + candidates, module.matrixSpace)
+    }
+}

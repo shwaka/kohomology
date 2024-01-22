322e4007b5c7aea5af4810233df8f29dbd4c790e
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-01-22 18:21:08 +0900

    test(core): add test for FreeComplex.tensorWith

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/complex/FreeComplexTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/complex/FreeComplexTest.kt
@@ -6,12 +6,17 @@ import com.github.shwaka.kohomology.linalg.Matrix
 import com.github.shwaka.kohomology.linalg.MatrixSpace
 import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
+import com.github.shwaka.kohomology.resol.algebra.MonoidRing
+import com.github.shwaka.kohomology.resol.algebra.OpMonoidRing
 import com.github.shwaka.kohomology.resol.module.FreeModule
 import com.github.shwaka.kohomology.resol.module.FreeModuleMap
+import com.github.shwaka.kohomology.resol.module.TrivialModule
 import com.github.shwaka.kohomology.resol.module.moduleTag
+import com.github.shwaka.kohomology.resol.monoid.CyclicGroup
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverF2
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverF3
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
+import com.github.shwaka.kohomology.vectsp.VectorSpace
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.spec.style.freeSpec
 import io.kotest.matchers.shouldBe
@@ -23,7 +28,9 @@ private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithFreeResolut
     matrixSpace: MatrixSpace<S, V, M>,
 ) = freeSpec {
     require(order > 1)
-    val complex = freeResolutionOverCyclicGroup(order, matrixSpace)
+    val cyclicGroup = CyclicGroup(order)
+    val coeffAlgebra = MonoidRing(cyclicGroup, matrixSpace)
+    val complex = freeResolutionOverCyclicGroup(coeffAlgebra, matrixSpace)
     val field = matrixSpace.field
 
     "test with free resolution of $field over $field[Z/$order]" - {
@@ -63,19 +70,34 @@ private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> testWithFreeResolut
             }
         }
 
+        fun expectedCohomologyDim(degree: Int): Int {
+            return when {
+                (order == matrixSpace.field.characteristic) -> when {
+                    (degree > 0) -> 0
+                    else -> 1
+                }
+                else -> when (degree) {
+                    0 -> 1
+                    else -> 0
+                }
+            }
+        }
+
         "test cohomology of tensorWithBaseField" {
             (-10..10).forAll { degree ->
-                val expected = when {
-                    (order == matrixSpace.field.characteristic) -> when {
-                        (degree > 0) -> 0
-                        else -> 1
-                    }
-                    else -> when (degree) {
-                        0 -> 1
-                        else -> 0
-                    }
-                }
-                complex.tensorWithBaseField.cohomology[degree].dim shouldBe expected
+                complex.tensorWithBaseField.cohomology[degree].dim shouldBe
+                    expectedCohomologyDim(degree)
+            }
+        }
+
+        "test cohomology of tensorWith(trivialModule)" {
+            val opCoeffAlgebra = OpMonoidRing(coeffAlgebra)
+            val oneDimensionalSpace = VectorSpace(matrixSpace.numVectorSpace, listOf("1"))
+            val trivialModule = TrivialModule(oneDimensionalSpace, opCoeffAlgebra)
+            val tensorProductComplex = complex.tensorWith(trivialModule)
+            (-10..10).forAll { degree ->
+                tensorProductComplex.cohomology[degree].dim shouldBe
+                    expectedCohomologyDim(degree)
             }
         }
     }

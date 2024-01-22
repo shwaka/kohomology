86253e3de4931a862beafe0eb21c3fa31bf31301
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-01-22 17:40:58 +0900

    feat(core): add TensorProductOverAlgebra.inducedMapOf

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/TensorProductOverAlgebra.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/module/TensorProductOverAlgebra.kt
@@ -5,6 +5,7 @@ import com.github.shwaka.kohomology.linalg.NumVector
 import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.vectsp.BasisName
 import com.github.shwaka.kohomology.vectsp.BilinearMap
+import com.github.shwaka.kohomology.vectsp.LinearMap
 import com.github.shwaka.kohomology.vectsp.Vector
 import com.github.shwaka.kohomology.vectsp.VectorSpace
 
@@ -24,4 +25,26 @@ public interface TensorProductOverAlgebra<
     public val leftModule: Module<BA, BL, S, V, M>
     public val tensorProductMap: BilinearMap<BR, BL, BT, S, V, M>
     public fun asPairList(vector: Vector<BT, S, V>): List<Pair<Vector<BR, S, V>, Vector<BL, S, V>>>
+    public fun <BRT : BasisName, BLT : BasisName, BTT : TensorProductBasisName<BRT, BLT>> inducedMapOf(
+        target: TensorProductOverAlgebra<BA, BRT, BLT, BTT, S, V, M>,
+        rightModuleMap: ModuleMap<BA, BR, BRT, S, V, M>,
+        leftModuleMap: ModuleMap<BA, BL, BLT, S, V, M>,
+    ): LinearMap<BT, BTT, S, V, M> {
+        val vectors = this.basisNames.map { basisName ->
+            val vector = this.fromBasisName(basisName)
+            val productList = this.asPairList(vector).map { (rightVector, leftVector) ->
+                target.tensorProductMap(
+                    rightModuleMap(rightVector),
+                    leftModuleMap(leftVector),
+                )
+            }
+            target.context.run { productList.sum() }
+        }
+        return LinearMap.fromVectors(
+            source = this,
+            target = target,
+            matrixSpace = this.rightModule.matrixSpace,
+            vectors = vectors,
+        )
+    }
 }

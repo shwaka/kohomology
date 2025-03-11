c89e3db646d92f63d6816d99ff6627e53dddddb8
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-11 15:01:27 +0900

    test(core): add tests to MonomialToStringTest

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/monoid/MonomialToStringTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/monoid/MonomialToStringTest.kt
@@ -7,12 +7,26 @@ import io.kotest.matchers.shouldBe
 
 class MonomialToStringTest : FreeSpec({
     "empty monomial should be printed as 1" {
-        monomialToString<IntDegree, StringIndeterminateName>(emptyList(), PrintType.PLAIN) {
-            it.toString()
-        } shouldBe "1"
+        monomialToString<IntDegree, StringIndeterminateName>(
+            emptyList(),
+            PrintType.PLAIN
+        ) { it.toString() } shouldBe "1"
     }
 
     "x should be printed as x" {
-        monomialToString(listOf(Pair(Indeterminate("x", 2), 1)), PrintType.PLAIN) { it.toString() } shouldBe "x"
+        monomialToString(
+            listOf(Pair(Indeterminate("x", 2), 1)),
+            PrintType.PLAIN
+        ) { it.toString() } shouldBe "x"
+    }
+
+    "xy should be printed as xy" {
+        monomialToString(
+            listOf(
+                Pair(Indeterminate("x", 2), 1),
+                Pair(Indeterminate("y", 2), 1),
+            ),
+            PrintType.PLAIN
+        ) { it.toString() } shouldBe "xy"
     }
 })

d1fc6380ff41aa2a9b8e40317ae835b74dbc68d6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-11 14:27:50 +0900

    test(core): add MonomialToStringTest.kt (WIP)

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/monoid/MonomialToStringTest.kt
@@ -0,0 +1,18 @@
+package com.github.shwaka.kohomology.free.monoid
+
+import com.github.shwaka.kohomology.dg.degree.IntDegree
+import com.github.shwaka.kohomology.util.PrintType
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class MonomialToStringTest : FreeSpec({
+    "empty monomial should be printed as 1" {
+        monomialToString<IntDegree, StringIndeterminateName>(emptyList(), PrintType.PLAIN) {
+            it.toString()
+        } shouldBe "1"
+    }
+
+    "x should be printed as x" {
+        monomialToString(listOf(Pair(Indeterminate("x", 2), 1)), PrintType.PLAIN) { it.toString() } shouldBe "x"
+    }
+})

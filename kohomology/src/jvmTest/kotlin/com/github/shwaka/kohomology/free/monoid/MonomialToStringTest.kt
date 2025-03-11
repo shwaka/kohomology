206c9656a7cad12683f27312dc0d3e8eaa62e032
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-11 15:17:32 +0900

    test(core): improving MonomialToStringTest using forAll

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/monoid/MonomialToStringTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/monoid/MonomialToStringTest.kt
@@ -2,7 +2,34 @@ package com.github.shwaka.kohomology.free.monoid
 
 import com.github.shwaka.kohomology.util.PrintType
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
+
+private data class Result(
+    val plain: String,
+    val tex: String,
+    val code: String,
+) {
+    private fun getValue(printType: PrintType): String {
+        return when (printType) {
+            PrintType.PLAIN -> this.plain
+            PrintType.TEX -> this.tex
+            PrintType.CODE -> this.code
+        }
+    }
+
+    fun check(getString: (PrintType) -> String) {
+        PrintType.values().forAll { printType ->
+            getString(printType) shouldBe this.getValue(printType)
+        }
+    }
+
+    companion object {
+        fun same(plainTexCode: String): Result {
+            return Result(plainTexCode, plainTexCode, plainTexCode)
+        }
+    }
+}
 
 class MonomialToStringTest : FreeSpec({
     "empty monomial should be printed as 1" {
@@ -27,5 +54,17 @@ class MonomialToStringTest : FreeSpec({
             ),
             PrintType.PLAIN
         ) { it.toString() } shouldBe "xy"
+    }
+
+    "test xy" {
+        Result.same("xy").check { printType ->
+            monomialToString(
+                listOf(
+                    Pair(StringIndeterminateName("x"), 1),
+                    Pair(StringIndeterminateName("y"), 1),
+                ),
+                printType
+            ) { it.toString() }
+        }
     }
 })

db88352ab64756945d238da1aab32d6170658a20
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-14 21:58:22 +0900

    feat(core): implement NCMonomialListGenerator

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/NCMonomialListGenerator.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/NCMonomialListGenerator.kt
@@ -2,12 +2,25 @@ package com.github.shwaka.kohomology.free.monoid
 
 import com.github.shwaka.kohomology.dg.degree.AugmentedDegreeGroup
 import com.github.shwaka.kohomology.dg.degree.Degree
+import com.github.shwaka.kohomology.util.shuffles
 
 internal class NCMonomialListGenerator<D : Degree, I : IndeterminateName>(
     val degreeGroup: AugmentedDegreeGroup<D>,
     val indeterminateList: IndeterminateList<D, I>,
 ) {
-    private val cache: MutableMap<Pair<D, Int>, List<NCMonomial<D, I>>> = mutableMapOf()
+    private val cache: MutableMap<D, List<NCMonomial<D, I>>> = mutableMapOf()
+    private val monomialListGenerator: MonomialListGenerator<D, I> =
+        MonomialListGenerator(degreeGroup, indeterminateList)
 
-    private val unit: NCMonomial<D, I> = NCMonomial.unit(this.degreeGroup, this.indeterminateList)
+    fun listNCMonomials(degree: D): List<NCMonomial<D, I>> {
+        return this.cache.getOrPut(degree) {
+            val monomials = this.monomialListGenerator.listMonomials(degree)
+            monomials.flatMap { monomial ->
+                shuffles(monomial.exponentList.toList()).map { shuffle: List<Int> ->
+                    val word = shuffle.map { i -> this.indeterminateList[i] }
+                    NCMonomial(this.degreeGroup, this.indeterminateList, word)
+                }
+            }
+        }
+    }
 }

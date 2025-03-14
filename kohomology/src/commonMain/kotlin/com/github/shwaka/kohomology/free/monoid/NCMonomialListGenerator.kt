296a2ab1207a33d39b27a993ff7588fa5885e0fe
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-14 21:47:01 +0900

    feat(core): add NCMonomialListGenerator.kt (WIP)

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/NCMonomialListGenerator.kt
@@ -0,0 +1,17 @@
+package com.github.shwaka.kohomology.free.monoid
+
+import com.github.shwaka.kohomology.dg.degree.AugmentedDegreeGroup
+import com.github.shwaka.kohomology.dg.degree.Degree
+
+internal class NCMonomialListGenerator<D : Degree, I : IndeterminateName>(
+    val degreeGroup: AugmentedDegreeGroup<D>,
+    val indeterminateList: IndeterminateList<D, I>,
+) {
+    private val cache: MutableMap<Pair<D, Int>, List<NCMonomial<D, I>>> = mutableMapOf()
+
+    private val unit: NCMonomial<D, I> = NCMonomial(
+        this.degreeGroup,
+        this.indeterminateList,
+        emptyList(),
+    )
+}

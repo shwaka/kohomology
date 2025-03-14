2ce6c8b54235cb85aaf48f9e28cf58534ea0ef82
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-15 01:29:57 +0900

    feat(core): add MonomialListGeneratorAugmented

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/MonomialListGenerator.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/MonomialListGenerator.kt
@@ -2,6 +2,8 @@ package com.github.shwaka.kohomology.free.monoid
 
 import com.github.shwaka.kohomology.dg.degree.AugmentedDegreeGroup
 import com.github.shwaka.kohomology.dg.degree.Degree
+import com.github.shwaka.kohomology.dg.degree.IntDegree
+import com.github.shwaka.kohomology.dg.degree.IntDegreeGroup
 
 internal interface MonomialListGenerator<D : Degree, I : IndeterminateName> {
     val degreeGroup: AugmentedDegreeGroup<D>
@@ -45,6 +47,34 @@ internal class MonomialListGeneratorBasic<D : Degree, I : IndeterminateName>(
             } else emptyList()
             val listWithZeroAtIndex = this.listMonomialsInternal(degree, index + 1)
             listWithNonZeroAtIndex + listWithZeroAtIndex
+        }
+    }
+}
+
+internal class MonomialListGeneratorAugmented<D : Degree, I : IndeterminateName>(
+    override val degreeGroup: AugmentedDegreeGroup<D>,
+    override val indeterminateList: IndeterminateList<D, I>,
+) : MonomialListGenerator<D, I> {
+    private val generatorForAugmentedDegree: MonomialListGenerator<IntDegree, I> = run {
+        val indeterminateRawList: List<Indeterminate<IntDegree, I>> = indeterminateList.map { indeterminate ->
+            Indeterminate(indeterminate.name, this.degreeGroup.augmentation(indeterminate.degree))
+        }
+        val indeterminateListWithAugDeg = IndeterminateList.from(IntDegreeGroup, indeterminateRawList)
+        MonomialListGeneratorBasic(IntDegreeGroup, indeterminateListWithAugDeg)
+    }
+
+    override fun listMonomials(degree: D): List<Monomial<D, I>> {
+        val augmentedDegree = this.degreeGroup.augmentation(degree)
+        return this.listMonomialsForAugmentedDegree(augmentedDegree).filter {
+            it.degree == degree
+        }
+    }
+
+    private fun listMonomialsForAugmentedDegree(augmentedDegree: Int): List<Monomial<D, I>> {
+        val elementListWithIntDegree: List<Monomial<IntDegree, I>> =
+            this.generatorForAugmentedDegree.listMonomials(IntDegree(augmentedDegree))
+        return elementListWithIntDegree.map { elementWithAugDeg ->
+            Monomial(this.degreeGroup, this.indeterminateList, elementWithAugDeg.exponentList)
         }
     }
 }

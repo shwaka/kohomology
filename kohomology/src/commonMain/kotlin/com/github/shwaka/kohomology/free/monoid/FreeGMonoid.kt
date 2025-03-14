7a750c03072a70987aa92408ef822695a315f457
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-14 21:44:59 +0900

    refactor(core): add MonomialListGenerator.kt

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/FreeGMonoid.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/FreeGMonoid.kt
@@ -179,50 +179,6 @@ internal data class MonomialSeparation<D : Degree, I : IndeterminateName>(
     }
 }
 
-private class MonomialListGenerator<D : Degree, I : IndeterminateName>(
-    val degreeGroup: AugmentedDegreeGroup<D>,
-    val indeterminateList: IndeterminateList<D, I>,
-    // val unit: Monomial<D, I>,
-) {
-    // (degree: D, index: Int) -> List<Monomial<D, I>>
-    private val cache: MutableMap<Pair<D, Int>, List<Monomial<D, I>>> = mutableMapOf()
-
-    private val unit: Monomial<D, I> = Monomial(
-        this.degreeGroup,
-        this.indeterminateList,
-        IntArray(this.indeterminateList.size) { 0 }
-    )
-
-    fun listMonomials(degree: D): List<Monomial<D, I>> {
-        if (!this.indeterminateList.isAllowedDegree(degree))
-            return emptyList()
-        return this.listMonomialsInternal(degree, 0)
-    }
-
-    private fun listMonomialsInternal(degree: D, index: Int): List<Monomial<D, I>> {
-        if (index < 0 || index > this.indeterminateList.size)
-            throw Exception("This can't happen! (illegal index: $index)")
-        if (index == this.indeterminateList.size) {
-            return if (degree.isZero())
-                listOf(this.unit)
-            else
-                emptyList()
-        }
-        val cacheKey = Pair(degree, index)
-        return this.cache.getOrPut(cacheKey) {
-            // Since 0 <= index < this.indeterminateList.size,
-            // we have 0 < this.indeterminateList.size
-            val newDegree = this.degreeGroup.context.run { degree - this@MonomialListGenerator.indeterminateList[index].degree }
-            val listWithNonZeroAtIndex = if (this.indeterminateList.isAllowedDegree(newDegree)) {
-                this.listMonomialsInternal(newDegree, index)
-                    .mapNotNull { monomial -> monomial.increaseExponentAtIndex(index) }
-            } else emptyList()
-            val listWithZeroAtIndex = this.listMonomialsInternal(degree, index + 1)
-            listWithNonZeroAtIndex + listWithZeroAtIndex
-        }
-    }
-}
-
 public class FreeGMonoidMorphismByDegreeChange<DS : Degree, DT : Degree, I : IndeterminateName>(
     override val source: FreeGMonoid<DS, I>,
     override val degreeMorphism: AugmentedDegreeMorphism<DS, DT>,

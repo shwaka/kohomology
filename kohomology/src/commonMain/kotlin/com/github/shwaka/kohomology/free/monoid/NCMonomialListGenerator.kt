b81342f48c86252205464bcaa6964b599b384587
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-17 09:40:27 +0900

    refactor(core): add interface NCMonomialListGenerator

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/NCMonomialListGenerator.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/free/monoid/NCMonomialListGenerator.kt
@@ -4,21 +4,35 @@ import com.github.shwaka.kohomology.dg.degree.AugmentedDegreeGroup
 import com.github.shwaka.kohomology.dg.degree.Degree
 import com.github.shwaka.kohomology.util.shuffles
 
-internal class NCMonomialListGenerator<D : Degree, I : IndeterminateName>(
-    val degreeGroup: AugmentedDegreeGroup<D>,
-    val indeterminateList: IndeterminateList<D, I>,
-) {
+public interface NCMonomialListGenerator<D : Degree, I : IndeterminateName> {
+    public val degreeGroup: AugmentedDegreeGroup<D>
+    public val indeterminateList: List<Indeterminate<D, I>>
+    public fun listNCMonomials(degree: D): List<NCMonomial<D, I>>
+}
+
+public class NCMonomialListGeneratorBasic<D : Degree, I : IndeterminateName> internal constructor(
+    override val degreeGroup: AugmentedDegreeGroup<D>,
+    private val indeterminateListInternal: IndeterminateList<D, I>,
+) : NCMonomialListGenerator<D, I> {
+    public constructor(
+        degreeGroup: AugmentedDegreeGroup<D>,
+        indeterminateList: List<Indeterminate<D, I>>,
+    ) : this(degreeGroup, IndeterminateList.from(degreeGroup, indeterminateList))
+
+    override val indeterminateList: List<Indeterminate<D, I>>
+        get() = this.indeterminateListInternal.toList()
+
     private val cache: MutableMap<D, List<NCMonomial<D, I>>> = mutableMapOf()
     private val monomialListGenerator: MonomialListGenerator<D, I> =
-        MonomialListGeneratorBasic(degreeGroup, indeterminateList)
+        MonomialListGeneratorBasic(degreeGroup, indeterminateListInternal)
 
-    fun listNCMonomials(degree: D): List<NCMonomial<D, I>> {
+    override fun listNCMonomials(degree: D): List<NCMonomial<D, I>> {
         return this.cache.getOrPut(degree) {
             val monomials = this.monomialListGenerator.listMonomials(degree)
             monomials.flatMap { monomial ->
                 shuffles(monomial.exponentList.toList()).map { shuffle: List<Int> ->
-                    val word = shuffle.map { i -> this.indeterminateList[i] }
-                    NCMonomial(this.degreeGroup, this.indeterminateList, word)
+                    val word = shuffle.map { i -> this.indeterminateListInternal[i] }
+                    NCMonomial(this.degreeGroup, this.indeterminateListInternal, word)
                 }
             }
         }

0604ae5b90cd08ffa7845a78a333be1c17f347eb
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-17 09:54:14 +0900

    test(core): improve NCMonomialListGeneratorTest by using test factory function

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/monoid/NCMonomialListGeneratorTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/monoid/NCMonomialListGeneratorTest.kt
@@ -17,28 +17,33 @@ private fun ncMonomialListGeneratorTestOverIntDegree(
     getNCMonomialListGenerator: GetNCMonomialListGenerator<IntDegree, StringIndeterminateName>,
 ) = freeSpec {
     "test ncMonomialListGenerator over IntDegree" - {
-        "nc free monoid with two generators of even degree" {
-            val indeterminateList = IndeterminateList.from(
-                IntDegreeGroup,
-                listOf(
-                    Indeterminate("x", 2),
-                    Indeterminate("y", 2),
+        suspend fun testWithDegree(degree: Int) {
+            "nc free monoid with two generators of degree $degree" {
+                val indeterminateList = IndeterminateList.from(
+                    IntDegreeGroup,
+                    listOf(
+                        Indeterminate("x", degree),
+                        Indeterminate("y", degree),
+                    )
                 )
-            )
-            val ncMonomialListGenerator = getNCMonomialListGenerator(IntDegreeGroup, indeterminateList)
-            (0..10).forAll { n ->
-                ncMonomialListGenerator.listNCMonomials(IntDegree(n)) shouldHaveSize when {
-                    n.mod(2) == 1 -> 0
-                    else -> 2.pow(n / 2)
+                val ncMonomialListGenerator = getNCMonomialListGenerator(IntDegreeGroup, indeterminateList)
+                (0..10).forAll { n ->
+                    ncMonomialListGenerator.listNCMonomials(IntDegree(n)) shouldHaveSize when {
+                        n.mod(degree) != 0 -> 0
+                        else -> 2.pow(n / degree)
+                    }
                 }
+                ncMonomialListGenerator.listNCMonomials(IntDegree(0)).map { it.toString() } shouldBe
+                    listOf("1")
+                ncMonomialListGenerator.listNCMonomials(IntDegree(degree)).map { it.toString() } shouldBe
+                    listOf("x", "y")
+                ncMonomialListGenerator.listNCMonomials(IntDegree(2 * degree)).map { it.toString() }.toSet() shouldBe
+                    setOf("x^2", "xy", "yx", "y^2")
             }
-            ncMonomialListGenerator.listNCMonomials(IntDegree(0)).map { it.toString() } shouldBe
-                listOf("1")
-            ncMonomialListGenerator.listNCMonomials(IntDegree(2)).map { it.toString() } shouldBe
-                listOf("x", "y")
-            ncMonomialListGenerator.listNCMonomials(IntDegree(4)).map { it.toString() }.toSet() shouldBe
-                setOf("x^2", "xy", "yx", "y^2")
         }
+
+        testWithDegree(2)
+        testWithDegree(4)
     }
 }
 

efe616fdda9826d74f6fc22de15d4b551aece7da
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-03-17 20:18:16 +0900

    test(core): add test for NCFreeGAlgebra from Polishchuk-Positselski's book

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/NCFreeGAlgebraTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/free/NCFreeGAlgebraTest.kt
@@ -14,6 +14,7 @@ import com.github.shwaka.kohomology.util.pow
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.spec.style.freeSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
@@ -109,10 +110,101 @@ private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> idealTest(matrixSpa
     }
 }
 
+private fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> pbwTest(
+    matrixSpace: MatrixSpace<S, V, M>,
+    a: Int,
+) = freeSpec {
+    // Polishchuk-Positselski, "Quadratic algebras", AMS
+    // Section 4.10, p.97, Example
+    // Correction is given in the following MO
+    // https://mathoverflow.net/questions/34427/an-example-of-a-z-pbw-algebra-which-is-not-a-pbw-algebra
+
+    "compute Polishchuk-Positselski's example" - {
+        val indeterminateList = listOf(
+            Indeterminate("x", 1),
+            Indeterminate("y", 1),
+            Indeterminate("z", 1),
+        )
+        val ncFreeGAlgebra = NCFreeGAlgebra(matrixSpace, indeterminateList)
+        val (x, y, z) = ncFreeGAlgebra.generatorList
+        val aInv = ncFreeGAlgebra.context.run { a.toScalar().inv() }
+        val xs = listOf(x, x, x)
+        val ys = ncFreeGAlgebra.context.run { listOf(y - x, y + x, y - a * x) }
+        val zs = ncFreeGAlgebra.context.run { listOf(z - x, z + aInv * x, z - aInv * x) }
+        val initialGenerator = ncFreeGAlgebra.context.run {
+            listOf(
+                x.pow(2) + y * z,
+                x.pow(2) + a * z * y,
+            )
+        }
+        val zyx = listOf(0, 1).map { i ->
+            ncFreeGAlgebra.context.run {
+                listOf(
+                    zs[i + 1] * zs[i],
+                    zs[i + 1] * ys[i],
+                    zs[i + 1] * xs[i],
+                    ys[i + 1] * zs[i],
+                    ys[i + 1] * ys[i],
+                    ys[i + 1] * xs[i],
+                    xs[i + 1] * zs[i],
+                    xs[i + 1] * ys[i],
+                    xs[i + 1] * xs[i],
+                )
+            }
+        }
+
+        "zyx[i][k] should be non-zero in a quotient except for k=6,7" {
+            // The expected results in this test are computed by Macaulay2
+            listOf(0, 1).forAll { i ->
+                (0 until 9).forAll { k ->
+                    val idealGenerator = initialGenerator + zyx[i].slice(0 until k)
+                    val ideal = ncFreeGAlgebra.getIdeal(idealGenerator)
+                    val quotientGAlgebra = ncFreeGAlgebra.getQuotientByIdeal(ideal)
+                    val proj = quotientGAlgebra.projection
+                    proj(zyx[i][k]).isZero() shouldBe when (k) {
+                        6, 7 -> true
+                        else -> false
+                    }
+                }
+            }
+        }
+
+        "these x,y,z give a basis at degree 3" {
+            val elements = ncFreeGAlgebra.context.run {
+                listOf(
+                    // ? * zs[1] * ?
+                    zs[2] * zs[1] * zs[0],
+                    zs[2] * zs[1] * ys[0],
+                    zs[2] * zs[1] * xs[0],
+                    ys[2] * zs[1] * zs[0],
+                    ys[2] * zs[1] * ys[0],
+                    ys[2] * zs[1] * xs[0],
+                    // ? * ys[1] * ?
+                    zs[2] * ys[1] * zs[0],
+                    zs[2] * ys[1] * ys[0],
+                    zs[2] * ys[1] * xs[0],
+                    ys[2] * ys[1] * zs[0],
+                    ys[2] * ys[1] * ys[0],
+                    ys[2] * ys[1] * xs[0],
+                    // ? * xs[1] * ?
+                    zs[2] * xs[1] * xs[0],
+                    ys[2] * xs[1] * xs[0],
+                    xs[2] * xs[1] * xs[0],
+                )
+            }
+            val ideal = ncFreeGAlgebra.getIdeal(initialGenerator)
+            val quotientGAlgebra = ncFreeGAlgebra.getQuotientByIdeal(ideal)
+            val proj = quotientGAlgebra.projection
+            quotientGAlgebra.isBasis(elements.map { proj(it) }, 3).shouldBeTrue()
+        }
+    }
+}
+
 class NCFreeGAlgebraTest : FreeSpec({
     val matrixSpace = SparseMatrixSpaceOverRational
 
     include(noGeneratorTest(matrixSpace))
     include(twoGeneratorTest(matrixSpace))
     include(idealTest(matrixSpace))
+    include(pbwTest(matrixSpace, 2))
 })

46b9cdf23429b599286931033699b34f9769d4a7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-06 18:22:13 +0900

    test(core): add FiniteMonoidMapEnumeratorTest.kt

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteMonoidMapTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteMonoidMapTest.kt
@@ -1,15 +1,11 @@
 package com.github.shwaka.kohomology.resol.monoid
 
-import com.github.shwaka.kohomology.util.isPrime
 import io.kotest.assertions.throwables.shouldNotThrow
-import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.NamedTag
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
-import io.kotest.matchers.collections.shouldContain
-import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 
 val finiteMonoidMapTag = NamedTag("FiniteMonoidMap")
@@ -148,67 +144,6 @@ class FiniteMonoidMapTest : FreeSpec({
         )
         for (monoid in monoids) {
             testForMonoid(monoid)
-        }
-    }
-})
-
-class FiniteMonoidMapListAllMapsTest : FreeSpec({
-    tags(finiteMonoidTag, finiteMonoidMapTag)
-
-    "test listAllMapsNaive" - {
-        "the number of monoid maps from ℤ/2 to ℤ/2 should be 2" {
-            val monoid = CyclicGroup(2)
-            FiniteMonoidMapEnumerator.Naive.listAllMaps(monoid, monoid) shouldHaveSize 2
-        }
-
-        "the number of monoid maps from ℤ/p to ℤ/p should be p" {
-            (2..7).filter { it.isPrime() }.forAll { p ->
-                val monoid = CyclicGroup(p)
-                FiniteMonoidMapEnumerator.Naive.listAllMaps(monoid, monoid) shouldHaveSize p
-            }
-        }
-
-        "the number of monoid maps from TruncatedAdditionMonoid(1) to itself should be 2" {
-            val monoid = TruncatedAdditionMonoid(1)
-            FiniteMonoidMapEnumerator.Naive.listAllMaps(monoid, monoid) shouldHaveSize 2
-        }
-
-        "any element of listAllMapsNaive() must be a monoid map" - {
-            suspend fun <E : FiniteMonoidElement> testForMonoid(monoid: FiniteMonoid<E>) {
-                "test with $monoid" {
-                    FiniteMonoidMapEnumerator.Naive.listAllMaps(monoid, monoid).forAll { finiteMonoidMap ->
-                        shouldNotThrowAny {
-                            finiteMonoidMap.checkFiniteMonoidMapAxioms()
-                        }
-                    }
-                }
-            }
-            val monoids = listOf(
-                CyclicGroup(2),
-                CyclicGroup(3),
-                TruncatedAdditionMonoid(2),
-            )
-            for (monoid in monoids) {
-                testForMonoid(monoid)
-            }
-        }
-
-        "listAllMapsNaive() must contain id and trivialMap" - {
-            suspend fun <E : FiniteMonoidElement> testForMonoid(monoid: FiniteMonoid<E>) {
-                "test with $monoid" {
-                    val list = FiniteMonoidMapEnumerator.Naive.listAllMaps(monoid, monoid)
-                    list shouldContain FiniteMonoidMap.id(monoid)
-                    list shouldContain FiniteMonoidMap.trivialMap(monoid)
-                }
-            }
-            val monoids = listOf(
-                CyclicGroup(2),
-                CyclicGroup(3),
-                TruncatedAdditionMonoid(2),
-            )
-            for (monoid in monoids) {
-                testForMonoid(monoid)
-            }
         }
     }
 })

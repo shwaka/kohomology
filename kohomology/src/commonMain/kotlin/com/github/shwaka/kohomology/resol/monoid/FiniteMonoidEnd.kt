21e0dcd56b2411c5f960c52f2e30e8ef7944125b
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-06 19:59:29 +0900

    feat(core): implement FiniteMonoidEnd.multiply

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteMonoidEnd.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteMonoidEnd.kt
@@ -4,7 +4,8 @@ import com.github.shwaka.kohomology.util.PrintConfig
 import com.github.shwaka.kohomology.util.PrintType
 
 public data class EndElement<E : FiniteMonoidElement>(
-    public val asMap: FiniteMonoidMap<E, E>
+    public val baseMonoid: FiniteMonoid<E>,
+    public val asMap: FiniteMonoidMap<E, E>,
 ) : FiniteMonoidElement {
     override fun toString(printConfig: PrintConfig): String {
         return this.asMap.toString(printConfig)
@@ -34,7 +35,7 @@ public data class EndElement<E : FiniteMonoidElement>(
             val id = FiniteMonoidMap.id(baseMonoid)
             val maps = FiniteMonoidMap.listAllMaps(baseMonoid, baseMonoid)
             val mapsSorted = listOf(id) + maps.filter { it != id }
-            return mapsSorted.map { EndElement(it) }
+            return mapsSorted.map { EndElement(baseMonoid, it) }
         }
     }
 }
@@ -43,14 +44,23 @@ public class FiniteMonoidEnd<E : FiniteMonoidElement>(
     public val baseMonoid: FiniteMonoid<E>,
 ) : FiniteMonoid<EndElement<E>> {
     override val context: FiniteMonoidContext<EndElement<E>> = FiniteMonoidContext(this)
-    override val unit: EndElement<E> = EndElement(FiniteMonoidMap.id(baseMonoid))
+    override val unit: EndElement<E> = EndElement(baseMonoid, FiniteMonoidMap.id(baseMonoid))
     override val elements: List<EndElement<E>> = EndElement.getAll(baseMonoid)
     override val isCommutative: Boolean by lazy {
         FiniteMonoid.isCommutative(elements, ::multiply)
     }
 
     override fun multiply(monoidElement1: EndElement<E>, monoidElement2: EndElement<E>): EndElement<E> {
-        TODO("Not yet implemented")
+        require(monoidElement1.baseMonoid == this.baseMonoid) {
+            "$monoidElement1 is not an endomorphism on ${this.baseMonoid}"
+        }
+        require(monoidElement2.baseMonoid == this.baseMonoid) {
+            "$monoidElement2 is not an endomorphism on ${this.baseMonoid}"
+        }
+        return EndElement(
+            baseMonoid = this.baseMonoid,
+            asMap = monoidElement1.asMap * monoidElement2.asMap,
+        )
     }
 
     override val multiplicationTable: List<List<EndElement<E>>> by lazy {

2a7b80d2a6103c8ea79c588101afb1bbbf0ff5c7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-06 19:31:04 +0900

    feat(core): add FiniteMonoidEnd.kt (WIP)

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/resol/monoid/FiniteMonoidEnd.kt
@@ -0,0 +1,67 @@
+package com.github.shwaka.kohomology.resol.monoid
+
+import com.github.shwaka.kohomology.util.PrintConfig
+import com.github.shwaka.kohomology.util.PrintType
+
+public data class EndElement<E : FiniteMonoidElement>(
+    public val asMap: FiniteMonoidMap<E, E>
+) : FiniteMonoidElement {
+    override fun toString(printConfig: PrintConfig): String {
+        return this.asMap.toString(printConfig)
+    }
+
+    override fun toString(): String {
+        return this.toString(PrintConfig(PrintType.PLAIN))
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as EndElement<*>
+
+        if (asMap != other.asMap) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return asMap.hashCode()
+    }
+
+    public companion object {
+        public fun <E : FiniteMonoidElement> getAll(baseMonoid: FiniteMonoid<E>): List<EndElement<E>> {
+            val id = FiniteMonoidMap.id(baseMonoid)
+            val maps = FiniteMonoidMap.listAllMaps(baseMonoid, baseMonoid)
+            val mapsSorted = listOf(id) + maps.filter { it != id }
+            return mapsSorted.map { EndElement(it) }
+        }
+    }
+}
+
+public class FiniteMonoidEnd<E : FiniteMonoidElement>(
+    public val baseMonoid: FiniteMonoid<E>,
+) : FiniteMonoid<EndElement<E>> {
+    override val context: FiniteMonoidContext<EndElement<E>> = FiniteMonoidContext(this)
+    override val unit: EndElement<E> = EndElement(FiniteMonoidMap.id(baseMonoid))
+    override val elements: List<EndElement<E>> = EndElement.getAll(baseMonoid)
+    override val isCommutative: Boolean by lazy {
+        FiniteMonoid.isCommutative(elements, ::multiply)
+    }
+
+    override fun multiply(monoidElement1: EndElement<E>, monoidElement2: EndElement<E>): EndElement<E> {
+        TODO("Not yet implemented")
+    }
+
+    override val multiplicationTable: List<List<EndElement<E>>> by lazy {
+        FiniteMonoid.getMultiplicationTable(this.elements, this::multiply)
+    }
+
+    override fun toString(printConfig: PrintConfig): String {
+        return "End(${this.baseMonoid.toString(printConfig)})"
+    }
+
+    override fun toString(): String {
+        return this.toString(PrintConfig(PrintType.PLAIN))
+    }
+}

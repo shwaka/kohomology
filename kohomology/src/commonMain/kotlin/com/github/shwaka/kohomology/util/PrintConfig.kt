b8390db10281a9df65639e6507196cc993be2de3
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-07 10:16:45 +0900

    refactor(core): add PrintConfigDefaultRegistry

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/util/PrintConfig.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/util/PrintConfig.kt
@@ -6,12 +6,13 @@ public interface PrintConfigEntry
 
 public data class CopiedNamePrintConfig(
     val showShift: ShowShift = ShowShift.S_WITH_DEGREE
-) : PrintConfigEntry
-// The init block in an object is not executed immediately.
-// It will be executed when it is accessed.
-// https://kotlinlang.org/docs/object-declarations.html#behavior-difference-between-object-declarations-and-expressions
-@Suppress("UNUSED")
-private val registerCopiedNamePrintConfig = PrintConfig.registerDefault(CopiedNamePrintConfig())
+) : PrintConfigEntry {
+    public companion object {
+        public fun registerDefault() {
+            PrintConfigDefaultRegistry.registerDefault(CopiedNamePrintConfig())
+        }
+    }
+}
 
 /**
  * Determines how to print [com.github.shwaka.kohomology.model.CopiedName].
@@ -40,29 +41,37 @@ public data class PrintConfig(
 
     public inline fun <reified T : PrintConfigEntry> get(): T {
         (this.entries[T::class] as? T)?.let { return it }
-        return PrintConfig.getDefault<T>()
+        return PrintConfigDefaultRegistry.getDefault<T>()
     }
 
     public companion object {
         public val default: PrintConfig = PrintConfig(PrintType.PLAIN)
+    }
+}
 
-        public val defaultEntries: MutableMap<KClass<out PrintConfigEntry>, PrintConfigEntry> = mutableMapOf()
+public object PrintConfigDefaultRegistry {
+    init {
+        // The init block in an object is not executed immediately.
+        // It will be executed when it is accessed.
+        // https://kotlinlang.org/docs/object-declarations.html#behavior-difference-between-object-declarations-and-expressions
+        CopiedNamePrintConfig.registerDefault()
+    }
 
-        public inline fun <reified T : PrintConfigEntry> registerDefault(entry: T) {
-            require(!this.defaultEntries.containsKey(T::class)) {
-                "Already registered: ${T::class}"
-            }
-            println("default value registered for ${T::class}")
-            this.defaultEntries[T::class] = entry
+    public val defaultEntries: MutableMap<KClass<out PrintConfigEntry>, PrintConfigEntry> = mutableMapOf()
+
+    public inline fun <reified T : PrintConfigEntry> registerDefault(entry: T) {
+        // require(!this.defaultEntries.containsKey(T::class)) {
+        //     "Already registered: ${T::class}"
+        // }
+        this.defaultEntries[T::class] = entry
+    }
+
+    public inline fun <reified T : PrintConfigEntry> getDefault(): T {
+        if (!this.defaultEntries.containsKey(T::class)) {
+            throw NoSuchElementException("Default value not registered for ${T::class}")
         }
-
-        public inline fun <reified T : PrintConfigEntry> getDefault(): T {
-            if (!this.defaultEntries.containsKey(T::class)) {
-                throw NoSuchElementException("Default value not registered for ${T::class}")
-            }
-            return (this.defaultEntries[T::class] as? T)
-                ?: throw NoSuchElementException("Failed to cast to ${T::class}")
-        }
+        return (this.defaultEntries[T::class] as? T)
+            ?: throw NoSuchElementException("Failed to cast to ${T::class}")
     }
 }
 

e3d219ed10ad119a687d4df0b07835fd26361855
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-18 15:16:04 +0900

    test(website): improve tests for numberSchemaWithRequiredError

--- website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/numberSchemaWithRequiredError.test.ts
+++ website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/numberSchemaWithRequiredError.test.ts
@@ -3,7 +3,7 @@ import { numberSchemaWithRequiredError } from "./numberSchemaWithRequiredError"
 describe("numberSchemaWithRequiredError", () => {
   const errorMessage = "number expected"
 
-  for (const value of [0, 1, 2, -1]) {
+  for (const value of [0, 1, 2, -1, 1.1, 0.5, -2.3]) {
     it(`should successfully parse ${value}`, () => {
       const result = numberSchemaWithRequiredError(errorMessage).safeParse(value)
       expect(result.success).toBe(true)
@@ -20,6 +20,14 @@ describe("numberSchemaWithRequiredError", () => {
 
   it("should fail to parse empty string", () => {
     const result = numberSchemaWithRequiredError(errorMessage).safeParse("")
+    expect(result.success).toBe(false)
+    if (!result.success) {
+      expect(result.error.flatten().formErrors).toContain(errorMessage)
+    }
+  })
+
+  it("should fail to parse non-empty string", () => {
+    const result = numberSchemaWithRequiredError(errorMessage).safeParse("foo")
     expect(result.success).toBe(false)
     if (!result.success) {
       expect(result.error.flatten().formErrors).toContain(errorMessage)

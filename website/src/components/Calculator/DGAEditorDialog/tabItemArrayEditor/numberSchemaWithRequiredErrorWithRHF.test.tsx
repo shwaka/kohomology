5d350f3a76043425ab994c8a5ea6a6530d9339b8
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-18 16:15:52 +0900

    test(website): improve numberSchemaWithRequiredErrorWithRHF.test.tsx

--- website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/numberSchemaWithRequiredErrorWithRHF.test.tsx
+++ website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/numberSchemaWithRequiredErrorWithRHF.test.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { render, screen, fireEvent, waitFor } from "@testing-library/react"
-import React from "react"
+import React, { useState } from "react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { numberSchemaWithRequiredError } from "./numberSchemaWithRequiredError"
@@ -22,17 +22,22 @@ function TestForm({ valueAsNumber }: { valueAsNumber: boolean }): JSX.Element {
     resolver: zodResolver(schema),
   })
 
-  /* eslint-disable @typescript-eslint/no-empty-function */
+  // If successfully submitted (without any error), then setSubmitted(true).
+  // If submission contains an error, then setSubmitted is not called.
+  const [submitted, setSubmitted] = useState(false)
+
   return (
-    <form onSubmit={handleSubmit(() => {})}>
+    <form onSubmit={handleSubmit(() => { setSubmitted(true) })}>
       <input type="number" placeholder="degree" {...register("degree", { valueAsNumber })} />
       {errors.degree && <p role="alert">{errors.degree.message}</p>}
       <button type="submit">Submit</button>
+      {submitted && <span data-testid="submitted">submitted</span>}
     </form>
   )
 }
 
 describe("numberSchemaWithRequiredError with react-hook-form", () => {
+  // Note: numberSchemaWithRequiredError must be used with { valueAsNumber: true }
   describe("registered with { valueAsNumber: true }", () => {
     it("should show the message given as the argument if the value is empty", async () => {
       render(<TestForm valueAsNumber={true}/>)
@@ -42,6 +47,7 @@ describe("numberSchemaWithRequiredError with react-hook-form", () => {
       await waitFor(() => {
         expect(screen.getByRole("alert")).toHaveTextContent(errorMessage)
       })
+      expect(screen.queryByTestId("submitted")).not.toBeInTheDocument()
     })
 
     it("should not show any error if the value is a number as a string", async () => {
@@ -50,17 +56,17 @@ describe("numberSchemaWithRequiredError with react-hook-form", () => {
       fireEvent.change(screen.getByPlaceholderText("degree"), {
         target: { value: "2" },
       })
-
       fireEvent.click(screen.getByText("Submit"))
 
       await waitFor(() => {
-        expect(screen.queryByRole("alert")).not.toBeInTheDocument()
+        expect(screen.getByTestId("submitted")).toBeInTheDocument()
       })
+      expect(screen.queryByRole("alert")).not.toBeInTheDocument()
     })
   })
 
   describe("registered with { valueAsNumber: false }", () => {
-    it("should show the message from z.number()", async () => {
+    it("should show the message from z.number() if the value is the empty string", async () => {
       render(<TestForm valueAsNumber={false}/>)
 
       fireEvent.click(screen.getByText("Submit"))
@@ -68,6 +74,22 @@ describe("numberSchemaWithRequiredError with react-hook-form", () => {
       await waitFor(() => {
         expect(screen.getByRole("alert")).toHaveTextContent("Expected number, received string")
       })
+      expect(screen.queryByTestId("submitted")).not.toBeInTheDocument()
+    })
+
+    it("should show the message from z.number() if the value is a number as a string", async () => {
+      // This does NOT work since { valueAsNumber: false }
+      render(<TestForm valueAsNumber={false}/>)
+
+      fireEvent.change(screen.getByPlaceholderText("degree"), {
+        target: { value: "2" },
+      })
+      fireEvent.click(screen.getByText("Submit"))
+
+      await waitFor(() => {
+        expect(screen.getByRole("alert")).toHaveTextContent("Expected number, received string")
+      })
+      expect(screen.queryByTestId("submitted")).not.toBeInTheDocument()
     })
   })
 })

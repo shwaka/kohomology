876a1667a89e8c78f35e2a12ff1811fbcce66967
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-19 10:21:23 +0900

    refactor(website): move validation from formValueSchema to generatorArraySchema

--- website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/generatorArraySchema.ts
+++ website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/generatorArraySchema.ts
@@ -1,8 +1,21 @@
-import { z } from "zod"
+import { RefinementCtx, z } from "zod"
 import { generatorSchema, Generator } from "./generatorSchema"
 import { validateDifferentialValue, validateGeneratorDegrees, validateGeneratorNames } from "./validation"
 
 export const generatorArraySchema = z.array(generatorSchema).superRefine((val: Generator[], ctx) => {
+  addIssueForDifferentialValue(val, ctx)
+  addIssueForGeneratorDegrees(val, ctx)
+  addIssueForGeneratorNames(val ,ctx)
+})
+
+export const formValueSchema = z.object({
+  generatorArray: generatorArraySchema,
+  dummy: z.literal("dummy"),
+})
+
+export type GeneratorFormInput = z.infer<typeof formValueSchema>
+
+function addIssueForDifferentialValue(val: Generator[], ctx: RefinementCtx): void {
   val.forEach((generator, index) => {
     const validationResult = validateDifferentialValue(val, index, generator.differentialValue)
     if (typeof validationResult === "string") {
@@ -13,28 +26,26 @@ export const generatorArraySchema = z.array(generatorSchema).superRefine((val: G
       })
     }
   })
-})
+}
 
-export const formValueSchema = z.object({
-  generatorArray: generatorArraySchema,
-  dummy: z.literal("dummy"),
-}).superRefine((val, ctx) => {
-  const validateDegreesResult = validateGeneratorDegrees(val.generatorArray)
+function addIssueForGeneratorDegrees(val: Generator[], ctx: RefinementCtx): void {
+  const validateDegreesResult = validateGeneratorDegrees(val)
   if (typeof validateDegreesResult === "string") {
     ctx.addIssue({
-      path: ["generatorArray"],
+      path: [],
       code: z.ZodIssueCode.custom,
       message: validateDegreesResult,
     })
   }
-  const validateNamesResult = validateGeneratorNames(val.generatorArray)
+}
+
+function addIssueForGeneratorNames(val: Generator[], ctx: RefinementCtx): void {
+  const validateNamesResult = validateGeneratorNames(val)
   if (typeof validateNamesResult === "string") {
     ctx.addIssue({
-      path: ["generatorArray"],
+      path: [],
       code: z.ZodIssueCode.custom,
       message: validateNamesResult,
     })
   }
-})
-
-export type GeneratorFormInput = z.infer<typeof formValueSchema>
+}

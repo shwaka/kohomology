c667d7486174b8b368e0d14b0625ec25e7900698
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-19 16:25:12 +0900

    test(website): update jest.config.js following deprecation warning

--- website/jest.config.js
+++ website/jest.config.js
@@ -9,13 +9,19 @@ module.exports = {
     "^.+\\.(md|mdx)$": "<rootDir>/markdownTransformer.js",
   },
   setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
-  globals: {
-    "ts-jest": {
-      // "isolatedModules: true" disables type checking on "npm test".
-      // This is necessary to use arrays in tests. (Why?)
-      isolatedModules: true,
-    }
-  },
+  // ts-jest[ts-jest-transformer] (WARN) Define `ts-jest` config under `globals` is deprecated. Please do
+  // transform: {
+  //   <transform_regex>: ['ts-jest', { /* ts-jest config goes here in Jest */ }],
+  // },
+  // See more at https://kulshekhar.github.io/ts-jest/docs/getting-started/presets#advanced
+  // globals: {
+  //   "ts-jest": {
+  //     // "isolatedModules: true" disables type checking on "npm test".
+  //     // This is necessary to use arrays in tests. (Why?)
+  //     // "isolatedModules" is deprecated and will be removed in v30.0.0
+  //     // isolatedModules: true,
+  //   }
+  // },
   modulePathIgnorePatterns: [
     "<rootDir>/kohomology-js/build/js/packages/kohomology-js",
     "<rootDir>/kohomology-js/build/tmp",

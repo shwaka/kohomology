2796c3f1ac54081abf8ee719dc32fc2b0aa03fb4
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-19 15:09:04 +0900

    test(website): fix test for WorkerContext by wrapping button.click() by act()

--- website/src/components/Calculator/WorkerContext/index.test.tsx
+++ website/src/components/Calculator/WorkerContext/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, act } from "@testing-library/react"
 import React from "react"
 import { TestApp } from "./__testutils__/TestApp"
 
@@ -8,11 +8,11 @@ test("WorkerContext", () => {
   const divLogFromListener = screen.getByTestId("show-log-from-listener")
   const divStateValue = screen.getByTestId("show-state-value")
   const button = screen.getByTestId("add3")
-  button.click()
+  act(() => button.click())
   expect(divLog).toContainHTML("value=3")
   expect(divLogFromListener).toContainHTML("value=3")
   expect(divStateValue).toContainHTML("stateValue=3")
-  button.click()
+  act(() => button.click())
   expect(divLog).toContainHTML("value=6")
   expect(divLogFromListener).toContainHTML("value=6")
   expect(divStateValue).toContainHTML("stateValue=6")

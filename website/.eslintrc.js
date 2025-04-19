8da3512477688e1a36e4e0bd59c46425cf7e774c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-19 17:52:08 +0900

    build(website): upgrade eslint, typescript-eslint (WIP)

--- website/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  "extends": ["eslint-config-shwaka", "plugin:testing-library/react", "plugin:jest-dom/recommended"],
-  "plugins": ["testing-library"]
-}
-module.exports = config
---
+++ website/eslint.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+import tseslint from "typescript-eslint"
+
+// const config = {
+//   "extends": ["eslint-config-shwaka", "plugin:testing-library/react", "plugin:jest-dom/recommended"],
+//   "plugins": ["testing-library"]
+// }
+
+export const eslintConfig = tseslint.config(
+  {
+    plugins: {
+
+    }
+  }
+)

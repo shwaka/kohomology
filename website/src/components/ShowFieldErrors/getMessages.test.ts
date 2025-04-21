c6ac9dd4f2b43f1299a1ba931e8f2da4916820d3
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-22 00:40:33 +0900

    test(website): add getMessages.test.ts

---
+++ website/src/components/ShowFieldErrors/getMessages.test.ts
@@ -0,0 +1,45 @@
+import { FieldError } from "react-hook-form"
+import { getMessages } from "./getMessages"
+
+describe("getMessages with criteriaMode=firstError", () => {
+  const criteriaMode = "firstError" as const
+
+  test("single FieldError without types", () => {
+    const fieldError: FieldError = { message: "an error message", type: "a type" }
+    const fieldErrors: FieldError[] = [fieldError]
+    expect(getMessages({ fieldErrors, criteriaMode })).toEqual([fieldError])
+  })
+
+  test("single FieldError with types", () => {
+    // This can't happen in actual react-hook-form (?)
+    const message = "an error message"
+    const type = "a type"
+    const fieldError: FieldError = {
+      type: "",
+      types: { [type]: message },
+    }
+    const fieldErrors: FieldError[] = [fieldError]
+    expect(getMessages({ fieldErrors, criteriaMode })).toEqual([])
+  })
+})
+
+describe("getMessages with criteriaMode=all", () => {
+  const criteriaMode = "all" as const
+
+  test("single FieldError without types", () => {
+    const fieldError: FieldError = { message: "an error message", type: "a type" }
+    const fieldErrors: FieldError[] = [fieldError]
+    expect(getMessages({ fieldErrors, criteriaMode })).toEqual([fieldError])
+  })
+
+  test("single FieldError with types", () => {
+    const message = "an error message"
+    const type = "a type"
+    const fieldError: FieldError = {
+      type: "",
+      types: { [type]: message },
+    }
+    const fieldErrors: FieldError[] = [fieldError]
+    expect(getMessages({ fieldErrors, criteriaMode })).toEqual([{ message, type }])
+  })
+})

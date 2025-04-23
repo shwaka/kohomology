e047fe097d7f5d0550b811ea5230571cf516bb15
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-23 23:15:22 +0900

    fix(website): call onSubmit correctly in ArrayEditorTestUtil.submit

--- website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/index.test.tsx
+++ website/src/components/Calculator/DGAEditorDialog/tabItemArrayEditor/index.test.tsx
@@ -81,8 +81,7 @@ class ArrayEditorTestUtil {
   async submit(): Promise<void> {
     const closeDialog = (): void => { return }
     await act(async () => {
-      // This async/await is necessary (why?)
-      this.result.current.onSubmit(closeDialog)
+      await this.result.current.onSubmit(closeDialog)()
     })
     this.rerender(this.result.current.render(this.closeDialog))
   }

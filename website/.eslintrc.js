3a49c45d44262c6253dbeb4be851088e7436d9bb
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-23 21:07:56 +0900

    style(website): add @typescript-eslint/no-floating-promises

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -3,6 +3,9 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
   "extends": ["eslint-config-shwaka", "plugin:testing-library/react", "plugin:jest-dom/recommended"],
-  "plugins": ["testing-library"]
+  "plugins": ["testing-library"],
+  "rules": {
+    "@typescript-eslint/no-floating-promises": "error",
+  }
 }
 module.exports = config

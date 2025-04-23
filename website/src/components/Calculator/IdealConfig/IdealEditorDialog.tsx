f07ca05b1e516208374ecdd7627b066e8c478e7c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-23 18:37:12 +0900

    refactor(website): add IdealConfig/{IdealEditorDialog.tsx,useIdealEditorDialog.ts}

---
+++ website/src/components/Calculator/IdealConfig/IdealEditorDialog.tsx
@@ -0,0 +1,46 @@
+import { Button, Dialog, DialogActions, DialogContent } from "@mui/material"
+import { useMobileMediaQuery } from "@site/src/utils/useMobileMediaQuery"
+import React from "react"
+import { IdealEditor, IdealEditorProps } from "./IdealEditor"
+
+export interface IdealEditorDialogProps {
+  open: boolean
+  onSubmit: () => void
+  tryToQuit: () => void
+  idealEditorProps: IdealEditorProps
+  disableSubmit: () => boolean
+}
+
+export function IdealEditorDialog({
+  open, onSubmit, tryToQuit,
+  idealEditorProps,
+  disableSubmit,
+}: IdealEditorDialogProps): React.JSX.Element {
+  const mobileMediaQuery = useMobileMediaQuery()
+  return (
+    <Dialog
+      open={open}
+      onClose={tryToQuit}
+      maxWidth="sm"
+      fullWidth={true}
+      PaperProps={{ sx: { [mobileMediaQuery]: { margin: 0, width: "calc(100% - 5pt)" } } }}
+    >
+      <DialogContent>
+        <IdealEditor {...idealEditorProps}/>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={onSubmit}
+          variant="contained"
+          sx={{ textTransform: "none" }}
+          disabled={disableSubmit()}
+        >
+          Apply
+        </Button>
+        <Button onClick={tryToQuit} variant="outlined">
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}

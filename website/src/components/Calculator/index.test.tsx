0b21b72e7653d4b71f380cf52985258474030c13
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-04-25 09:55:11 +0900

    test(website): refactor Calculator/index.test.tsx by using describe

--- website/src/components/Calculator/index.test.tsx
+++ website/src/components/Calculator/index.test.tsx
@@ -13,168 +13,177 @@ beforeEach(() => {
   })
 })
 
-test("Calculator", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  // const calculator = screen.getByTestId("Calculator")
-  clickComputeCohomologyButton()
-  expectResultsToContainHTML(
-    [
-      "Computing $H^n(Λ(x, y), d)$ for",
-      "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
-      "$H^{2} =\\ $ $\\mathbb{Q}\\{$ $[x]$ $\\}$"
-    ],
-  )
-  expectComputeCohomologyButtonToContain("Compute")
+describe("basic features", () => {
+  test("Calculator", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    // const calculator = screen.getByTestId("Calculator")
+    clickComputeCohomologyButton()
+    expectResultsToContainHTML(
+      [
+        "Computing $H^n(Λ(x, y), d)$ for",
+        "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
+        "$H^{2} =\\ $ $\\mathbb{Q}\\{$ $[x]$ $\\}$"
+      ],
+    )
+    expectComputeCohomologyButtonToContain("Compute")
+  })
+
+  test("restart", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    await clickRestartButton()
+    expectResultsToContainHTML(
+      ["The background process is restarted"]
+    )
+    clickComputeCohomologyButton()
+    expectResultsToContainHTML(
+      [
+        "Computing $H^n(Λ(x, y), d)$ for",
+        "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
+        "$H^{2} =\\ $ $\\mathbb{Q}\\{$ $[x]$ $\\}$"
+      ],
+    )
+    expectComputeCohomologyButtonToContain("Compute")
+  })
 })
 
-test("restart", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  await clickRestartButton()
-  expectResultsToContainHTML(
-    ["The background process is restarted"]
-  )
-  clickComputeCohomologyButton()
-  expectResultsToContainHTML(
-    [
-      "Computing $H^n(Λ(x, y), d)$ for",
-      "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
-      "$H^{2} =\\ $ $\\mathbb{Q}\\{$ $[x]$ $\\}$"
-    ],
-  )
-  expectComputeCohomologyButtonToContain("Compute")
-})
-
-test("input json", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  // const calculator = screen.getByTestId("Calculator")
-  // const results = getResultsDiv()
-  const json = `[
+describe("input json", () => {
+  test("valid json", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    // const calculator = screen.getByTestId("Calculator")
+    // const results = getResultsDiv()
+    const json = `[
   ["x", 3, "zero"],
   ["y", 3, "zero"],
   ["z", 5, "x * y"]
 ]`
-  await InputJson.inputValidJson(json)
-  clickComputeCohomologyButton()
-  expectResultsToContainHTML(
-    [
-      "Computing $H^n(Λ(x, y, z), d)$ for",
-      "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
-      "$H^{3} =\\ $ $\\mathbb{Q}\\{$ $[x],\\ $ $[y]$ $\\}$",
-    ]
-  )
-  expectComputeCohomologyButtonToContain("Compute")
-})
-
-test("invalid json", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  const json = "invalid json"
-  await InputJson.inputInvalidJson(json)
-  const dialog = screen.getByRole("dialog")
-  expect(dialog).toContainHTML("Unexpected JSON token at offset 0")
-  expect(dialog).toContainHTML(`JSON input: ${json}`)
-})
-
-test("compute cohomology of LX", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  selectComputationTarget("freeLoopSpace")
-  clickComputeCohomologyButton()
-  expectResultsToContainHTML(
-    [
-      "Computing $H^n(Λ({x}, {y}, \\bar{x}, \\bar{y}), d)$ for",
-      "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
-      "$H^{1} =\\ $ $\\mathbb{Q}\\{$ $[\\bar{x}]$ $\\}$",
-      "$H^{2} =\\ $ $\\mathbb{Q}\\{$ $[{x}]$ $\\}$",
-    ]
-  )
-  expectComputeCohomologyButtonToContain("Compute")
-})
-
-test("compute cohomology of ΛV/I", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  selectComputationTarget("idealQuot")
-  await InputIdeal.inputValidIdealGenerator(["x"])
-  clickComputeCohomologyButton()
-  expectResultsToContainHTML(
-    [
-      "Computing $H^n((Λ(x, y), d)/\\mathrm{DGIdeal}(x))$ for $0 \\leq n \\leq 20$",
-      "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[[1]]$ $\\}$",
-      "$H^{2} =\\ $ $0$",
-      "$H^{3} =\\ $ $\\mathbb{Q}\\{$ $[[y]]$ $\\}$",
-    ],
-  )
-  expectComputeCohomologyButtonToContain("Compute")
-})
-
-test("ideal not closed under d", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  selectComputationTarget("idealQuot")
-  await InputIdeal.inputInvalidIdealGenerator(["y"])
-  const dialog = screen.getByRole("dialog")
-  expect(dialog).toContainHTML("d(y)=x^2 must be contained in the ideal Ideal(y) to define dg ideal.")
-})
-
-test("empty ideal generator", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  selectComputationTarget("idealQuot")
-  await InputIdeal.inputInvalidIdealGenerator([""])
-  const dialog = screen.getByRole("dialog")
-  expect(dialog).toContainHTML("Please enter the generator.")
-})
-
-test("invalid generator of DGA", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  selectComputationTarget("idealQuot")
-  await InputIdeal.inputInvalidIdealGenerator(["a"])
-  const dialog = screen.getByRole("dialog")
-  expect(dialog).toContainHTML("Invalid generator name: a")
-  expect(dialog).toContainHTML("Valid names are: x, y")
-})
-
-test("parse failure", async () => {
-  render(<Calculator/>)
-  expectInitialState()
-  selectComputationTarget("idealQuot")
-  await InputIdeal.inputInvalidIdealGenerator(["+"])
-  const dialog = screen.getByRole("dialog")
-  expect(dialog).toContainHTML("Could not parse input: AlternativesFailure")
-})
-
-
-test("url query", async () => {
-  mockUseLocation.mockReturnValue({
-    search: "?dgaJson=%5B%5B%22x%22%2C3%2C%22zero%22%5D%2C%5B%22y%22%2C3%2C%22zero%22%5D%2C%5B%22z%22%2C5%2C%22x+*+y%22%5D%5D"
+    await InputJson.inputValidJson(json)
+    clickComputeCohomologyButton()
+    expectResultsToContainHTML(
+      [
+        "Computing $H^n(Λ(x, y, z), d)$ for",
+        "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
+        "$H^{3} =\\ $ $\\mathbb{Q}\\{$ $[x],\\ $ $[y]$ $\\}$",
+      ]
+    )
+    expectComputeCohomologyButtonToContain("Compute")
   })
-  render(<Calculator/>)
-  expectInitialState()
-  clickComputeCohomologyButton()
-  expectResultsToContainHTML(
-    [
-      "Computing $H^n(Λ(x, y, z), d)$ for",
-      "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
-      "$H^{3} =\\ $ $\\mathbb{Q}\\{$ $[x],\\ $ $[y]$ $\\}$",
-    ]
-  )
-  expectComputeCohomologyButtonToContain("Compute")
+
+  test("invalid json", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    const json = "invalid json"
+    await InputJson.inputInvalidJson(json)
+    const dialog = screen.getByRole("dialog")
+    expect(dialog).toContainHTML("Unexpected JSON token at offset 0")
+    expect(dialog).toContainHTML(`JSON input: ${json}`)
+  })
 })
 
-test("url query with invalid json", async () => {
-  mockUseLocation.mockReturnValue({
-    search: "?dgaJson=invalidJson"
+describe("freeLoopSpace", () => {
+  test("compute cohomology of LX", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    selectComputationTarget("freeLoopSpace")
+    clickComputeCohomologyButton()
+    expectResultsToContainHTML(
+      [
+        "Computing $H^n(Λ({x}, {y}, \\bar{x}, \\bar{y}), d)$ for",
+        "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
+        "$H^{1} =\\ $ $\\mathbb{Q}\\{$ $[\\bar{x}]$ $\\}$",
+        "$H^{2} =\\ $ $\\mathbb{Q}\\{$ $[{x}]$ $\\}$",
+      ]
+    )
+    expectComputeCohomologyButtonToContain("Compute")
   })
-  render(<Calculator/>)
-  expectInitialState()
-  expectSnackbarToContainHTML(
-    [
-      "[Error] Invalid JSON is given as URL parameter.",
-    ]
-  )
+})
+
+describe("idealQuot", () => {
+  test("compute cohomology of ΛV/I", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    selectComputationTarget("idealQuot")
+    await InputIdeal.inputValidIdealGenerator(["x"])
+    clickComputeCohomologyButton()
+    expectResultsToContainHTML(
+      [
+        "Computing $H^n((Λ(x, y), d)/\\mathrm{DGIdeal}(x))$ for $0 \\leq n \\leq 20$",
+        "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[[1]]$ $\\}$",
+        "$H^{2} =\\ $ $0$",
+        "$H^{3} =\\ $ $\\mathbb{Q}\\{$ $[[y]]$ $\\}$",
+      ],
+    )
+    expectComputeCohomologyButtonToContain("Compute")
+  })
+
+  test("ideal not closed under d", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    selectComputationTarget("idealQuot")
+    await InputIdeal.inputInvalidIdealGenerator(["y"])
+    const dialog = screen.getByRole("dialog")
+    expect(dialog).toContainHTML("d(y)=x^2 must be contained in the ideal Ideal(y) to define dg ideal.")
+  })
+
+  test("empty ideal generator", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    selectComputationTarget("idealQuot")
+    await InputIdeal.inputInvalidIdealGenerator([""])
+    const dialog = screen.getByRole("dialog")
+    expect(dialog).toContainHTML("Please enter the generator.")
+  })
+
+  test("invalid generator of DGA", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    selectComputationTarget("idealQuot")
+    await InputIdeal.inputInvalidIdealGenerator(["a"])
+    const dialog = screen.getByRole("dialog")
+    expect(dialog).toContainHTML("Invalid generator name: a")
+    expect(dialog).toContainHTML("Valid names are: x, y")
+  })
+
+  test("parse failure", async () => {
+    render(<Calculator/>)
+    expectInitialState()
+    selectComputationTarget("idealQuot")
+    await InputIdeal.inputInvalidIdealGenerator(["+"])
+    const dialog = screen.getByRole("dialog")
+    expect(dialog).toContainHTML("Could not parse input: AlternativesFailure")
+  })
+})
+
+describe("url query", () => {
+  test("url query with valid json", async () => {
+    mockUseLocation.mockReturnValue({
+      search: "?dgaJson=%5B%5B%22x%22%2C3%2C%22zero%22%5D%2C%5B%22y%22%2C3%2C%22zero%22%5D%2C%5B%22z%22%2C5%2C%22x+*+y%22%5D%5D"
+    })
+    render(<Calculator/>)
+    expectInitialState()
+    clickComputeCohomologyButton()
+    expectResultsToContainHTML(
+      [
+        "Computing $H^n(Λ(x, y, z), d)$ for",
+        "$H^{0} =\\ $ $\\mathbb{Q}\\{$ $[1]$ $\\}$",
+        "$H^{3} =\\ $ $\\mathbb{Q}\\{$ $[x],\\ $ $[y]$ $\\}$",
+      ]
+    )
+    expectComputeCohomologyButtonToContain("Compute")
+  })
+
+  test("url query with invalid json", async () => {
+    mockUseLocation.mockReturnValue({
+      search: "?dgaJson=invalidJson"
+    })
+    render(<Calculator/>)
+    expectInitialState()
+    expectSnackbarToContainHTML(
+      [
+        "[Error] Invalid JSON is given as URL parameter.",
+      ]
+    )
+  })
 })

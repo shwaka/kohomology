2208d0810a913addc367623d7b5bd408a531ba59
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-05-05 18:57:54 +0900

    test(website): improve test for EditorDialog

--- website/src/components/EditorDialog/EditorDialog.test.tsx
+++ website/src/components/EditorDialog/EditorDialog.test.tsx
@@ -20,6 +20,36 @@ function EditorDialogTestContainer({ editor }: ContainerProps): React.JSX.Elemen
   )
 }
 
+async function openDialog(): Promise<HTMLElement> {
+  const openButton = screen.getByText("Open dialog")
+  fireEvent.click(openButton)
+
+  await waitFor(() => {
+    expect(screen.getByRole("dialog")).toBeInTheDocument()
+  })
+
+  const dialog = screen.getByRole("dialog")
+  return dialog
+}
+
+async function apply(dialog: HTMLElement): Promise<void> {
+  const applyButton = within(dialog).getByText("Apply")
+  fireEvent.click(applyButton)
+
+  await waitFor(() => {
+    expect(dialog).not.toBeInTheDocument()
+  })
+}
+
+async function cancel(dialog: HTMLElement): Promise<void> {
+  const applyButton = within(dialog).getByText("Cancel")
+  fireEvent.click(applyButton)
+
+  await waitFor(() => {
+    expect(dialog).not.toBeInTheDocument()
+  })
+}
+
 describe("EditorDialog", () => {
   test("submit", async () => {
     const onSubmit = jest.fn()
@@ -32,17 +62,26 @@ describe("EditorDialog", () => {
     }
     render(<EditorDialogTestContainer editor={editor}/>)
 
-    const openButton = screen.getByText("Open dialog")
-    fireEvent.click(openButton)
-
-    const dialog = screen.getByRole("dialog")
-    const applyButton = within(dialog).getByText("Apply")
-    fireEvent.click(applyButton)
-
-    await waitFor(() => {
-      expect(dialog).not.toBeInTheDocument()
-    })
+    const dialog = await openDialog()
+    await apply(dialog)
 
     expect(onSubmit).toHaveBeenCalled()
+  })
+
+  test("cancel", async () => {
+    const onSubmit = jest.fn()
+    const editor: Editor = {
+      renderContent: (_closeDialog) => (<div>Content of editor</div>),
+      getOnSubmit: (closeDialog) => async (e) => {
+        onSubmit(e),
+        closeDialog()
+      },
+    }
+    render(<EditorDialogTestContainer editor={editor}/>)
+
+    const dialog = await openDialog()
+    await cancel(dialog)
+
+    expect(onSubmit).not.toHaveBeenCalled()
   })
 })

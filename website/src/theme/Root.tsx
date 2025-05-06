1b820c7fdd2b75d735f8f4d07b198aed3038a0f5
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-05-06 12:33:44 +0900

    feat(website): change background color of Navbar in development mode

--- website/src/theme/Root.tsx
+++ website/src/theme/Root.tsx
@@ -1,12 +1,29 @@
 import Head from "@docusaurus/Head"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
-import React from "react"
+import React, { useEffect } from "react"
+import { isDevelopmentMode } from "../utils/isDevelopmentMode"
 
 // https://docusaurus.io/docs/swizzling#wrapper-your-site-with-root
 
 export default function Root({children}: {children: React.ReactNode}): React.JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   const baseUrl = siteConfig.baseUrl // baseUrl ends with "/"
+
+  useEffect(() => {
+    // Set data attribute: <html data-build-env="dev" ...>
+    // This is used ins src/css/custom.scss
+    // I don't know why, but the following did not work (overridden by docusaurus?)
+    // - document.documentElement.classList.add("dev-mode")
+    // - document.body.classList.add("dev-mode")
+    const key = "data-build-env"
+    if (isDevelopmentMode()) {
+      document.documentElement.setAttribute(key, "dev")
+    } else {
+      document.documentElement.setAttribute(key, "prod")
+    }
+    // Since Root never unmounts, we do not need cleanup functions.
+  }, [])
+
   return (
     <React.Fragment>
       <Head>

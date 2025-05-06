451fc73f32b0ecdd6e7a12f6a48d384c6e8e2548
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-05-06 10:17:03 +0900

    refactor(website): use TextEdtitor in tabItemJsonEditor.tsx

--- website/src/components/Calculator/DGAEditorDialog/tabItemJsonEditor.tsx
+++ website/src/components/Calculator/DGAEditorDialog/tabItemJsonEditor.tsx
@@ -1,65 +1,22 @@
-import { OnSubmit } from "@components/EditorDialog"
 import { TabItem } from "@components/TabDialog"
-import { Alert, Stack, TextField } from "@mui/material"
 import { validateJson } from "kohomology-js"
-import React from "react"
-import { DeepRequired, FieldErrorsImpl, useForm, UseFormRegister } from "react-hook-form"
-
-// type TextAreaEvent = React.ChangeEvent<HTMLTextAreaElement>
+import { useTextEditor } from "./TextEditor"
 
 export function useTabItemJsonEditor(args: {
   json: string
   updateDgaWrapper: (json: string) => void
 }): TabItem {
-  const { register, handleSubmit, getValues, reset, formState: { errors } } = useForm<FormInput>({
-    mode: "onBlur",
-    reValidateMode: "onBlur",
-    shouldUnregister: false, // necessary for setValue with MUI
-    defaultValues: { json: args.json },
+  const editor = useTextEditor({
+    text: args.json, setText: args.updateDgaWrapper,
+    preventPrompt: "Your JSON is not saved. Are you sure you want to quit?",
+    fieldLabel: "Input your DGA",
+    fieldTestid: "JsonEditorDialog-input-json",
+    validate,
   })
-  function getOnSubmit(closeDialog: () => void): OnSubmit {
-    return handleSubmit(
-      ({ json: formJson }) => {
-        args.updateDgaWrapper(formJson)
-        closeDialog()
-      }
-    )
-  }
-  function beforeOpen(): void {
-    reset({ json: args.json })
-  }
-  function preventQuit(): string | undefined {
-    if (getValues().json !== args.json) {
-      return "Your JSON is not saved. Are you sure you want to quit?"
-    } else {
-      return undefined
-    }
-  }
-  function disableSubmit(): boolean {
-    return errors.json !== undefined
-  }
-  const jsonEditorProps: JsonEditorProps = {
-    register, errors
-  }
   return {
     label: "JSON",
-    editor: {
-      preventQuit,
-      getOnSubmit,
-      beforeOpen,
-      disableSubmit,
-      renderContent: (_) => (<JsonEditor {...jsonEditorProps}/>),
-    }
+    editor,
   }
-}
-
-interface JsonEditorProps {
-  register: UseFormRegister<FormInput>
-  errors: FieldErrorsImpl<DeepRequired<FormInput>>
-}
-
-interface FormInput {
-  json: string
 }
 
 function validate(value: string): true | string {
@@ -69,18 +26,4 @@ function validate(value: string): true | string {
   } else {
     return validationResult.message
   }
-}
-
-function JsonEditor({ register, errors }: JsonEditorProps): React.JSX.Element {
-  return (
-    <Stack spacing={2} sx={{ marginTop: 1 }}>
-      <TextField
-        label="Input your DGA" multiline
-        inputProps={{"data-testid": "JsonEditorDialog-input-json"}}
-        {...register("json", { validate })}
-        error={errors.json !== undefined}
-      />
-      {errors.json !== undefined && <Alert severity="error">{errors.json.message}</Alert>}
-    </Stack>
-  )
 }

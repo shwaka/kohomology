3826bab68496bac16d3a62505e0f61fb24d7543d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-06 22:51:21 +0900

    refactor(website): add useQueryTabs.tsx

--- website/webapp/src/components/Playground/index.tsx
+++ website/webapp/src/components/Playground/index.tsx
@@ -1,11 +1,5 @@
-import React, { useCallback } from "react"
-import { useQueryState } from "./useQueryState"
-
-interface PlaygroundTab {
-  key: string
-  name: string
-  render: () => React.JSX.Element
-}
+import React from "react"
+import { QueryTab, useQueryTabs } from "./useQueryTabs"
 
 const tabs = [
   {
@@ -18,55 +12,15 @@ const tabs = [
     name: "Another",
     render: () => (<div>This is another tab.</div>)
   },
-] as const satisfies PlaygroundTab[]
-
-type TabKey = (typeof tabs)[number]["key"]
-
-const tabKeys: TabKey[] = tabs.map((tab) => tab.key)
-function isTabKey(tabKey: string): tabKey is TabKey {
-  return (tabKeys as string[]).includes(tabKey)
-}
-
-function getTab(tabKey: TabKey): PlaygroundTab {
-  for (const tab of tabs) {
-    if (tab.key === tabKey) {
-      return tab
-    }
-  }
-  throw new Error(`No tab found for key ${tabKey}`)
-}
-
-function useTabKey(): [TabKey, (newTabKey: string) => void] {
-  const defaultTabKey = tabs[0].key
-  const [tabKeyString, setTabKeyString] = useQueryState("tab-key", defaultTabKey)
-  const tabKey: TabKey = isTabKey(tabKeyString) ? tabKeyString : defaultTabKey
-  const setTabKey = useCallback((newTabKey: string) => {
-    if (isTabKey(newTabKey)) {
-      setTabKeyString(newTabKey)
-    } else {
-      throw new Error(`${newTabKey} is not a valid key for PlaygroundTab`)
-    }
-  }, [setTabKeyString])
-  return [tabKey, setTabKey]
-}
+] as const satisfies QueryTab<string>[]
 
 export function Playground(): React.JSX.Element {
-  const [tabKey, setTabKey] = useTabKey()
-  const tab = getTab(tabKey)
+  const { renderSelect, renderTabs } = useQueryTabs(tabs)
   return (
     <div>
       This is playground.
-      <select
-        value={tabKey}
-        onChange={(e) => setTabKey(e.target.value)}
-      >
-        {tabs.map((tab) => (
-          <option value={tab.key} key={tab.key}>
-            {tab.name}
-          </option>
-        ))}
-      </select>
-      {tab.render()}
+      {renderSelect()}
+      {renderTabs()}
     </div>
   )
 }
---
+++ website/webapp/src/components/Playground/useQueryTabs.tsx
@@ -0,0 +1,66 @@
+import React, { useCallback } from "react"
+import { useQueryState } from "./useQueryState"
+
+export interface QueryTab<K extends string> {
+  key: K
+  name: string
+  render: () => React.JSX.Element
+}
+
+function isTabKey<K extends string>(tabKeys: K[], tabKey: string): tabKey is K {
+  return (tabKeys as string[]).includes(tabKey)
+}
+
+function getTab<K extends string>(tabs: QueryTab<K>[], tabKey: K): QueryTab<K> {
+  for (const tab of tabs) {
+    if (tab.key === tabKey) {
+      return tab
+    }
+  }
+  throw new Error(`No tab found for key ${tabKey}`)
+}
+
+function getKeys<K extends string>(tabs: QueryTab<K>[]): K[] {
+  return tabs.map((tab) => tab.key)
+}
+
+function useTabKey<K extends string>(tabKeys: K[]): [K, (newTabKey: string) => void] {
+  const defaultTabKey: K = tabKeys[0]
+  const [tabKeyString, setTabKeyString] = useQueryState("tab-key", defaultTabKey)
+  const tabKey: K = isTabKey(tabKeys, tabKeyString) ? tabKeyString : defaultTabKey
+  const setTabKey = useCallback((newTabKey: string) => {
+    if (isTabKey(tabKeys, newTabKey)) {
+      setTabKeyString(newTabKey)
+    } else {
+      throw new Error(`${newTabKey} is not a valid key for PlaygroundTab`)
+    }
+  }, [setTabKeyString, tabKeys])
+  return [tabKey, setTabKey]
+}
+
+interface UseQueryTabsReturnValue {
+  renderSelect: () => React.JSX.Element
+  renderTabs: () => React.JSX.Element
+}
+
+export function useQueryTabs<K extends string>(tabs: QueryTab<K>[]): UseQueryTabsReturnValue {
+  const tabKeys = getKeys(tabs)
+  const [tabKey, setTabKey] = useTabKey(tabKeys)
+  const tab = getTab(tabs, tabKey)
+
+  return {
+    renderSelect: () => (
+      <select
+        value={tabKey}
+        onChange={(e) => setTabKey(e.target.value)}
+      >
+        {tabs.map((tab) => (
+          <option value={tab.key} key={tab.key}>
+            {tab.name}
+          </option>
+        ))}
+      </select>
+    ),
+    renderTabs: () => tab.render(),
+  }
+}

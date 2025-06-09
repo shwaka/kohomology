d3b1c647df78a9cfd17ed004b68847c7c14ac91c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-09 19:47:57 +0900

    feat(website): show [dev] in docs page (close #317)

---
+++ website/webapp/src/theme/DocItem/Metadata/index.tsx
@@ -0,0 +1,18 @@
+import React, {type ReactNode} from "react"
+
+import {useDoc} from "@docusaurus/plugin-content-docs/client"
+import {PageMetadata} from "@docusaurus/theme-common"
+import { getTitleWithDev } from "@theme/getTitleWithDev"
+
+export default function DocItemMetadata(): ReactNode {
+  const {metadata, frontMatter, assets} = useDoc()
+  const title = getTitleWithDev(metadata.title)
+  return (
+    <PageMetadata
+      title={title}
+      description={metadata.description}
+      keywords={frontMatter.keywords}
+      image={assets.image ?? frontMatter.image}
+    />
+  )
+}

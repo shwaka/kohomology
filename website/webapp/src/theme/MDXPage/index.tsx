97bd99d56b50331eafd672f48d03af4d35c0d440
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-09 19:46:19 +0900

    feat(website): show [dev] in MDX page (#317)

---
+++ website/webapp/src/theme/MDXPage/index.tsx
@@ -0,0 +1,46 @@
+import React, {type ReactNode} from "react"
+
+import type {WrapperProps} from "@docusaurus/types"
+import { getTitleWithDev } from "@theme/getTitleWithDev"
+import type MDXPageType from "@theme/MDXPage"
+import MDXPage from "@theme-original/MDXPage"
+
+type Props = WrapperProps<typeof MDXPageType>
+type Content = Props["content"] // LoadedMDXContent<...>
+
+function getContentWithUpdatedTitle(content: Content): Content {
+  // export type LoadedMDXContent<FrontMatter, Metadata, Assets = undefined> = {
+  //   ...
+  //   readonly metadata: Metadata;
+  //   ...
+  //   (): ReactNode; // <- content is callable!
+  // };
+
+  // Since content is callable (see the above def of LoadedMDXContent),
+  // we use Object.assign (together with the spread operator).
+  return Object.assign(
+    () => content(),
+    {
+      ...content,
+      metadata: {
+        ...content.metadata,
+        title: getTitleWithDev(content.metadata.title),
+      }
+    }
+  )
+}
+
+function getPropsWithUpdatedTitle(props: Props): Props {
+  return {
+    ...props,
+    content: getContentWithUpdatedTitle(props.content),
+  }
+}
+
+export default function MDXPageWrapper(props: Props): ReactNode {
+  return (
+    <React.Fragment>
+      <MDXPage {...getPropsWithUpdatedTitle(props)} />
+    </React.Fragment>
+  )
+}

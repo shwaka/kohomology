514e25df372ad32c67fea08b622e81749b1377d1
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-09 10:07:57 +0900

    style(website): add eslint-import.config.mjs

---
+++ website/webapp/eslint-import.config.mjs
@@ -0,0 +1,26 @@
+import { eslintConfigShwakaImport } from "eslint-config-shwaka"
+import importPlugin from "eslint-plugin-import"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+  tseslint.configs.base,
+  {
+    // Due to eslint-disable, definition for react-hooks/exhaustive-deps is required:
+    //   /home/shun/Git/kohomology/website/webapp/src/calculator/useScrollToBottom.ts
+    //     29:3  error  Definition for rule 'react-hooks/exhaustive-deps' was not found
+    // To avoid this error, we define dummy plugins here.
+    plugins: {
+      "react-hooks": {
+        rules: {
+          "exhaustive-deps": {},
+        },
+      },
+      "testing-library": {
+        rules: {
+          "no-node-access": {},
+        }
+      },
+    },
+  },
+  ...eslintConfigShwakaImport,
+)

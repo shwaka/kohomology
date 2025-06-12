343c81881914a9446ee6de86e1b71876d9f5613f
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-13 01:15:49 +0900

    test(website): add WorkerForm to MessageBoxForWorkerSample.tsx

--- website/webapp/src/calculator/MessageBoxForWorker/__playground__/MessageBoxForWorkerSample.tsx
+++ website/webapp/src/calculator/MessageBoxForWorker/__playground__/MessageBoxForWorkerSample.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+import { useWorker } from "@calculator/WorkerContext"
 import SampleWorker from "worker-loader!./sample.worker"
 
 import { MessageBoxForWorker } from ".."
@@ -15,7 +16,19 @@ export function MessageBoxForWorkerSample(): React.JSX.Element {
         value: 0,
       }}
     >
+      <WorkerForm/>
       <MessageBoxForWorker context={sampleWorkerContext}/>
     </sampleWorkerContext.Provider>
+  )
+}
+
+function WorkerForm(): React.JSX.Element {
+  const { postMessage } = useWorker(sampleWorkerContext)
+  return (
+    <div>
+      <button onClick={() => postMessage({ value: 1 })}>
+        Add 1
+      </button>
+    </div>
   )
 }

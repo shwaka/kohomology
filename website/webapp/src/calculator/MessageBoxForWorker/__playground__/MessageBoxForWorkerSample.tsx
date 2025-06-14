67a9e54598613f43952a721088e489aa26592dd1
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-14 15:50:21 +0900

    fix(website): re-enable MessageBoxForWorkerSample (#318)

--- website/webapp/src/calculator/MessageBoxForWorker/__playground__/MessageBoxForWorkerSample.tsx
+++ website/webapp/src/calculator/MessageBoxForWorker/__playground__/MessageBoxForWorkerSample.tsx
@@ -1,38 +1,33 @@
 import React from "react"
 
-// import { useWorker } from "@calculator/WorkerContext"
-// import SampleWorker from "worker-loader!./sample.worker"
+import { useWorker } from "@calculator/WorkerContext"
 
-// import { MessageBoxForWorker } from ".."
-// import { sampleWorkerContext } from "./sampleWorkerContext"
+import { MessageBoxForWorker } from ".."
+import { sampleWorkerContext } from "./sampleWorkerContext"
 
 export function MessageBoxForWorkerSample(): React.JSX.Element {
-  return (<div>Temporarily disabled</div>)
+  const createWorker = (): Worker => new Worker(new URL("./sample.worker.ts", import.meta.url))
+
+  return (
+    <sampleWorkerContext.Provider
+      createWorker={createWorker}
+      defaultState={{
+        value: 0,
+      }}
+    >
+      <WorkerForm/>
+      <MessageBoxForWorker context={sampleWorkerContext}/>
+    </sampleWorkerContext.Provider>
+  )
 }
 
-// export function MessageBoxForWorkerSample(): React.JSX.Element {
-//   const createWorker = (): Worker => new SampleWorker()
-//
-//   return (
-//     <sampleWorkerContext.Provider
-//       createWorker={createWorker}
-//       defaultState={{
-//         value: 0,
-//       }}
-//     >
-//       <WorkerForm/>
-//       <MessageBoxForWorker context={sampleWorkerContext}/>
-//     </sampleWorkerContext.Provider>
-//   )
-// }
-//
-// function WorkerForm(): React.JSX.Element {
-//   const { postMessage } = useWorker(sampleWorkerContext)
-//   return (
-//     <div>
-//       <button onClick={() => postMessage({ value: 1 })}>
-//         Add 1
-//       </button>
-//     </div>
-//   )
-// }
+function WorkerForm(): React.JSX.Element {
+  const { postMessage } = useWorker(sampleWorkerContext)
+  return (
+    <div>
+      <button onClick={() => postMessage({ value: 1 })}>
+        Add 1
+      </button>
+    </div>
+  )
+}

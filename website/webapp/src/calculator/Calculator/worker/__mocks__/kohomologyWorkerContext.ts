4b2d0aa04567fbb0fa935876a98fb6d152936e3a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-15 17:50:53 +0900

    refactor(website): remove worker/__mocks__/kohomology.worker.ts

--- website/webapp/src/calculator/Calculator/worker/__mocks__/kohomologyWorkerContext.ts
+++ website/webapp/src/calculator/Calculator/worker/__mocks__/kohomologyWorkerContext.ts
@@ -1,10 +1,28 @@
 import { createWorkerContext } from "@calculator/WorkerContext"
+import { MockWorker } from "@calculator/WorkerContext/__testutils__/MockWorker"
+import { CallbackData } from "@calculator/WorkerContext/expose"
 
-import KohomologyWorker from "./kohomology.worker"
+import { KohomologyWorkerImpl } from "../KohomologyMessageHandler"
 import { WorkerFunc, WorkerInput, WorkerOutput, WorkerState } from "../workerInterface"
 
+function getWorkerImpl(
+  callbackData: CallbackData<WorkerOutput, WorkerState>
+): KohomologyWorkerImpl {
+  return new KohomologyWorkerImpl({
+    ...callbackData,
+    log: (_message) => {
+      // console.log(_message)
+      return
+    },
+    error: (_message) => {
+      // console.error(_message)
+      return
+    },
+  })
+}
+
 function createWorker(): Worker {
-  return new KohomologyWorker() as unknown as Worker
+  return new MockWorker(getWorkerImpl) as unknown as Worker
 }
 
 export const kohomologyWorkerContext = createWorkerContext<WorkerInput, WorkerOutput, WorkerState, WorkerFunc>(createWorker)

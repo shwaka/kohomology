515a081f4fd149c967a6af3ae5547a6076880f93
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-16 23:16:08 +0900

    feat(website): add ThemeProvider to Playground

--- website/webapp/src/components/Playground/index.tsx
+++ website/webapp/src/components/Playground/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 
+import { useCustomTheme } from "@calculator/Calculator/useCustomTheme"
 import { MessageBoxForWorkerSample } from "@calculator/MessageBoxForWorker/__playground__/MessageBoxForWorkerSample"
 import { ShowErrorsSample } from "@calculator/ShowErrors/__playground__/ShowErrorsSample"
 import { SortableFieldsSample } from "@calculator/SortableFields/__playground__/SortableFieldsSample"
 import { TextEditorSample } from "@calculator/TextEditor/__playground__/TextEditorSample"
 import BrowserOnly from "@docusaurus/BrowserOnly"
+import { ThemeProvider } from "@mui/material"
 
 import { PlaygroundBox, usePlaygroundBox } from "./PlaygroundBox"
 import { QueryTab } from "./QueryTab"
@@ -36,15 +38,19 @@ const tabs = [
 function PlaygroundImpl(): React.JSX.Element {
   const { renderSelect, renderTabs } = useQueryTabs(tabs)
   const { props, renderControl } = usePlaygroundBox()
+  const theme = useCustomTheme()
+
   return (
-    <div>
-      This is playground.
-      {renderSelect()}
-      {renderControl()}
-      <PlaygroundBox {...props}>
-        {renderTabs()}
-      </PlaygroundBox>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        This is playground.
+        {renderSelect()}
+        {renderControl()}
+        <PlaygroundBox {...props}>
+          {renderTabs()}
+        </PlaygroundBox>
+      </div>
+    </ThemeProvider>
   )
 }
 

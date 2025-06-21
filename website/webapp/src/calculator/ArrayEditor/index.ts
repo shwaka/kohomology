b677baf0cda135bbfe254941ca0c7a99b407f888
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-21 17:14:09 +0900

    refactor(website): remove unused exports from ArrayEdior/index.ts

--- website/webapp/src/calculator/ArrayEditor/index.ts
+++ website/webapp/src/calculator/ArrayEditor/index.ts
@@ -1,4 +1,2 @@
 export { ArrayEditorItem, type FieldOptions } from "./ArrayEditorItem"
-export { ArrayEditor } from "./ArrayEditor"
-export { type UseArrayEditorArgs } from "./UseArrayEditorArgs"
 export { useArrayEditor } from "./useArrayEditor"

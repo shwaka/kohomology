a43429677d2bb78696f64b328962226ec8ef4831
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-26 17:36:06 +0900

    style(website): remove unused eslint-disable directive from *.worker.ts

--- website/webapp/src/calculator/MessageBoxForWorker/__playground__/sample.worker.ts
+++ website/webapp/src/calculator/MessageBoxForWorker/__playground__/sample.worker.ts
@@ -2,7 +2,6 @@ import { expose } from "@calculator/WorkerContext/expose"
 
 import { getSampleWorkerImpl } from "./SampleWorker"
 
-// eslint-disable-next-line no-restricted-globals
 const ctx = self as unknown as Worker
 
 const exposed = expose(ctx.postMessage.bind(ctx), getSampleWorkerImpl)
--- website/webapp/src/calculator/WorkerContext/__playground__/my.worker.ts
+++ website/webapp/src/calculator/WorkerContext/__playground__/my.worker.ts
@@ -2,7 +2,6 @@ import { expose } from "@calculator/WorkerContext/expose"
 
 import { getMyWorkerImpl } from "./MyWorkerImpl"
 
-// eslint-disable-next-line no-restricted-globals
 const ctx = self as unknown as Worker
 
 const exposed = expose(ctx.postMessage.bind(ctx), getMyWorkerImpl)

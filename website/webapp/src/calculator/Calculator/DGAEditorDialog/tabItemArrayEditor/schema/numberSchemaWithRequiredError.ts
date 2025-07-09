955c2b0c79f2749982fc40123cd86d5dc9570402
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-07-09 21:11:44 +0900

    docs(website): add comment for numberSchemaWithRequiredError

--- website/webapp/src/calculator/Calculator/DGAEditorDialog/tabItemArrayEditor/schema/numberSchemaWithRequiredError.ts
+++ website/webapp/src/calculator/Calculator/DGAEditorDialog/tabItemArrayEditor/schema/numberSchemaWithRequiredError.ts
@@ -1,5 +1,10 @@
 import { z } from "zod/v4"
 
+// numberSchemaWithRequiredError is intended to be used with react-hook-form.
+// Consider an input tag with register(key, { valueAsNumber: true }).
+// Its value is NaN if the input is empty.
+// z.number().parse(NaN) throws an error, but its message cannot be customized.
+// numberSchemaWithRequiredError throws an error with a custom error message.
 export function numberSchemaWithRequiredError(message: string): z.ZodPipe<z.ZodTransform<number, number>, z.ZodNumber> {
   return z.preprocess(
     (val) => {

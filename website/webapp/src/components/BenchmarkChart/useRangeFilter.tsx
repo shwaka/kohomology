50c49f1ca8d7afa10f749c984379f12a6fbf7516
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-07-10 18:53:27 +0900

    feat(website): assert that the array items is sorted

--- website/webapp/src/components/BenchmarkChart/useRangeFilter.tsx
+++ website/webapp/src/components/BenchmarkChart/useRangeFilter.tsx
@@ -1,6 +1,7 @@
-import { useState, ReactElement } from "react"
+import { useState, ReactElement, useEffect } from "react"
 
 import { Slider } from "@mui/material"
+import { range } from "remeda"
 
 interface RangeSliderProps<T> {
   items: T[]
@@ -37,6 +38,11 @@ export function useRangeFilter<T>(
   getValue: (item: T) => number,
   getLabel: (item: T, index: number) => string,
 ): UseRangeFilterReturnValue<T> {
+  useEffect(() => {
+    if (!isSorted(items.map(getValue))) {
+      throw new Error("items is not sorted")
+    }
+  }, [items, getValue])
   const [indexRange, setIndexRange] = useState<number[]>([0, items.length - 1])
   const isSelected = (item: T): boolean => {
     const value = getValue(item)
@@ -53,4 +59,13 @@ export function useRangeFilter<T>(
       getLabel,
     }
   }
+}
+
+function isSorted<T>(items: T[]): boolean {
+  for (const i of range(0, items.length - 1)) {
+    if (items[i] >= items[i + 1]) {
+      return false
+    }
+  }
+  return true
 }

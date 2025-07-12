391e4b76c1fe37ad48fbc496ff883f312f1e8b9c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-07-12 17:23:17 +0900

    docs(website): add comment to useArrayChanged.ts

--- website/webapp/src/components/BenchmarkChart/useArrayChanged.ts
+++ website/webapp/src/components/BenchmarkChart/useArrayChanged.ts
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react"
 import { isDeepEqual } from "remeda"
 
 export function useArrayChanged<T>(array: T[], callbackOnChange: () => void): void {
+  // Use useRef instead of useState since there is no need to re-render
   const prevArrayRef = useRef<T[] | null>(null)
   useEffect(() => {
     if (prevArrayRef.current !== null) {

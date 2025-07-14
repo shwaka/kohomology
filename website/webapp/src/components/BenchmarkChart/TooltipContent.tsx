cfbb28e444912efb3a210c6372a4ae780f6627d6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-07-14 09:52:04 +0900

    feat(website): show relevant commits to BenchmarkChart

--- website/webapp/src/components/BenchmarkChart/TooltipContent.tsx
+++ website/webapp/src/components/BenchmarkChart/TooltipContent.tsx
@@ -1,18 +1,52 @@
-import { ReactElement } from "react"
+import { ReactElement, useMemo } from "react"
+
+import localCommits from "@benchmark/localCommits.json"
 
 import { BenchWithCommit } from "./BenchmarkDataHandler"
 import { Bench } from "./benchmarkDataSchema"
+import { groupBySeparatorKeys } from "./groupBySeparatorKeys"
+import { LocalCommit } from "./localCommitSchema"
 import { TooltipContentProps } from "./useTooltip"
+
+function ShowCommits(
+  { commitHash, commitHashArray }: {
+    commitHash: string
+    commitHashArray: string[]
+  }
+): ReactElement {
+  const groups = useMemo(() => (
+    groupBySeparatorKeys(
+      localCommits as LocalCommit[],
+      commitHashArray,
+      (localCommit) => localCommit.id,
+    )
+  ), [commitHashArray])
+  const localCommitsToShow: LocalCommit[] | undefined = groups.get(commitHash)
+  if (localCommitsToShow === undefined) {
+    throw new Error(`commit hash ${commitHash} not found`)
+  }
+  return (
+    <div>
+      {localCommitsToShow.map((localCommit) => (
+        <div key={localCommit.id}>
+          {localCommit.id}
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export function TooltipContent(
   {
     item: benchWithCommit,
     renderBox,
+    globalData: commitHashArray,
   }: TooltipContentProps<BenchWithCommit, string[]>
 ): ReactElement {
   const { commit, bench } = benchWithCommit
   return (
     <div>
+      <ShowCommits commitHash={commit.id} commitHashArray={commitHashArray} />
       <div>
         <a
           href={commit.url} target="_blank" rel="noreferrer"

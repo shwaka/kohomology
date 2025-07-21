b1537aac45ffe2e769004c06c44107bae03d85d9
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-07-21 18:22:31 +0900

    test(website): add vite-tsconfig-paths

--- website/webapp/vite.config.ts
+++ website/webapp/vite.config.ts
@@ -1,11 +1,12 @@
 import path from "path"
 
 import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",

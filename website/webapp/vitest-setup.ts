ead9ca841bb6d1268a5400efe50f3f025fb52795
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-07-21 18:37:52 +0900

    test(website): add mock for worker contexts to vitest-setup.ts

--- website/webapp/vitest-setup.ts
+++ website/webapp/vitest-setup.ts
@@ -2,7 +2,21 @@ import "@testing-library/jest-dom/vitest"
 
 import { cleanup } from "@testing-library/react"
 import { afterEach } from "vitest"
+import { vi } from "vitest"
+
+import { kohomologyWorkerContext as mockKohomologyWorkerContext } from "./src/calculator/Calculator/kohomologyWorker/__mocks__/kohomologyWorkerContext"
+import * as kohomologyWorkerContextModule from "./src/calculator/Calculator/kohomologyWorker/kohomologyWorkerContext"
+import { myWorkerContext as mockMyWorkerContext } from "./src/calculator/WorkerContext/__mocks__/myWorkerContext"
+import * as myWorkerContextModule from "./src/calculator/WorkerContext/__playground__/myWorkerContext"
 
 afterEach(() => {
   cleanup()
 })
+
+vi.spyOn(kohomologyWorkerContextModule, "kohomologyWorkerContext", "get").mockReturnValue(
+  mockKohomologyWorkerContext
+)
+
+vi.spyOn(myWorkerContextModule, "myWorkerContext", "get").mockReturnValue(
+  mockMyWorkerContext
+)

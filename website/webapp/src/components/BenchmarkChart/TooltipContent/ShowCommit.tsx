036d85b5d63a41484e076d8eee6bbd1a27ca32df
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-07-27 10:06:42 +0900

    refactor(website): add ShowCommit.tsx

---
+++ website/webapp/src/components/BenchmarkChart/TooltipContent/ShowCommit.tsx
@@ -0,0 +1,78 @@
+import { ReactElement } from "react"
+
+import { LocalCommit } from "../schema/localCommitSchema"
+
+function ShowTimestamp({ timestamp }: { timestamp: string }): ReactElement {
+  const regex = /^(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})([+-]\d{2}:\d{2})$/
+  const match = timestamp.match(regex)
+
+  if (match) {
+    const [, date, time, timezone] = match
+    return (
+      <span>
+        {`${date} ${time} (UTC${timezone})`}
+      </span>
+    )
+  } else {
+    return (
+      <span>{timestamp}</span>
+    )
+  }
+}
+
+function ShowCommitHeader(
+  { url, commitHash, timestamp }: {
+    url: string
+    commitHash: string
+    timestamp: string
+  }
+): ReactElement {
+  return (
+    <div>
+      <a
+        href={url} target="_blank" rel="noreferrer"
+        style={{
+          color: "inherit",
+          textDecoration: "underline",
+          fontFamily: "monospace",
+          paddingRight: "3px",
+        }}
+      >
+        {commitHash}
+      </a>
+      <span>
+        <ShowTimestamp timestamp={timestamp} />
+      </span>
+    </div>
+  )
+}
+
+function ShowMessage({ message }: { message: string }): ReactElement {
+  return (
+    <div
+      style={{
+        paddingLeft: "3px",
+        lineHeight: 1.1,
+        whiteSpace: "pre-wrap",
+      }}
+    >
+      {message}
+    </div>
+  )
+}
+
+export function ShowCommit(
+  { localCommit }: { localCommit: LocalCommit }
+): ReactElement {
+  const commitHash = localCommit.id.slice(0, 7)
+  return (
+    <div>
+      <ShowCommitHeader
+        url={localCommit.url}
+        timestamp={localCommit.timestamp}
+        commitHash={commitHash}
+      />
+      <ShowMessage message={localCommit.message} />
+    </div>
+  )
+}

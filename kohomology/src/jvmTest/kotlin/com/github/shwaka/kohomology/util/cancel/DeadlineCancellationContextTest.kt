66291b588d7f6b87dc2e4f90eac47d06226a9af3
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-06-25 14:59:23 +0900

    test(core): add tests for each CancellationCheckerStorage

--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/util/cancel/DeadlineCancellationContextTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/util/cancel/DeadlineCancellationContextTest.kt
@@ -3,9 +3,23 @@ package com.github.shwaka.kohomology.util.cancel
 import io.kotest.core.spec.style.FreeSpec
 
 class DeadlineCancellationContextTest : FreeSpec({
-    "test with CancellationTestUtil" - {
+    "test with default storage" - {
         CancellationTestUtil.run {
             testAll { DeadlineCancellationContext() }
+        }
+    }
+
+    "test with ThreadLocalCancellationCheckerStorage" - {
+        CancellationTestUtil.run {
+            val storage = ThreadLocalCancellationCheckerStorage()
+            testAll { DeadlineCancellationContext(storage = storage) }
+        }
+    }
+
+    "test with SingleThreadCancellationCheckerStorage" - {
+        CancellationTestUtil.run {
+            val storage = SingleThreadCancellationCheckerStorage()
+            testAll { DeadlineCancellationContext(storage = storage) }
         }
     }
 })

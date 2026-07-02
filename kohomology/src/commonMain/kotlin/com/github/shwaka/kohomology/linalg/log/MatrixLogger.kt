f8862fedfdf175cd5b21e11540c76f35cd34d9d7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-07-02 14:00:46 +0900

    test(core): add matrixLogTest

--- kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/linalg/log/MatrixLogger.kt
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/linalg/log/MatrixLogger.kt
@@ -1,9 +1,15 @@
 package com.github.shwaka.kohomology.linalg.log
 
 public class MatrixLogger {
-    private val entries: MutableList<MatrixLogEntry<*>> = mutableListOf()
+    private val _entries: MutableList<MatrixLogEntry<*>> = mutableListOf()
+    public val entries: List<MatrixLogEntry<*>>
+        get() = _entries.toList()
 
     public fun addEntry(entry: MatrixLogEntry<*>) {
-        this.entries.add(entry)
+        this._entries.add(entry)
+    }
+
+    public fun clearEntries() {
+        this._entries.clear()
     }
 }
--- kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/linalg/log/MatrixSpaceWithLogTest.kt
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/linalg/log/MatrixSpaceWithLogTest.kt
@@ -1,5 +1,8 @@
 package com.github.shwaka.kohomology.linalg.log
 
+import com.github.shwaka.kohomology.linalg.Matrix
+import com.github.shwaka.kohomology.linalg.NumVector
+import com.github.shwaka.kohomology.linalg.Scalar
 import com.github.shwaka.kohomology.linalg.determinantTest
 import com.github.shwaka.kohomology.linalg.findPreimageGenTest
 import com.github.shwaka.kohomology.linalg.matrixOfRank2Test
@@ -10,7 +13,34 @@ import com.github.shwaka.kohomology.linalg.maxValueForDet
 import com.github.shwaka.kohomology.linalg.rowEchelonFormGenTest
 import com.github.shwaka.kohomology.specific.SparseMatrixSpaceOverRational
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.freeSpec
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
+
+fun <S : Scalar, V : NumVector<S>, M : Matrix<S, V>> matrixLogTest(
+    matrixSpace: MatrixSpaceWithLog<S, V, M>
+) = freeSpec {
+    val logger = matrixSpace.logger
+    beforeEach {
+        logger.clearEntries()
+    }
+    "test logger" - {
+        matrixSpace.context.run {
+            "add entry" {
+                logger.entries.shouldHaveSize(0)
+                val matrix1 = matrixSpace.getZero(2, 3)
+                val matrix2 = matrixSpace.getZero(3, 4)
+                matrix1 * matrix2
+                logger.entries.shouldHaveSize(1)
+                logger.entries[0].data shouldBe MultiplyMatrixLog(
+                    firstRowCount = 2,
+                    firstColCount = 3,
+                    secondColCount = 4,
+                )
+            }
+        }
+    }
+}
 
 class MatrixSpaceWithLogTest : FreeSpec({
     tags(matrixTag)
@@ -28,4 +58,6 @@ class MatrixSpaceWithLogTest : FreeSpec({
     "test toString()" {
         matrixSpace.toString() shouldBe "MatrixSpaceWithLog(SparseMatrixSpace(RationalField))"
     }
+
+    include(matrixLogTest(matrixSpace))
 })

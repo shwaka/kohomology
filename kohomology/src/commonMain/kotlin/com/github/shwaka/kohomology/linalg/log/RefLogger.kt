db7fadbc440098ad768f6fff27727f64fb7dbb37
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-07-04 14:08:40 +0900

    feat(core): add RefLogger.kt (WIP)

---
+++ kohomology/src/commonMain/kotlin/com/github/shwaka/kohomology/linalg/log/RefLogger.kt
@@ -0,0 +1,94 @@
+package com.github.shwaka.kohomology.linalg.log
+
+import kotlin.time.Duration
+
+public enum class RefOperation(
+    override val displayName: String
+) : OperationKind {
+    UNREDUCED("REF_Unreduced"),
+    REDUCED("REF_Reduced"),
+    PIVOTS("REF_Pivots"),
+    SIGN("REF_Sign"),
+    RANK("REF_Rank"),
+}
+
+public sealed interface RefOperationInput : OperationInput<RefOperation> {
+    public data class Unreduced(
+        public val rowCount: Int,
+        public val colCount: Int,
+    ) : RefOperationInput {
+        override val operation: RefOperation = RefOperation.UNREDUCED
+    }
+}
+
+public sealed interface RefOperationMetrics {
+    public fun toPrettyString(): String
+
+    public data class Unreduced(
+        public val maxRowCount: Int,
+        public val maxColCount: Int,
+    ) : RefOperationMetrics {
+        override fun toPrettyString(): String =
+            "maxRow=$maxRowCount, maxCol=$maxColCount"
+
+        public companion object {
+            public fun fromInputs(
+                inputs: List<RefOperationInput.Unreduced>,
+            ): Unreduced {
+                require(inputs.isNotEmpty()) { "inputs must not be empty." }
+                return Unreduced(
+                    maxRowCount = inputs.maxOf { it.rowCount },
+                    maxColCount = inputs.maxOf { it.colCount },
+                )
+            }
+        }
+    }
+}
+
+public data class RefOperationSummary(
+    override val operation: RefOperation,
+    override val invocationCount: Int,
+    override val maxDuration: Duration,
+    override val totalDuration: Duration,
+    public val metrics: RefOperationMetrics,
+) : OperationSummary<RefOperation> {
+    override val metricsText: String
+        get() = metrics.toPrettyString()
+}
+
+public object RefOperationSummaryFactory :
+    OperationSummaryFactory<RefOperation, RefOperationInput, RefOperationSummary> {
+    override fun create(
+        operation: RefOperation,
+        measurements: List<OperationMeasurement<RefOperation, RefOperationInput>>
+    ): RefOperationSummary {
+        require(measurements.isNotEmpty()) {
+            "measurements must not be empty."
+        }
+
+        require(measurements.all { it.input.operation == operation }) {
+            "All measurements must have the specified operation."
+        }
+
+        return RefOperationSummary(
+            operation = operation,
+            invocationCount = measurements.size,
+            maxDuration = measurements.maxOfOrNull { it.duration } ?: Duration.ZERO,
+            totalDuration = measurements.fold(Duration.ZERO) { acc, measurement ->
+                acc + measurement.duration
+            },
+            metrics = this.createMetrics(operation, measurements),
+        )
+    }
+
+    private fun createMetrics(
+        operation: RefOperation,
+        measurements: List<OperationMeasurement<RefOperation, RefOperationInput>>,
+    ): RefOperationMetrics {
+        return when (operation) {
+            RefOperation.UNREDUCED ->
+                RefOperationMetrics.Unreduced.fromInputs(measurements.castedInputs())
+            else -> TODO()
+        }
+    }
+}

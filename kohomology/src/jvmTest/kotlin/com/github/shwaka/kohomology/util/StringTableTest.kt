f96fb4af312e0cd0b9e88594b8a689c7b6410e7e
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-07-12 12:00:27 +0900

    test(core): add StringTableTest.kt

---
+++ kohomology/src/jvmTest/kotlin/com/github/shwaka/kohomology/util/StringTableTest.kt
@@ -0,0 +1,66 @@
+package com.github.shwaka.kohomology.util
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class StringTableTest : FreeSpec({
+    "toString for empty table" {
+        val table = StringTable(emptyList())
+
+        table.toString() shouldBe "[  ]"
+    }
+
+    "toPrettyString for empty table" {
+        val table = StringTable(emptyList())
+
+        table.toPrettyString() shouldBe "[ ]"
+    }
+
+    "toString for multiple rows" {
+        val data = listOf(
+            listOf("1", "22", "333"),
+            listOf("44", "5", "6"),
+        )
+        val table = StringTable(data)
+
+        table.toString() shouldBe "[ [1, 22, 333] [44, 5, 6] ]"
+    }
+
+    "toPrettyString for one row" {
+        val data = listOf(
+            listOf("1", "22", "333"),
+        )
+        val table = StringTable(data)
+
+        table.toPrettyString() shouldBe "[ 1 22 333 ]"
+    }
+
+    "toPrettyString for multiple rows" {
+        val data = listOf(
+            listOf("1", "22", "333"),
+            listOf("44", "5", "6"),
+        )
+        val table = StringTable(data)
+        val expected = """
+            |⎡  1 22 333 ⎤
+            |⎣ 44  5   6 ⎦
+        """.trimMargin()
+
+        table.toPrettyString() shouldBe expected
+    }
+
+    "toPrettyString with custom separator and paren" {
+        val data = listOf(
+            listOf("1", "22", "333"),
+            listOf("44", "5", "6"),
+        )
+        val paren = Paren(separator = ",")
+        val table = StringTable(data, paren)
+        val expected = """
+            |[  1,22,333 ]
+            |[ 44, 5,  6 ]
+        """.trimMargin()
+
+        table.toPrettyString() shouldBe expected
+    }
+})
